6d02fd333d1c583b380785ff7334f3c9e36e520e
Armand <95971503+Jhobahtes@users.noreply.github.com>
2022-04-03 01:45:09 +0200

    Use their respective files for users and db
    Move users endpoint to their respective file
    Move database connector in dependencies

--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from app import api
+
+from . import models
+from .database import engine
 
 from app import api
 
 app = FastAPI()
 
 app.include_router(api.api_router)
+
+
+# @app.post("/users/{user_id}/items/", response_model=schemas.Item)
+# def create_item_for_user(
+#     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+# ):
+#     return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+# @app.get("/items/", response_model=list[schemas.Item])
+# def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#     items = crud.get_items(db, skip=skip, limit=limit)
+#     return items
