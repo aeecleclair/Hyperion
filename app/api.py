ef4d402b7d713fd46372831cb5a701a7af0babbc
SESTU Flavio <flaviosestu@yahoo.fr>
2022-04-02 22:47:33 +0200

    Changes done as asked by reviewer

--- app/api.py
+++ app/api.py
@@ -3,8 +3,8 @@ from app.endpoints import amap, associations, bdebooking, bdecalendar, users
 
 
 api_router = APIRouter()
-api_router.include_router(amap.router)
+api_router.include_router(amap.router, prefix="/amap")
 api_router.include_router(associations.router)
-api_router.include_router(bdebooking.router)
-api_router.include_router(bdecalendar.router)
+api_router.include_router(bdebooking.router, prefix="/bdebooking")
+api_router.include_router(bdecalendar.router, prefix="/bdecalendar")
 api_router.include_router(users.router)
