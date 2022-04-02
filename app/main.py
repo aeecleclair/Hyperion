dab4871bb6fd7ca104265a49cdcadf0f9729db35
Nathanael-lscx <95610494+Nathanael-lscx@users.noreply.github.com>
2022-04-03 01:04:39 +0200

    First post possible

--- app/main.py
+++ app/main.py
@@ -22,9 +22,9 @@ def get_db():
 
 @app.post("/users/", response_model=schemas.CoreUser)
 def create_user(user: schemas.CoreUserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
+    # db_user = crud.get_user_by_email(db, email=user.email)
+    # if db_user:
+    #     raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
 
