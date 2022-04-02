2ac6ab55efdcb6255831852ef39d816f90999f9b
Nathanael-lscx <95610494+Nathanael-lscx@users.noreply.github.com>
2022-04-02 22:08:22 +0200

    Changement nom classes

--- app/main.py
+++ app/main.py
@@ -20,21 +20,21 @@ def get_db():
         db.close()
 
 
-# @app.post("/users/", response_model=schemas.User)
-# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-#     db_user = crud.get_user_by_email(db, email=user.email)
-#     if db_user:
-#         raise HTTPException(status_code=400, detail="Email already registered")
-#     return crud.create_user(db=db, user=user)
+@app.post("/users/", response_model=schemas.CoreUser)
+def create_user(user: schemas.CoreUserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
 
 
-@app.get("/users/", response_model=list[schemas.Core_user])
+@app.get("/users/", response_model=list[schemas.CoreUser])
 def read_users(db: Session = Depends(get_db)):
     users = crud.get_users(db)
     return users
 
 
-@app.get("/users/{user_id}", response_model=schemas.Core_user)
+@app.get("/users/{user_id}", response_model=schemas.CoreUser)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_id(db, user_id=user_id)
     if db_user is None:
