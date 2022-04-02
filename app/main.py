13cf4d66d2bd125bd8b609b7c047be0706ade18e
AtomHare <29772841+AtomHare@users.noreply.github.com>
2022-04-02 22:07:17 +0200

    Comply with the unit test

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import amap, associations, bdebooking, bdecalendar, users
+from app.endpoints import amap, associations, bdebooking, bdecalendar, users
 
 
 app = FastAPI()
