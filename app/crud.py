3c838a2f3c4d1e535f991c0ff13b71c914d5c2aa
Nathanael-lscx <95610494+Nathanael-lscx@users.noreply.github.com>
2022-04-03 01:04:39 +0200

    First post possible

--- app/crud.py
+++ app/crud.py
@@ -15,21 +15,32 @@ def get_group(db: Session):
     return db.query(models.CoreGroup).all()
 
 
+def create_user(db: Session, user: schemas.CoreUserCreate):
+    fakePassword = user.password + "notreallyhashed"
+    db_user = models.CoreUser(
+        login=user.login,
+        password=fakePassword,
+        name=user.name,
+        firstname=user.firstname,
+        nick=user.nick,
+        birth=user.birth,
+        promo=user.promo,
+        floor=user.floor,
+        created_on=user.created_on,
+        email=user.email,
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
 # def get_user_by_email(db: Session, email: str):
 #     return db.query(models.User).filter(models.User.email == email).first()
 
 
 # def get_users(db: Session, skip: int = 0, limit: int = 100):
 #     return db.query(models.User).offset(skip).limit(limit).all()
-
-
-# def create_user(db: Session, user: schemas.UserCreate):
-#     fakePassword = user.password + "notreallyhashed"
-#     db_user = models.User(email=user.email, hashed_password=fakePassword)
-#     db.add(db_user)
-#     db.commit()
-#     db.refresh(db_user)
-#     return db_user
 
 
 # def get_items(db: Session, skip: int = 0, limit: int = 100):
