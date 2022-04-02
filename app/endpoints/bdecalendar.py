c08c11bde445da0a165d89d702472ad98d955f0e
Baptiste Grignon <baptistegrignon67@gmail.com>
2022-04-02 19:33:51 +0200

    Create bdecalendar.py

---
+++ app/endpoints/bdecalendar.py
@@ -0,0 +1,34 @@
+@app.get("/events")
+async def get_events():
+
+    return ""
+
+
+@app.get("/events/unconfirmed")
+async def get_events_unconfirmed():
+
+    return ""
+
+
+@app.post("/events")
+async def create_events():
+
+    return ""
+
+
+@app.put("/events/{events_id}")
+async def edit_events_id(events_id):
+
+    return ""
+
+
+@app.put("/events/{events_id}/confirm")
+async def edit_events_id_confirmation(events_id):
+
+    return ""
+
+
+@app.delete("/events/{events_id}")
+async def delete_events_id(events_id):
+
+    return ""
