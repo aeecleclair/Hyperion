6ec6716018c6eef3bd86da5a144a3ded0483e48e
Armand <95971503+Jhobahtes@users.noreply.github.com>
2022-04-03 15:49:29 +0200

    Remove old tests

---
+++ tests/test_users.py
@@ -0,0 +1,30 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_create_user():
+    # TODO
+    pass
+
+
+def test_read_users():
+    # TODO
+    pass
+
+
+def test_read_user():
+    # TODO
+    pass
+
+
+def test_edit_user():
+    # TODO
+    pass
+
+
+def test_delete_user():
+    # TODO
+    pass
