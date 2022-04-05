e7e74815041724afdac167ef7faf4ac5bc37ed80
Nathanael-lscx <95610494+Nathanael-lscx@users.noreply.github.com>
2022-04-05 12:53:22 +0200

    POST request on groups working

--- app/cruds/cruds_groups.py
+++ app/cruds/cruds_groups.py
@@ -1,8 +1,8 @@
-# from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from ..models import models_core
+from ..schemas import schemas_core
 
-# from ..schemas import schemas_core
 from sqlalchemy import select
 
 # from sqlalchemy import delete
@@ -11,3 +11,17 @@ from sqlalchemy import select
 async def get_groups(db: AsyncSession):
     result = await db.execute(select(models_core.CoreGroup))
     return result.scalars().all()
+
+
+async def create_group(group: schemas_core.CoreGroupCreate, db: AsyncSession):
+    db_group = models_core.CoreGroup(
+        name=group.name,
+        description=group.description,
+    )
+    db.add(db_group)
+    try:
+        await db.commit()
+        return db_group
+    except IntegrityError:
+        await db.rollback()
+        raise ValueError("Email already registered")
--- app/endpoints/groups.py
+++ app/endpoints/groups.py
@@ -1,6 +1,4 @@
-from fastapi import APIRouter, Depends
-
-# from fastapi import HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.dependencies import get_db
 from app.cruds import cruds_groups
@@ -8,8 +6,30 @@ from app.schemas import schemas_core
 
 router = APIRouter()
 
+""" Working """
+
 
 @router.get("/groups", response_model=list[schemas_core.CoreGroupBase])
 async def get_groups(db: AsyncSession = Depends(get_db)):
     groups = await cruds_groups.get_groups(db)
     return groups
+
+
+@router.post("/groups", response_model=schemas_core.CoreGroupBase)
+async def create_group(
+    group: schemas_core.CoreGroupCreate, db: AsyncSession = Depends(get_db)
+):
+    try:
+        return await cruds_groups.create_group(group=group, db=db)
+    except ValueError as error:
+        raise HTTPException(status_code=422, detail=str(error))
+
+
+# schemas_core.CoreUserBase)
+# async def create_user(
+#     user: schemas_core.CoreUserCreate, db: AsyncSession = Depends(get_db)
+# ):
+#     try:
+#         return await cruds_users.create_user(user=user, db=db)
+#     except ValueError as error:
+#         raise HTTPException(status_code=422, detail=str(error))
