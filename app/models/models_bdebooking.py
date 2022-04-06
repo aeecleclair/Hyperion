171977d099a834f67718ae60db3d17136d50d30f
vangot <victor.angot@ecl21.ec-lyon.fr>
2022-04-06 21:11:56 +0200

    Comment the database and create a schema for booking request

--- app/models/models_bdebooking.py
+++ app/models/models_bdebooking.py
@@ -6,16 +6,21 @@ class RoomBooking(Base):
     __tablename__ = "room_booking"
 
     id = Column(Integer, primary_key=True, index=True)  # Use UUID later
-    booker = Column(VARCHAR)
-    room = Column(VARCHAR)
-    start = Column(DateTime)
-    end = Column(DateTime)
-    reason = Column(TEXT)
-    notes = Column(TEXT)
-    key = Column(Boolean)
-    pending = Column(Boolean)
-    multiple_days = Column(Boolean)
-    recurring = Column(Boolean)
+    booker = Column(
+        VARCHAR, nullable=False
+    )  # the id of the user in the table CoreUser (require authentification)
+    room = Column(VARCHAR, nullable=False, unique=True)
+    start = Column(DateTime, nullable=False)
+    end = Column(DateTime, nullable=False)
+    reason = Column(TEXT)  # Reason of the booking
+    notes = Column(TEXT)  # Allow booker to precise the reason of the booking
+    key = Column(Boolean, nullable=False)  # True if the room booked require a key
+    pending = Column(Boolean, default=True)  # True if the booking is pending
+    multiple_days = Column(Boolean)  # is it useful ?
+    recurring = Column(Boolean)  # True if the booking is recurring
 
     # link the table RoomBooking to the table CoreUser with a one to many relationship on the id_user
     booker_id = Column(Integer, ForeignKey("core_user.id"))
+
+
+# TODO : create a table room
