a9915439d88962ba1a8b154106cbe18d6af155de
vangot <victor.angot@ecl21.ec-lyon.fr>
2022-04-06 19:25:36 +0200

    Add the database and the crud for the booking module (except PUT)
    I've also add commentary on some files to start document the code

--- app/models/models_core.py
+++ app/models/models_core.py
@@ -9,17 +9,23 @@ from sqlalchemy.orm import relationship
 from ..database import Base
 
 
-class CoreMembership(Base):
+class CoreMembership(
+    Base
+):  # Create a table for the many to many relationship between CoreUser and CoreGroup
     __tablename__ = "core_membership"
 
-    id_user = Column(ForeignKey("core_user.id"), primary_key=True)
-    id_group = Column(ForeignKey("core_group.id"), primary_key=True)
+    id_user = Column(
+        ForeignKey("core_user.id"), primary_key=True
+    )  # The id of the user in the table CoreUser
+    id_group = Column(
+        ForeignKey("core_group.id"), primary_key=True
+    )  # The id of the group in the table CoreGroup
 
 
 class CoreUser(Base):
     __tablename__ = "core_user"
 
-    id = Column(Integer, primary_key=True, index=True)  # Use UID later
+    id = Column(Integer, primary_key=True, index=True)  # Use UUID later
     email = Column(String, unique=True, index=True, nullable=False)
     name = Column(String, nullable=False)
     firstname = Column(String, nullable=False)
@@ -31,12 +37,15 @@ class CoreUser(Base):
     created_on = Column(DateTime)
     password = Column(String, nullable=False)  # the password is hashed
 
-    groups = relationship(
+    groups = relationship(  # Create a many to many relationship between CoreUser and CoreGroup
         "CoreGroup",
         secondary="core_membership",
         back_populates="members",
         lazy="selectin",  # So that we don't have problems of implicite query that cause async issue
     )
+    booker = relationship(
+        "RoomBooking"
+    )  # create a one to many relationship between CoreUser and RoomBooking
 
 
 class CoreGroup(Base):
