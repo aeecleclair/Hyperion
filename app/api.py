a62fac492f308e89eb607e80a4b6f5b8eb742e76
Armand <95971503+Jhobahtes@users.noreply.github.com>
2022-04-09 22:47:49 +0200

    Add an admin endpoint to send an email

--- app/api.py
+++ app/api.py
@@ -1,10 +1,20 @@
 """File defining all the routes for the module, to configure the router"""
 
 from fastapi import APIRouter
+from app.endpoints import (
+    admin,
+    amap,
+    associations,
+    bdebooking,
+    bdecalendar,
+    groups,
+    users,
+)
 
 from app.endpoints import amap, associations, bdebooking, bdecalendar, groups, users
 
 api_router = APIRouter()
+api_router.include_router(admin.router)
 api_router.include_router(amap.router)
 api_router.include_router(associations.router)
 api_router.include_router(bdebooking.router)
