b83d1073f24e9bc4cced75d8561ebb43b80a25b7
Armand <95971503+Jhobahtes@users.noreply.github.com>
2022-04-12 11:11:25 +0200

    Use a dedicated function to generate tokens

--- app/core/security.py
+++ app/core/security.py
@@ -1,2 +1,13 @@
+import secrets
+
+
 def get_password_hash(password: str) -> str:
     return password
+
+
+def generate_token() -> str:
+    """
+    Generate a 32 bytes cryptographically strong random urlsafe token using the *secrets* library
+    """
+    # We use https://docs.python.org/3/library/secrets.html#secrets.token_urlsafe to generate the activation secret token
+    return secrets.token_urlsafe(32)
