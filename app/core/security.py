55a4c663d86d1dabf66996e7df703232183a1e97
Armand <95971503+Jhobahtes@users.noreply.github.com>
2022-04-12 21:00:22 +0200

    Add a pydantic password validator

--- app/core/security.py
+++ app/core/security.py
@@ -1,6 +1,18 @@
 import secrets
 
 
+def password_validator(password: str) -> str:
+    """
+    Check the password strength, validity and remove trailing spaces.
+    This function is intended to be used as a Pydantic validator:
+    https://pydantic-docs.helpmanual.io/usage/validators/#reuse-validators
+    """
+    # TODO
+    if len(password) < 6:
+        raise ValueError("The password must be at least 6 characters long")
+    return password.strip()
+
+
 def get_password_hash(password: str) -> str:
     return password
 
