665151d36a70abeaa6cb46357fd7f268e787b251
Brzuszek Mael <m.brzuszek48@gmail.com>
2022-04-16 14:18:00 +0200

    Examples for the doc and the tests

---
+++ app/utils/examples/examples_core.py
@@ -0,0 +1,18 @@
+example_CoreUserCreateRequest = {
+    "example": {
+        "email": "user@example.fr",
+        "account_type": "39691052-2ae5-4e12-99d0-7a9f5f2b0136",
+    }
+}
+
+
+example_CoreUserActivateRequest = {
+    "example": {
+        "name": "Name",
+        "firstname": "Firstname",
+        "nickname": "Antoine",
+        "activation_token": "62D-QJI5IYrjuywH8IWnuBo0xHrbTCfw_18HP4mdRrA",
+        "password": "areallycomplexpassword",
+        "floor": "A",
+    }
+}
