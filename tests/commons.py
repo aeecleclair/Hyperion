5fbac6bdfccdb3da8c27a792106ac391e2f565c3
Brzuszek Mael <m.brzuszek48@gmail.com>
2022-04-16 14:07:53 +0200

    Basic configuration for tests

--- tests/commons.py
+++ tests/commons.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import sessionmaker
 from app.database import Base
 from app.dependencies import get_db
 from app.main import app
+from app.models import models_core
+from app.utils.types.account_type import AccountType
 
 SQLALCHEMY_DATABASE_URL = (
     "sqlite+aiosqlite:///./test.db"  # Connect to the test's database
@@ -15,14 +17,6 @@ engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 TestingSessionLocal = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )  # Create a session for testing purposes
-
-
-@app.on_event("startup")
-async def startuptest():
-    # create db tables in test.db
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
 
 
 async def override_get_db() -> AsyncSession:
@@ -38,9 +32,22 @@ async def override_get_db() -> AsyncSession:
 app.dependency_overrides[get_db] = override_get_db
 
 
-def test_create_db():  # A first test is needed to run startuptest once
-    with TestClient(app):
-        pass
+@app.on_event("startup")
+async def startuptest():
+    # create db tables in test.db
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    # Add the necessary groups for account types
+    description = "Account type"
+    account_types = [
+        models_core.CoreGroup(id=id, name=id.name, description=description)
+        for id in AccountType
+    ]
+    async with TestingSessionLocal() as db:
+        db.add_all(account_types)
+        await db.commit()
 
 
 client = TestClient(app)  # Create a client to execute tests
