f5fd4c0ef679c443ac62074948b21b8e0ca10c7e
Armand <95971503+Jhobahtes@users.noreply.github.com>
2022-04-16 19:39:13 +0200

    [Authorization] Add a dependency to check if a user is in a group

--- app/dependencies.py
+++ app/dependencies.py
@@ -21,6 +21,7 @@ from app.core.settings import settings
 from app.cruds import cruds_users
 from app.models import models_core
 from app.schemas import schemas_core
+from app.utils.types.groups_type import GroupType
 
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
@@ -60,3 +61,23 @@ async def get_current_user(
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
+
+
+def generate_is_user_a_member_of_dependency(group_id: GroupType):
+    async def is_user_a_member_of(
+        db: AsyncSession = Depends(get_db),
+        user: models_core.CoreUser = Depends(get_current_user),
+    ):
+        # We can not directly test is group_id is in user.groups
+        # As user.groups is a list of CoreGroup as group_id is an UUID
+        for user_group in user.groups:
+            if group_id == user_group.id:
+                # We know the user is a member of the group, we don't need to return an error and can return the CoreUser object
+                return user
+
+        raise HTTPException(
+            status_code=403,
+            detail=f"Unauthorized, user is not a member of the group {group_id}",
+        )
+
+    return is_user_a_member_of
