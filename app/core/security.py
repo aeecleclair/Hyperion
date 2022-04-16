a89140f39438f58a78f55f7e16c33246d613e6fa
Armand <95971503+Jhobahtes@users.noreply.github.com>
2022-04-16 18:28:17 +0200

    [Security] Add password hashing

--- app/core/security.py
+++ app/core/security.py
@@ -1,5 +1,16 @@
 import secrets
 
+from passlib.context import CryptContext
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__rounds=13)
+"""
+In order to salt and hash password, we use a *passlib* [CryptContext](https://passlib.readthedocs.io/en/stable/narr/quickstart.html) object.
+
+We use "bcrypt" to hash password, a different hash will be added automatically for each password. See [Auth0 Understanding bcrypt](https://auth0.com/blog/hashing-in-action-understanding-bcrypt/) for informations about bcrypt.
+deprecated="auto" may be used to do password hash migration, see [Passlib hash migration](https://passlib.readthedocs.io/en/stable/narr/context-tutorial.html#deprecation-hash-migration).
+It is improtant to use enough rounds while accounting for the hash computation time. Default is 12. 13 allows for a 0.5 seconds computing delay.
+"""
 
 def password_validator(password: str) -> str:
     """
@@ -14,7 +25,18 @@ def password_validator(password: str) -> str:
 
 
 def get_password_hash(password: str) -> str:
-    return password
+    """
+    Return a salted hash computed from password. The function use a bcrypt based *passlib* CryptContext.
+    Both the salt and the algorithme identifier are included in the hash.
+    """
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password) -> bool:
+    """
+    Compare `plain_password` against its salted hash representation `hashed_password`. The function use a bcrypt based *passlib* CryptContext.
+    """
+    return pwd_context.verify(plain_password, hashed_password)
 
 
 def generate_token() -> str:
