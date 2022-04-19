a1d711986bab9f1575199a895bd80254f830f7c5
Armand <95971503+Jhobahtes@users.noreply.github.com>
2022-04-19 15:09:35 +0200

    Authorization code flow 2

---
+++ app/cruds/cruds_auth.py
@@ -0,0 +1,33 @@
+"""File defining the functions called by the endpoints, making queries to the table using the models"""
+
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models import models_core
+
+
+async def get_authorization_token_by_token(
+    db: AsyncSession, code: str
+) -> models_core.AuthorizationCode | None:
+    """Return group with id from database"""
+    result = await db.execute(
+        select(models_core.AuthorizationCode).where(
+            models_core.AuthorizationCode.code == code
+        )
+    )
+    return result.scalars().first()
+
+
+async def create_authorization_token(
+    db_authorization_code: models_core.AuthorizationCode, db: AsyncSession
+) -> models_core.CoreGroup:
+    """Create a new group in database and return it"""
+
+    db.add(db_authorization_code)
+    try:
+        await db.commit()
+        return db_authorization_code
+    except IntegrityError:
+        await db.rollback()
+        raise ValueError()
