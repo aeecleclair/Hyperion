3a24b1116c0b4c8f00fe8aaefd723581e48a1656
Maxime Lemonnier <maxime.lemonnier@ecl21.ec-lyon.fr>
2022-05-22 19:12:50 +0200

    Last AMAP Models and Schemas

--- app/schemas/schemas_amap.py
+++ app/schemas/schemas_amap.py
@@ -2,8 +2,9 @@
 
 from datetime import date, datetime
 
-from pydantic import BaseModel, validator
+from pydantic import BaseModel
 
+from app.schemas.schemas_core import CoreUserSimple
 from app.utils.types.groups_type import AmapSlotType
 
 
@@ -52,3 +53,54 @@ class DeliveryBase(BaseModel):
 class DeliveryCreate(BaseModel):
     deliveryDate: date
     products: list[ProductBase] = []
+
+
+class OrderBase(BaseModel):
+    """Base schema for AMAP orders"""
+
+    order_id: str
+    user: CoreUserSimple
+    delivery_id: str
+    products: list[ProductBase]
+    amount: float
+    collection_slot: AmapSlotType
+    ordering_date: datetime
+    delivery_date: date
+
+
+class OrderCreate(OrderBase):
+    user: CoreUserSimple
+    delivery_id: str
+    products: list[ProductBase]
+    collection_slot: AmapSlotType
+    delivery_date: date
+
+    class Config:
+        orm_mode = True
+
+
+class OrderEdit(OrderBase):
+    order_id: str
+    user: CoreUserSimple
+    delivery_id: str
+    products: list[ProductBase]
+    collection_slot: AmapSlotType
+    delivery_date: date
+
+    class Config:
+        orm_mode = True
+
+
+class AddProductDelivery(BaseModel):
+    product_id: str
+    delivery_id: str
+
+
+class CashBase(BaseModel):
+    user_id: str
+    user: CoreUserSimple
+    balance: float
+
+
+class CashUpdate(CashBase):
+    balance: float
