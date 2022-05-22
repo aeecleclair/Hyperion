a95d1ed658d672973fc4085d0c589d2402eb7c04
Armand <95971503+Jhobahtes@users.noreply.github.com>
2022-05-22 12:13:39 +0200

    PKCE

--- app/models/models_core.py
+++ app/models/models_core.py
@@ -11,12 +11,14 @@ from app.database import Base
 class AuthorizationCode(Base):
     __tablename__ = "authorization_code"
 
-    code = Column(String, primary_key=True, index=True)
-    expire_on = Column(DateTime, nullable=False)
-    scope = Column(String)
-    redirect_uri = Column(String)
-    user_id = Column(String)
-    nonce = Column(String)
+    code: str = Column(String, primary_key=True, index=True)
+    expire_on: datetime = Column(DateTime, nullable=False)
+    scope: str | None = Column(String)
+    redirect_uri: str | None = Column(String)
+    user_id: str = Column(String, nullable=False)
+    nonce: str | None = Column(String)
+    code_challenge: str | None = Column(String)
+    code_challenge_method: str | None = Column(String)
 
 
 class CoreMembership(Base):
