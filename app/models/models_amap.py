2cb07f0b622d7f0e5222baabf41bb2685ddb0945
Maxime Lemonnier <maxime.lemonnier@ecl21.ec-lyon.fr>
2022-05-22 16:08:45 +0200

    Add Amap Models

--- app/models/models_amap.py
+++ app/models/models_amap.py
@@ -1,17 +1,7 @@
 """model file for amap"""
 from datetime import date, datetime
 
-from sqlalchemy import (
-    Boolean,
-    Column,
-    Date,
-    DateTime,
-    Enum,
-    Float,
-    ForeignKey,
-    Integer,
-    String,
-)
+from sqlalchemy import Column, Date, DateTime, Enum, Float, ForeignKey, String, Table
 from sqlalchemy.orm import relationship
 
 from app.database import Base
@@ -22,14 +12,15 @@ from app.utils.types.groups_type import AmapSlotType
 class AmapOrderContent(Base):
     __tablename__ = "amap_order_content"
     product_id: str = Column(ForeignKey("amap_product.id"), primary_key=True)
-    order_id: str = Column(ForeignKey("amap_order.order_id"), primary_key=True)
-    quantity: int = Column(Integer)
+    order_id: str = Column(ForeignKey("amap_order.orderId"), primary_key=True)
 
 
 class AmapDeliveryContent(Base):
     __tablename__ = "amap_delivery_content"
     product_id: str = Column(ForeignKey("amap_product.id"), primary_key=True)
-    delivery_id: str = Column(ForeignKey("amap_delivery.id"), primary_key=True)
+    delivery_id: str = Column(
+        ForeignKey("amap_delivery.deliveryDate"), primary_key=True
+    )
 
 
 class Product(Base):
@@ -41,42 +32,40 @@ class Product(Base):
     category: str = Column(String, index=True, nullable=False)
 
 
-class Delivery(Base):
-    __tablename__ = "amap_delivery"
-
-    id: str = Column(String, primary_key=True, index=True)
-    delivery_date: date = Column(Date, nullable=False, unique=True, index=True)
-    products: list[Product] = relationship(
-        "Product",
-        secondary="amap_delivery_content",
-    )
-    locked: bool = Column(Boolean, nullable=False)
-
-
 class Order(Base):
     __tablename__ = "amap_order"
 
-    user_id: str = Column(String, ForeignKey("core_user.id"), primary_key=True)
+    userId: str = Column(String, ForeignKey("core_user.id"))
     user: CoreUser = relationship(
         "CoreUser",
     )
-    delivery_id: str = Column(
-        String, ForeignKey("amap_delivery.id"), index=True, nullable=True
-    )
-    delivery: Delivery = relationship("Delivery")
-    order_id: str = Column(String, primary_key=True, index=True)
-    products: list[Product] = relationship(
+    deliveryId: str = Column(String, index=True, nullable=True)
+    orderId: str = Column(String, primary_key=True, index=True)
+    products: list["Product"] = relationship(
         "Product",
         secondary="amap_order_content",
     )
     amount: float = Column(Float, nullable=False)
-    collection_slot: AmapSlotType = Column(Enum(AmapSlotType), nullable=False)
-    ordering_date: datetime = Column(DateTime, nullable=False)
-    delivery_date: date = Column(Date, nullable=False)
+    collectionSlot: AmapSlotType = Column(Enum(AmapSlotType), nullable=False)
+    orderingDate: datetime = Column(DateTime, nullable=False)
+    deliveryDate: date = Column(Date, nullable=False)
+
+
+class Delivery(Base):
+    __tablename__ = "amap_delivery"
+
+    deliveryDate: datetime = Column(Date, primary_key=True)
+    products: list["Product"] = relationship(
+        "Product",
+        secondary="amap_delivery_content",
+    )
 
 
 class Cash(Base):
     __tablename__ = "amap_cash"
 
-    user_id: str = Column(String, ForeignKey("core_user.id"), primary_key=True)
+    userId: str = Column(String, ForeignKey("core_user.id"), primary_key=True)
+    user: CoreUser = relationship(
+        "CoreUser",
+    )
     balance: float = Column(Float, nullable=False)
