5200c4e420a3fb4f0187cea525b5b096110d08c1
Armand <95971503+Jhobahtes@users.noreply.github.com>
2022-05-27 17:37:40 +0200

    Move password_validator to utils submodules
    to prevent a circular import between security and schemas

--- app/core/security.py
+++ app/core/security.py
@@ -31,18 +31,6 @@ The algorithme used to generate JWT access tokens
 """
 
 
-def password_validator(password: str) -> str:
-    """
-    Check the password strength, validity and remove trailing spaces.
-    This function is intended to be used as a Pydantic validator:
-    https://pydantic-docs.helpmanual.io/usage/validators/#reuse-validators
-    """
-    # TODO
-    if len(password) < 6:
-        raise ValueError("The password must be at least 6 characters long")
-    return password.strip()
-
-
 def get_password_hash(password: str) -> str:
     """
     Return a salted hash computed from password. The function use a bcrypt based *passlib* CryptContext.
