477d8f10420fbac3e0c143eb2b35a544b623d399
Haven <34400034+maximeroucher@users.noreply.github.com>
2022-06-06 22:35:32 +0200

    Change put to patch

--- app/endpoints/amap.py
+++ app/endpoints/amap.py
@@ -56,7 +56,7 @@ async def get_product_by_id(product_id: str, db: AsyncSession = Depends(get_db))
     return product
 
 
-@router.put(
+@router.patch(
     "/amap/products/{product_id}",
     status_code=200,
     tags=[Tags.amap],
@@ -247,7 +247,7 @@ async def add_order_to_delievery(
         raise HTTPException(status_code=422, detail=str(error))
 
 
-@router.put(
+@router.patch(
     "/amap/deliveries/{delivery_id}/orders",
     status_code=200,
     tags=[Tags.amap],
@@ -326,7 +326,7 @@ async def create_cash_of_user(
     return schemas_amap.CashBase(**result.__dict__)
 
 
-@router.put(
+@router.patch(
     "/amap/users/{user_id}/cash",
     status_code=200,
     tags=[Tags.amap],
