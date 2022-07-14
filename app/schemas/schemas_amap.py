d129b494d3f252a3e0b0d3316a4d2f0a529a91ed
Maxime Lemonnier <maxime.lemonnier@ecl21.ec-lyon.fr>
2022-07-14 13:46:10 +0200

    Fix Cash

--- app/schemas/schemas_amap.py
+++ app/schemas/schemas_amap.py
@@ -103,8 +103,11 @@ class CashBase(BaseModel):
     balance: float
 
 
-class CashComplete(CashBase):
+class CashId(CashBase):
     user_id: str
+
+
+class CashComplete(CashId):
     user: CoreUserSimple
 
     class Config:
