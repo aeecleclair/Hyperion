1017e2334ae600c5f481bbf8400635a9b55d87df
Jhobahtes <95971503+Jhobahtes@users.noreply.github.com>
2022-07-17 12:12:23 +0200

    Filter granted scopes by auth client

--- app/utils/types/scopes_type.py
+++ app/utils/types/scopes_type.py
@@ -6,9 +6,9 @@ class ScopeType(str, Enum):
     Various scope that can be included in JWT token
     """
 
-    # API allows the user to access every endpoint from the api (but not the auth endpoints)
+    # API allows the user to access every endpoint from the api (but not auth endpoints)
     API = "API"
-    # auth only allows the user to access the auth endpoints
+    # auth only allows the user to access auth endpoints
     auth = "auth"
     # openid only allows the user to access the userinfos auth endpoint
     openid = "openid"
