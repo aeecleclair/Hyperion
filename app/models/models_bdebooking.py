baef9a032a3f2282a35714bff6097a3ffa468726
Maxime Lemonnier <maxime.lemonnier@ecl21.ec-lyon.fr>
2022-07-23 11:27:03 +0200

    BDE Booking module

--- app/models/models_bdebooking.py
+++ app/models/models_bdebooking.py
@@ -1,26 +1,21 @@
-from sqlalchemy import Column, Integer, Boolean, DateTime, VARCHAR, TEXT, ForeignKey
-from ..database import Base
+"""model file for bdebooking"""
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, DateTime, String
+
+from app.database import Base
 
 
-class RoomBooking(Base):
-    __tablename__ = "room_booking"
-
-    id = Column(Integer, primary_key=True, index=True)  # Use UUID later
-    booker = Column(
-        VARCHAR, nullable=False
-    )  # the id of the user in the table CoreUser (require authentification)
-    room = Column(VARCHAR, nullable=False)
-    start = Column(DateTime, nullable=False)
-    end = Column(DateTime, nullable=False)
-    reason = Column(TEXT)  # Reason of the booking
-    notes = Column(TEXT)  # Allow booker to precise the reason of the booking
-    key = Column(Boolean, nullable=False)  # True if the room booked require a key
-    pending = Column(Boolean, default=True)  # True if the booking is pending
-    multiple_days = Column(Boolean)  # is it useful ?
-    recurring = Column(Boolean)  # True if the booking is recurring
-
-    # link the table RoomBooking to the table CoreUser with a one to many relationship on the id_user
-    booker_id = Column(Integer, ForeignKey("core_user.id"))
-
-
-# TODO : create a table room
+class Booking(Base):
+    __tablename__ = "bde_booking"
+    url: str = Column(String, nullable=False)
+    id: str = Column(String, primary_key=True, index=True)
+    reason: str = Column(String, nullable=False)
+    start: datetime = Column(DateTime, nullable=False)
+    end: datetime = Column(DateTime, nullable=False)
+    note: str = Column(String, nullable=True)
+    room: str = Column(String, nullable=False, index=True)
+    key: bool = Column(Boolean, nullable=False)
+    confirmed: bool = Column(Boolean, nullable=False)
+    multipleDay: bool = Column(Boolean, nullable=False)
+    recurring: bool = Column(Boolean, nullable=False)
---
+++ app/utils/types/tags.py
@@ -0,0 +1,11 @@
+"""File defining the tags for the automatic FastAPI documentation (found on http://domainname/docs)"""
+
+from enum import Enum
+
+
+class Tags(str, Enum):
+    users = "Users"
+    groups = "Groups"
+    auth = "Auth"
+    amap = "AMAP"
+    bdebooking = "BDEBooking"
