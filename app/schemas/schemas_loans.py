bbaff166e9cb082421db141af6a7b1c012611112
Victor Angot <victor.angot@gmail.com>
2022-08-01 19:24:05 +0200

    First schemas for loan

---
+++ app/schemas/schemas_loans.py
@@ -0,0 +1,51 @@
+from datetime import date
+
+from pydantic import BaseModel
+
+from app.schemas.schemas_core import CoreGroup, CoreUserSimple
+
+
+class ItemBase(BaseModel):
+    """Base schema for item's model"""
+
+    name: str
+    caution: str
+    group_id: str
+    expiration: date | None = None
+
+
+class ItemInDB(ItemBase):
+
+    id: str
+
+
+class Item(ItemInDB):
+
+    group: CoreGroup
+
+    class Config:
+        orm_mode = True
+
+
+class LoanBase(BaseModel):
+    """Base schema for loan's model"""
+
+    borrower_id: str
+    item: Item
+    notes: str | None = None
+    caution: bool | None = None
+
+
+class LoanInDB(LoanBase):
+
+    start: date
+    end: date
+    id: str
+
+
+class Loan(LoanInDB):
+    borrower: CoreUserSimple
+    item: Item  # TODO change in list[Item] = []
+
+    class Config:
+        orm_mode = True
