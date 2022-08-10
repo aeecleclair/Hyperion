9b809e6971c115fabefb5851230f4f33815c3ef2
Maxime Lemonnier <maxime.lemonnier@ecl21.ec-lyon.fr>
2022-08-10 14:37:08 +0200

    Resolve tests

--- tests/test_amap.py
+++ tests/test_amap.py
@@ -9,12 +9,8 @@ async def startuptest():
         test1 = models_amap.Product(
             id="test1", name="Test1", price=0.99, category="Tests"
         )
-        test2 = models_amap.Product(
-            id="test2", name="Test2", price=12, category="Tests"
-        )
 
         db.add(test1)
-        db.add(test2)
         await db.commit()
 
 
