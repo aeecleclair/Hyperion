3cec329aae34091da418a631d8304672daba44e4
Maxime Lemonnier <maxime.lemonnier@ecl21.ec-lyon.fr>
2022-08-15 14:10:55 +0200

    Add first test

--- tests/test_amap.py
+++ tests/test_amap.py
@@ -1,14 +1,28 @@
 from app.main import app
-from app.models import models_amap
-from tests.commons import TestingSessionLocal
+from app.models import models_core
+from app.utils.types.groups_type import GroupType
+from tests.commons import (
+    TestingSessionLocal,
+    client,
+    create_api_access_token,
+    create_user_with_groups,
+)
+
+amap_user: models_core.CoreUser | None = None
 
 
 @app.on_event("startup")  # create the datas needed in the tests
 async def startuptest():
+    global amap_user
     async with TestingSessionLocal() as db:
-        test1 = models_amap.Product(
-            id="test1", name="Test1", price=0.99, category="Tests"
-        )
-
-        db.add(test1)
+        amap_user = await create_user_with_groups([GroupType.amap], db=db)
         await db.commit()
+
+
+def test_get_rights():
+    token = create_api_access_token(amap_user)
+    response = client.get(
+        "/amap/rights",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 200
