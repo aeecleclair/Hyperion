cec329aab52b6d2748d9b43b4e93f515cbf054c5
Jhobahtes <95971503+Jhobahtes@users.noreply.github.com>
2022-08-15 15:37:58 +0200

    Models cleanup

--- app/models/models_loan.py
+++ app/models/models_loan.py
@@ -4,29 +4,43 @@ from sqlalchemy import TEXT, Boolean, Column, Date, DateTime, ForeignKey, String
 from sqlalchemy.orm import relationship
 
 from app.database import Base
-from app.models.models_core import CoreGroup, CoreUser
+from app.models.models_core import CoreUser
+
+
+class Loaner(Base):
+    __tablename__ = "loaner"
+
+    id: str = Column(String, primary_key=True, index=True)
+    name: str = Column(String, nullable=False, unique=True)
+    group_manager_id: str = Column(String, nullable=False)
+
+    items: list["LoanerItem"] = relationship("LoanerItem")
+    loans: list["Loan"] = relationship("Loan")
 
 
 class LoanContent(Base):
     __tablename__ = "loan_content"
 
     loan_id: str = Column(ForeignKey("loan.id"), primary_key=True)
-    item_id: str = Column(ForeignKey("item.id"), primary_key=True)
+    item_id: str = Column(ForeignKey("loaner_item.id"), primary_key=True)
 
 
-class Item(Base):
-    __tablename__ = "item"
+class LoanerItem(Base):
+    __tablename__ = "loaner_item"
 
     id: str = Column(String, primary_key=True, index=True)
-    name: str = Column(String)
-    caution: str = Column(String)
-    group_id: str = Column(ForeignKey("core_group.id"), index=True)
-    group: CoreGroup = relationship(
-        "CoreGroup",
-    )
+    name: str = Column(String, nullable=False, unique=True)
+    loaner_id: str = Column(ForeignKey("loaner.id"))
+    suggested_caution: str = Column(String)
+    multiple: bool = Column(Boolean)
+
     expiration: date = Column(Date, nullable=False)
 
-    loan: list["Loan"] = relationship("Loan", secondary="loan_content")
+    available: bool = Column(Boolean)
+
+    # loaner: Loaner = relationship(Loaner)
+
+    # loans: list["Loan"] = relationship("Loan", secondary="loan_content")
 
 
 class Loan(Base):
@@ -42,13 +56,20 @@ class Loan(Base):
     borrower: CoreUser = relationship(
         "CoreUser",
     )
+    loaner_id: str = Column(
+        ForeignKey("loaner.id"),
+        index=True,
+    )
+    loaner: Loaner = relationship(
+        "Loaner",
+    )
     start: datetime | None = Column(DateTime, nullable=False)
     end: datetime | None = Column(DateTime, nullable=False)
     notes: str | None = Column(TEXT, nullable=False)
     returned: bool = Column(Boolean)
-    caution: bool = Column(Boolean, nullable=False)
+    caution: str = Column(String)
 
-    item: list["Item"] = relationship(
-        "Item",
+    items: list["LoanerItem"] = relationship(
+        "LoanerItem",
         secondary="loan_content",
     )
