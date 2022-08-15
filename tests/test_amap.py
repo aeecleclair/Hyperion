f262db9e369ddc599d145ed422840ea94f4afd3a
Maxime Lemonnier <maxime.lemonnier@ecl21.ec-lyon.fr>
2022-08-15 16:10:07 +0200

    Add tests

--- tests/test_amap.py
+++ tests/test_amap.py
@@ -28,7 +28,7 @@ def test_get_rights():
     assert response.status_code == 200
 
 
-def test_product():
+def test_amap():
     token = create_api_access_token(amap_user)
 
     response = client.post(
@@ -60,6 +60,146 @@ def test_product():
 
     response = client.delete(
         f"/amap/products/{id}",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 204
+
+    response = client.post(
+        "/amap/products",
+        json={"name": "test", "price": 0.1, "category": "test"},
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    prod_id = response.json()["id"]
+
+    response = client.post(
+        "/amap/deliveries",
+        json={"delivery_date": "2022-08-15", "products_ids": [], "locked": False},
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 201
+    id = response.json()["id"]
+
+    response = client.get(
+        "/amap/deliveries",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 200
+
+    response = client.patch(
+        f"/amap/deliveries/{id}",
+        json={"delivery_date": "2022-08-18", "products_ids": [], "locked": False},
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 200
+
+    response = client.get(
+        f"/amap/deliveries/{id}/products",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 200
+
+    response = client.get(
+        f"/amap/deliveries/{id}/products",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 200
+
+    response = client.post(
+        f"/amap/deliveries/{id}/products",
+        json={"delivery_id": id, "product_id": prod_id},
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 201
+
+    response = client.delete(
+        f"/amap/deliveries/{id}/products",
+        json={"delivery_id": id, "product_id": prod_id},
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 204
+
+    response = client.get(
+        f"/amap/deliveries/{id}/orders",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 200
+
+    response = client.post(
+        f"/amap/deliveries/{amap_user.id}/products",
+        json={"balance": 50},
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 201
+
+    response = client.get(
+        "/amap/users/cash",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 200
+
+    response = client.get(
+        f"/amap/users/{amap_user.id}/cash",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 200
+
+    response = client.patch(
+        f"/amap/users/{amap_user.id}/cash",
+        json={"balance": 45},
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 200
+
+    response = client.post(
+        f"/amap/deliveries/{id}/orders",
+        json={
+            "user_id": amap_user.id,
+            "delivery_id": id,
+            "products_ids": [],
+            "collection_slot": "midi",
+            "delivery_date": "2022-08-16",
+            "products_quantity": [],
+        },
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 201
+    order_id = response.json()["order_id"]
+
+    response = client.patch(
+        f"/amap/deliveries/{id}/orders",
+        json={
+            "user_id": amap_user.id,
+            "delivery_id": id,
+            "products_ids": [],
+            "collection_slot": "soir",
+            "delivery_date": "2022-08-16",
+            "products_quantity": [],
+            "order_id": order_id,
+        },
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 200
+
+    response = client.get(
+        f"/amap/deliveries/{id}/orders/{order_id}",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 200
+
+    response = client.delete(
+        f"/amap/deliveries/{id}/orders/{order_id}",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 204
+
+    response = client.get(
+        f"/amap/users/{amap_user.id}/orders",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 200
+
+    response = client.delete(
+        f"/amap/deliveries/{id}",
         headers={"Authorization": f"Bearer {token}"},
     )
     assert response.status_code == 204
