a78d151f5611fee364d4fd4dad55cac886c296bb
Jhobahtes <95971503+Jhobahtes@users.noreply.github.com>
2022-08-17 15:49:43 +0200

    Fix test

--- tests/test_groups.py
+++ tests/test_groups.py
@@ -1,12 +1,23 @@
 from app.main import app
 from app.models import models_core
-from tests.commons import TestingSessionLocal, client
+from app.utils.types.groups_type import GroupType
+from tests.commons import (
+    TestingSessionLocal,
+    client,
+    create_api_access_token,
+    create_user_with_groups,
+)
+
+admin_user: models_core.CoreUser | None = None
+
 
 id_eclair = "8aab79e7-1e15-456d-b6e2-11e4e9f77e4f"
 
 
 @app.on_event("startup")  # create the datas needed in the tests
 async def startuptest():
+    global admin_user
+
     async with TestingSessionLocal() as db:
         eclair = models_core.CoreGroup(
             id=id_eclair,
@@ -14,11 +25,19 @@ async def startuptest():
             description="Les meilleurs",
         )
         db.add(eclair)
+
+        admin_user = await create_user_with_groups([GroupType.admin], db=db)
+
         await db.commit()
 
 
 def test_get_group_by_id():
-    response = client.get(f"/groups/{id_eclair}")
+    token = create_api_access_token(admin_user)
+
+    response = client.get(
+        f"/groups/{id_eclair}",
+        headers={"Authorization": f"Bearer {token}"},
+    )
     assert response.status_code == 200
     data = response.json()
     assert data["name"] == "eclair"
