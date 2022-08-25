136f64c4f2e5c4047b4d06571d6a079602434af4
Jhobahtes <95971503+Jhobahtes@users.noreply.github.com>
2022-08-25 10:02:34 +0200

    fixup

--- app/utils/types/standard_responses.py
+++ app/utils/types/standard_responses.py
@@ -3,3 +3,11 @@ from pydantic import BaseModel
 
 class Result(BaseModel):
     success: bool = True
+
+
+class BatchResult(BaseModel):
+    """
+    Return a dictionary of {key: error message} indicating which element of failed.
+    """
+
+    failed: dict[str, str]
