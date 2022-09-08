db7bd22b42fc5a336c62d3d7684a0ecef0da7058
benjamindenise <benjamin.denise@protonmail.com>
2022-09-08 10:44:08 +0200

    Add endpoints and cruds

--- app/cruds/cruds_calendar.py
+++ app/cruds/cruds_calendar.py
@@ -6,15 +6,23 @@ from app.models import models_calendar
 
 
 async def get_all_events(db: AsyncSession) -> list[models_calendar.Event]:
-
-    # On récupère tous les éléments Event dont le user_id correspond à celui que l'on recherche
+    """Retriveve all the events in the database."""
     result = await db.execute(select(models_calendar.Event))
     return result.scalars().all()
+
+
+async def get_event(db: AsyncSession, event_id: str) -> models_calendar.Event | None:
+    """Retrieve the event corresponding to `event_id` from the database."""
+    result = await db.execute(
+        select(models_calendar.Event).where(models_calendar.Event.id == event_id)
+    )
+    return result.scalars().first()
 
 
 async def add_event(
     db: AsyncSession, event: models_calendar.Event
 ) -> models_calendar.Event:
+    """Add an event to the database."""
 
     db.add(event)
     try:
