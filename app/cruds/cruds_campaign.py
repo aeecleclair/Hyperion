6ec3e47ff4586687d6802b8f6e8840c5c23c8668
benjamindenise <benjamin.denise@protonmail.com>
2022-09-19 12:46:57 +0200

    Add an endpoint

--- app/cruds/cruds_campaign.py
+++ app/cruds/cruds_campaign.py
@@ -1,9 +1,9 @@
-from sqlalchemy import delete, select, update
+from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import selectinload
 
 from app.models import models_campaign
+from app.schemas import schemas_campaign
 
 
 async def get_sections(db: AsyncSession) -> list[models_campaign.Sections]:
@@ -11,3 +11,15 @@ async def get_sections(db: AsyncSession) -> list[models_campaign.Sections]:
 
     result = await db.execute(select(models_campaign.Sections))
     return result.scalars().all()
+
+
+async def add_section(
+    db: AsyncSession, section: schemas_campaign.SectionComplete
+) -> None:
+    db_section = models_campaign.Sections(**section.dict())
+    db.add(db_section)
+    try:
+        await db.commit()
+    except IntegrityError:
+        await db.rollback()
+        raise ValueError("This name is already used")
