2cc85844325850a3b6cd0319865fd8daecdb5450
Jhobahtes <95971503+Jhobahtes@users.noreply.github.com>
2022-10-27 09:33:25 +0200

    Remove unused commented code

--- app/main.py
+++ app/main.py
@@ -147,16 +147,3 @@ async def startup():
 
 
 app.include_router(api.api_router)
-
-
-# @app.post("/users/{user_id}/items/", response_model=schemas.Item)
-# def create_item_for_user(
-#     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-# ):
-#     return crud.create_user_item(db=db, item=item, user_id=user_id)
-
-
-# @app.get("/items/", response_model=list[schemas.Item])
-# def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     items = crud.get_items(db, skip=skip, limit=limit)
-#     return items
