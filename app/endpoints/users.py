c423c34887a4dda2493dc90f12954bdfef68edda
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2022-12-06 17:14:08 +0100

    fix: token generator

--- app/endpoints/users.py
+++ app/endpoints/users.py
@@ -257,7 +257,7 @@ async def create_user(
         raise ValueError(f"An account with the email {email} already exist")
     # There might be an unconfirmed user in the database but its not an issue. We will generate a second activation token.
 
-    activation_token = security.generate_token()
+    activation_token = security.generate_token(nbytes=8)
 
     # Add the unconfirmed user to the unconfirmed_user table
 
