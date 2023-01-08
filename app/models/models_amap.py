092d99986691127ad001d7196d1b619340beec1b
Maxime Lemonnier <98470688+Malem38@users.noreply.github.com>
2023-01-08 18:07:30 +0100

    Add delivery status

--- app/models/models_amap.py
+++ app/models/models_amap.py
@@ -1,22 +1,12 @@
 """Models file for amap"""
 from datetime import date, datetime
 
-from sqlalchemy import (
-    Boolean,
-    Column,
-    Date,
-    DateTime,
-    Enum,
-    Float,
-    ForeignKey,
-    Integer,
-    String,
-)
+from sqlalchemy import Column, Date, DateTime, Enum, Float, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from app.database import Base
 from app.models.models_core import CoreUser
-from app.utils.types.amap_types import AmapSlotType
+from app.utils.types.amap_types import AmapSlotType, DeliveryStatusType
 
 
 class AmapOrderContent(Base):
@@ -24,6 +14,7 @@ class AmapOrderContent(Base):
     product_id: str = Column(ForeignKey("amap_product.id"), primary_key=True)
     order_id: str = Column(ForeignKey("amap_order.order_id"), primary_key=True)
     quantity: int = Column(Integer)
+    product: "Product" = relationship("Product")
 
 
 class AmapDeliveryContent(Base):
@@ -51,7 +42,7 @@ class Delivery(Base):
         secondary="amap_delivery_content",
     )
     orders: list["Order"] = relationship("Order", back_populates="delivery")
-    locked: bool = Column(Boolean, nullable=False)
+    status: DeliveryStatusType = Column(String, nullable=False)
 
 
 class Order(Base):
@@ -64,7 +55,6 @@ class Order(Base):
     delivery_id: str = Column(
         String, ForeignKey("amap_delivery.id"), index=True, nullable=True
     )
-    delivery: Delivery = relationship("Delivery", back_populates="orders")
     order_id: str = Column(String, primary_key=True, index=True)
     products: list[Product] = relationship(
         "Product",
--- app/utils/types/amap_types.py
+++ app/utils/types/amap_types.py
@@ -8,3 +8,14 @@ class AmapSlotType(str, Enum):
 
     def __str__(self):
         return f"{self.name}<{self.value}"
+
+
+class DeliveryStatusType(str, Enum):
+
+    creation = "creation"  # Can edit date, add and remove products, no order possible
+    orderable = "orderable"  # Ordering is possible, no edition possible
+    locked = "locked"  # Can't order
+    delivered = "delivered"  # Delivery can be archived
+
+    def __str__(self):
+        return f"{self.name}<{self.value}"
