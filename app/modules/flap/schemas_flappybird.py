2612909023ce909c21ab85322c29349e14f14b67
shaihh <guillaume.tavernier@ec22.ec-lyon.fr>
2023-01-11 15:17:31 +0100

    new simpler schema for flappy bird scores responses

--- app/modules/flap/schemas_flappybird.py
+++ app/modules/flap/schemas_flappybird.py
@@ -20,3 +20,13 @@ class FlappyBirdScoreInDB(FlappyBirdScoreBase):
     id: str
     user: CoreUserSimple
     creation_time: datetime
+
+
+class FlappyBirdScoreFeedback(BaseModel):
+    value: int
+    user: CoreUserSimple
+    creation_time: datetime
+
+    # Required latter to initiate schema using models
+    class Config:
+        orm_mode = True
