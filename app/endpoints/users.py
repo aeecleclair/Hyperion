3c3f7f8ece52e99c58d4f5cfe11bb9fc3b1025c1
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2023-01-15 12:07:36 +0100

    Update regex to support masters and auditors email address

--- app/endpoints/users.py
+++ app/endpoints/users.py
@@ -154,17 +154,17 @@ async def create_user_by_user(
     # Check the account type
 
     # For staff and student
-    # ^[\w\-.]*@(ecl\d{2})|(alternance\d{4})|(auditeur)?.ec-lyon.fr$
+    # ^[\w\-.]*@(ecl\d{2})|(alternance\d{4})|(auditeur)|(master)?.ec-lyon.fr$
     # For staff
-    # ^[\w\-.]*@ec-lyon.fr$
+    # ^[\w\-.]*@(auditeur.)?ec-lyon.fr$
     # For student
-    # ^[\w\-.]*@(ecl\d{2})|(alternance\d{4}).ec-lyon.fr$
+    # ^[\w\-.]*@((ecl\d{2})|(alternance\d{4})|(master)).ec-lyon.fr$
 
-    if re.match(r"^[a-zA-Z0-9_\-.]*@ec-lyon.fr", user_create.email):
+    if re.match(r"^[\w\-.]*@(auditeur.)?ec-lyon.fr", user_create.email):
         # Its a staff email address
         account_type = AccountType.staff
     elif re.match(
-        r"^[\w\-.]*@((ecl\d{2})|(alternance\d{4})|(auditeur)).ec-lyon.fr$",
+        r"^[\w\-.]*@((ecl\d{2})|(alternance\d{4})|(master)).ec-lyon.fr$",
         user_create.email,
     ):
         # Its a student email address
