d743776a705bfd7ea8eafcccf5ccee10506ede33
landacus <ludovic.araye@gmail.com>
2023-02-26 01:33:15 +0100

    Create schemas for Raffle module

---
+++ app/schemas/schemas_raffle.py
@@ -0,0 +1,99 @@
+"""Schemas file for endpoint /amap"""
+
+from datetime import date, datetime
+
+from pydantic import BaseModel
+
+from app.schemas.schemas_core import CoreUserSimple
+
+
+class RaffleBase(BaseModel):
+    """Base schema for raffles"""
+
+    name: str
+    start_date: datetime
+    end_date: datetime
+
+
+class RaffleEdit(BaseModel):
+    name: str | None = None
+    start_date: str
+    end_date: str
+    group_id: str
+    description: str | None = None
+
+
+class RaffleSimple(RaffleBase):
+    group_id: str
+
+
+class RaffleComplete(RaffleSimple):
+    id: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    name: str
+    surname: str
+    email: str
+    nb_tickets_bought: int
+    year: int
+
+
+class UserComplete(UserBase):
+    id: int
+
+
+class WinnerComplete(UserComplete):
+    user: UserComplete
+    lot: str
+
+    class Config:
+        orm_mode = True
+
+
+class LotBase(BaseModel):
+    description: str
+    raffle_name: str
+
+
+class LotEdit(BaseModel):
+    id: str
+    raffle_id: str
+    description: str
+    quantity: int
+
+
+class LotComplete(LotBase):
+    id: str
+
+    class Config:
+        orm_mode = True
+
+
+class TypeTicketBase(BaseModel):
+    price: float
+    nb_ticket: int
+    raffle_id: str
+
+
+class TypeTicketEdit(BaseModel):
+    raffle_id: str
+    price: float
+    nb_ticket: int
+
+
+class TypeTicketComplete(TypeTicketBase):
+    id: str
+
+    class Config:
+        orm_mode = True
+
+
+class TicketEdit(BaseModel):
+    id: str
+    type_id: str
+    user_id: str
+    winning_lot: int
