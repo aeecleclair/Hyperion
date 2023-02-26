b5a72b65ae7438ee18b3e003c03f101b8a7f72d3
Francesco Scopelliti <francesco.scopelliti@ecl22.ec-lyon.fr>
2023-02-26 02:01:55 +0100

    Schema sans classes

--- app/schemas/schemas_phonebook.py
+++ app/schemas/schemas_phonebook.py
@@ -4,37 +4,11 @@ from app.schemas import schemas_core
 from app.utils.types.campaign_type import ListType
 
 
-class RoleBase(BaseModel):
-    """Base schema for Role in association"""
-
-    name: str
-
-
-class RoleComplete(RoleBase):
-    id: str
-
-    class Config:
-        orm_mode = True
-
-
-class AssociationBase(BaseModel):
-    """Base schema for  association"""
-
-    name: str
-
-
-class AssociationComplete(RoleBase):
-    id: str
-
-    class Config:
-        orm_mode = True
-
-
 class RequestUserReturn(BaseModel):
     user: schemas_core.CoreUserSimple
     id: str
-    roles: list[RoleComplete]
-    associations: list[AssociationComplete]
+    roles: list[str]
+    associations: list[str]
 
 
 class RoleReturn(BaseModel):
@@ -43,29 +17,6 @@ class RoleReturn(BaseModel):
 
     class Config:
         orm_mode = True
-
-
-class AssociationMemberBase(BaseModel):
-    user_id: str
-    role: str
-
-    class Config:
-        orm_mode = True
-
-
-class AssociationMemberComplete(AssociationMemberBase):
-    user: schemas_core.CoreUserSimple
-
-    class Config:
-        orm_mode = True
-
-
-class AssociationMembersBase(BaseModel):
-    """Base schema for a list."""
-
-    name: str
-    type: ListType
-    members: list[AssociationMemberBase]
 
 
 class MemberEdit(BaseModel):
