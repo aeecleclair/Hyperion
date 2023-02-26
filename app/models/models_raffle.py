2ecceb73b68c3fafd85bba1333363fdb95a5992f
landacus <ludovic.araye@gmail.com>
2023-02-26 03:35:32 +0100

    Fix nullable issue

--- app/models/models_raffle.py
+++ app/models/models_raffle.py
@@ -8,35 +8,35 @@ from app.database import Base
 
 class Raffle(Base):
     __tablename__ = "raffle"
-    id: str = Column(String, primary_key=True, index=True)
-    name: str = Column(String)
+    id: str = Column(String, primary_key=True, index=True, nullable=False)
+    name: str = Column(String, nullable=False)
     start_date: datetime = Column(DateTime, nullable=False)
     end_date: datetime = Column(DateTime, nullable=False)
-    group_id: str = Column(String, index=True)
-    description: str = Column(String, index=True)
+    group_id: str = Column(String, index=True, nullable=False)
+    description: str = Column(String, index=True, nullable=True)
 
 
 class TypeTicket(Base):
     __tablename__ = "type_ticket"
-    price: float = Column(Float)
-    id: str = Column(String, primary_key=True, index=True)
+    price: float = Column(Float, nullable=False)
+    id: str = Column(String, primary_key=True, index=True, nullable=False)
     nb_ticket: int = Column(Integer)
-    raffle_id: str = Column(ForeignKey("raffle.id"), index=True)
+    raffle_id: str = Column(ForeignKey("raffle.id"), index=True, nullable=False)
 
 
 class Lots(Base):
     __tablename__ = "lots"
 
-    id: str = Column(String, primary_key=True, index=True)
-    raffle_id: str = Column(ForeignKey("raffle.id"), index=True)
-    description: str = Column(String)
+    id: str = Column(String, primary_key=True, index=True, nullable=False)
+    raffle_id: str = Column(ForeignKey("raffle.id"), index=True, nullable=False)
+    description: str = Column(String, nullable=True)
     quantity: int = Column(Integer)
 
 
 class Tickets(Base):
     __tablename__ = "tickets"
 
-    id: str = Column(String, primary_key=True, index=True)
-    type_id: str = Column(ForeignKey("type_ticket.id"))
-    # user_id: string --> Récupérer sur hyperion
+    id: str = Column(String, primary_key=True, index=True, nullable=False)
+    type_id: str = Column(ForeignKey("type_ticket.id"), nullable=False)
+    user_id: str = Column(Integer, nullable=False)  # --> Récupérer sur hyperion
     winning_lot: str = Column(String, nullable=True, index=True)
