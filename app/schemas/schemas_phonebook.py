40a309be21d0c8a4daa00c7c8fde6ee103e8be71
Francesco Scopelliti <francesco.scopelliti@ecl22.ec-lyon.fr>
2023-02-26 02:37:22 +0100

    ajout rolebase assobase

--- app/schemas/schemas_phonebook.py
+++ app/schemas/schemas_phonebook.py
@@ -1,14 +1,44 @@
 from pydantic import BaseModel
 
 from app.schemas import schemas_core
-from app.utils.types.campaign_type import ListType
+
+
+class RoleBase(BaseModel):
+    name: str
+
+
+class RoleComplete(RoleBase):
+    id: str
+
+    class Config:
+        orm_mode = True
+
+
+class AssociationBase(BaseModel):
+    name: str
+
+
+class AssociationComplete(AssociationBase):
+    id: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserReturn(BaseModel):
+    user: schemas_core.CoreUserSimple
+    roles: list[RoleComplete]
+    associations: list[AssociationComplete]
+
+    class Config:
+        orm_mode = True
 
 
 class RequestUserReturn(BaseModel):
-    user: schemas_core.CoreUserSimple
-    id: str
-    roles: list[str]
-    associations: list[str]
+    reponse: list[UserReturn]
+
+    class Config:
+        orm_mode = True
 
 
 class RoleReturn(BaseModel):
