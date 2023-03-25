295412ede008bc5cbbad723115077b8f1e15437e
landacus <ludovic.araye@gmail.com>
2023-03-25 13:41:42 +0100

    Delete nb_ticket column in typetickets

--- app/models/models_raffle.py
+++ app/models/models_raffle.py
@@ -22,7 +22,6 @@ class TypeTicket(Base):
     __tablename__ = "raffle_type_ticket"
     id: str = Column(String, primary_key=True, index=True, nullable=False)
     price: float = Column(Float, nullable=False)
-    nb_ticket: int = Column(Integer)
     value: int = Column(Integer, nullable=False)
     raffle_id: str = Column(ForeignKey("raffle.id"), index=True, nullable=False)
 
