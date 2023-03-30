276038d3591c75faff30f21154ae6f5ca17c0d97
Studio axs <studioo.axs@gmail.com>
2023-03-30 11:35:10 +0200

    cleaning files
    cleaning files to make them easier to read and understand.

--- app/cruds/cruds_phonebook.py
+++ app/cruds/cruds_phonebook.py
@@ -67,6 +67,30 @@ async def update_association(association_id, association, db: AsyncSession):
     """Update an association in database"""
     update(models_phonebook.Association).where(
         association_id == models_phonebook.Association.id
+    ).values(**association.dict(exclude_none=True))
+    try:
+        await db.commit()
+    except IntegrityError:
+        await db.rollback()
+        raise
+
+
+async def create_role(role: schemas_phonebook.RoleComplete, db: AsyncSession):
+    """Create a role in database"""
+    db.add(role)
+    try:
+        await db.commit()
+    except IntegrityError:
+        await db.rollback()
+        raise
+
+
+async def update_role(
+    role_id: str, role: schemas_phonebook.RoleComplete, db: AsyncSession
+):
+    """Update a role in database"""
+    update(models_phonebook.Role).where(role_id == models_phonebook.Role.id).values(
+        **role.dict(exclude_none=True)
     )
     try:
         await db.commit()
@@ -75,11 +99,30 @@ async def update_association(association_id, association, db: AsyncSession):
         raise
 
 
-async def create_role(role: schemas_phonebook.Role, db: AsyncSession):
-    """Create a role in database"""
-    db.add(role)
+async def delete_role(role_id: str, db: AsyncSession):
+    """Delete a role in database"""
+    delete(models_phonebook.Role).where(role_id == models_phonebook.Role.id)
     try:
         await db.commit()
     except IntegrityError:
         await db.rollback()
         raise
+
+
+async def get_roles(db: AsyncSession) -> list[models_phonebook.Role]:
+    """Return all roles from database"""
+    result = await db.execute(select(models_phonebook.Role))
+    return result.scalars().all()
+
+
+async def get_roles_by_query(db: AsyncSession) -> list[models_phonebook.Post]:
+    """Return all roles from database"""
+    result = await db.execute(select(models_phonebook.Role))
+    return result.scalars().all()
+
+
+async def get_all_memberships(db: AsyncSession) -> list[models_phonebook.Membership]:
+    """Return all memberships from database"""
+
+    result = await db.execute(select(models_phonebook.Membership))
+    return result.scalars().all()
