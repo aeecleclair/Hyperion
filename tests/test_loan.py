c348745130cb434ac7c2b6a76bc64c70636f549e
Shaihh <guillaume.tavernier@ecl22.ec-lyon.fr>
2023-04-08 16:10:33 +0200

    Add every loan unit tests

---
+++ tests/test_loan.py
@@ -0,0 +1,283 @@
+import datetime
+import uuid
+from datetime import timedelta
+
+from sqlalchemy import update
+
+from app.main import app
+from app.models import models_core, models_loan
+from app.utils.types.groups_type import GroupType
+from tests.commons import (
+    TestingSessionLocal,
+    client,
+    create_api_access_token,
+    create_user_with_groups,
+)
+
+admin_user: models_core.CoreUser | None = None
+loan_user_loaner: models_core.CoreUser | None = None
+loan_user_simple: models_core.CoreUser | None = None
+loaner: models_loan.Loaner | None = None
+loaner_to_delete: models_loan.Loaner | None = None
+loan: models_loan.Loan | None = None
+item: models_loan.Item | None = None
+item_to_delete: models_loan.Item | None = None
+token_loaner: str = ""
+token_simple: str = ""
+token_admin: str = ""
+
+
+@app.on_event("startup")  # create the datas needed in the tests
+async def startuptest():
+    global admin_user
+    global loan_user_loaner
+    global loaner
+    global loan_user_simple
+    global loaner_to_delete
+    global loan
+    global item
+    global item_to_delete
+    async with TestingSessionLocal() as db:
+        admin_user = await create_user_with_groups([GroupType.admin], db=db)
+
+        loan_user_loaner = await create_user_with_groups([GroupType.CAA], db=db)
+        loaner = models_loan.Loaner(
+            id=str(uuid.uuid4()),
+            name="CAA",
+            group_manager_id="6c6d7e88-fdb8-4e42-b2b5-3d3cfd12e7d6",
+        )
+        db.add(loaner)
+
+        loan_user_simple = await create_user_with_groups([GroupType.amap], db=db)
+
+        loaner_to_delete = models_loan.Loaner(
+            id=str(uuid.uuid4()),
+            name="cinema",
+            group_manager_id="ce5f36e6-5377-489f-9696-de70e2477300",
+        )
+        db.add(loaner_to_delete)
+
+        item = models_loan.Item(
+            id=str(uuid.uuid4()),
+            name="Test Item",
+            loaner_id=loaner.id,
+            suggested_lending_duration=timedelta(
+                days=50,
+            ),
+            suggested_caution=10,
+            total_amount=8,
+            loaned_amount=0,
+            loaner=loaner,
+        )
+        db.add(item)
+
+        item_to_delete = models_loan.Item(
+            id=str(uuid.uuid4()),
+            name="Test Item To Delete",
+            loaner_id=loaner.id,
+            suggested_lending_duration=timedelta(
+                days=50,
+            ),
+            suggested_caution=10,
+            total_amount=5,
+            loaned_amount=1,
+            loaner=loaner,
+        )
+        db.add(item_to_delete)
+        loan = models_loan.Loan(
+            id=str(uuid.uuid4()),
+            borrower_id=loan_user_simple.id,
+            loaner_id=loaner.id,
+            start=datetime.date(year=2023, month=3, day=29),
+            end=datetime.date(year=2023, month=4, day=26),
+            caution="Carte etudiante",
+            returned=False,
+            items=[item],
+        )
+        db.add(loan)
+        loan_content = models_loan.LoanContent(
+            loan_id=loan.id,
+            item_id=item.id,
+            amount=1,
+        )
+
+        await db.execute(
+            update(models_loan.LoanContent)
+            .where(
+                models_loan.LoanContent.loan_id == loan_content.loan_id
+                and models_loan.LoanContent.item_id == loan_content.item_id
+            )
+            .values({"amount": loan_content.amount})
+        )
+
+        await db.commit()
+
+    global token_admin
+    token_admin = create_api_access_token(admin_user)
+
+    global token_loaner
+    token_loaner = create_api_access_token(loan_user_loaner)
+
+    global token_simple
+    token_simple = create_api_access_token(loan_user_simple)
+
+
+def test_get_loaners():
+    response = client.get(
+        "/loans/loaners/",
+        headers={"Authorization": f"Bearer {token_admin}"},
+    )
+    assert response.status_code == 200
+
+
+def test_create_loaners():
+    response = client.post(
+        "/loans/loaners/",
+        json={
+            "name": "BDE",
+            "group_manager_id": "ce5f36e6-5377-489f-9696-de70e2477300",
+        },
+        headers={"Authorization": f"Bearer {token_admin}"},
+    )
+    assert response.status_code == 201
+
+
+def test_update_loaners():
+    response = client.patch(
+        f"/loans/loaners/{loaner_to_delete.id}",
+        json={
+            "name": "AE",
+            "group_manager_id": "45649735-866a-49df-b04b-a13c74fd5886",
+        },
+        headers={"Authorization": f"Bearer {token_admin}"},
+    )
+    assert response.status_code == 204
+
+
+def test_delete_loaners():
+    response = client.delete(
+        f"/loans/loaners/{loaner_to_delete.id}",
+        headers={"Authorization": f"Bearer {token_admin}"},
+    )
+    assert response.status_code == 204
+
+
+def test_get_loans_by_loaner():
+    response = client.get(
+        f"/loans/loaners/{loaner.id}/loans",
+        headers={"Authorization": f"Bearer {token_loaner}"},
+    )
+    assert response.status_code == 200
+
+
+def test_create_items_for_loaner():
+    response = client.post(
+        f"/loans/loaners/{loaner.id}/items",
+        json={
+            "name": "TestItem",
+            "suggested_caution": 100,
+            "total_amount": 4,
+            "suggested_lending_duration": str(timedelta(days=10)),
+        },
+        headers={"Authorization": f"Bearer {token_loaner}"},
+    )
+    assert response.status_code == 201
+
+
+def test_update_items_for_loaner():
+    response = client.patch(
+        f"/loans/loaners/{loaner.id}/items/{item.id}",
+        json={
+            "name": "TestItem",
+            "suggested_caution": 100,
+            "total_amount": 7,
+            "loaned_amount": 1,
+            "suggested_lending_duration": str(timedelta(days=10)),
+        },
+        headers={"Authorization": f"Bearer {token_loaner}"},
+    )
+    assert response.status_code == 204
+
+
+def test_delete_loaner_item():
+    response = client.delete(
+        f"/loans/loaners/{loaner.id}/items/{item_to_delete.id}",
+        headers={"Authorization": f"Bearer {token_loaner}"},
+    )
+    assert response.status_code == 204
+
+
+def test_get_current_user_loans():
+    response = client.get(
+        "/loans/users/me",
+        headers={"Authorization": f"Bearer {token_simple}"},
+    )
+    assert response.status_code == 200
+
+
+def test_get_current_user_loaners():
+    response = client.get(
+        "/loans/users/me/loaners",
+        headers={"Authorization": f"Bearer {token_simple}"},
+    )
+    assert response.status_code == 200
+
+
+def test_create_loan():
+    response = client.post(
+        "/loans/",
+        json={
+            "borrower_id": loan_user_simple.id,
+            "loaner_id": loaner.id,
+            "start": "2022-10-23",
+            "end": "2022-10-23",
+            "notes": "Je pense que cela va fonctionner à merveille :)",
+            "caution": "20€",
+            "items_borrowed": [
+                {
+                    "item_id": item.id,
+                    "amount_borrowed": 2,
+                },
+            ],
+        },
+        headers={"Authorization": f"Bearer {token_loaner}"},
+    )
+    assert response.status_code == 201
+
+
+def test_update_loan():
+    response = client.patch(
+        f"/loans/{loan.id}",
+        json={
+            "borrower_id": loan_user_simple.id,
+            "start": "2022-10-23",
+            "end": "2022-10-23",
+            "notes": "Je pense que cela va fonctionner à merveille :)",
+            "caution": "20€",
+            "returned": True,
+            "items_borrowed": [
+                {
+                    "item_id": item.id,
+                    "amount_borrowed": 2,
+                },
+            ],
+        },
+        headers={"Authorization": f"Bearer {token_loaner}"},
+    )
+    assert response.status_code == 204
+
+
+def test_return_loan():
+    response = client.post(
+        f"/loans/{loan.id}/return",
+        headers={"Authorization": f"Bearer {token_loaner}"},
+    )
+    assert response.status_code == 204
+
+
+def test_delete_loan():
+    response = client.delete(
+        f"/loans/{loan.id}",
+        headers={"Authorization": f"Bearer {token_loaner}"},
+    )
+    assert response.status_code == 204
