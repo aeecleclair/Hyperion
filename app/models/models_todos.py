7b21e858112025af433f5171fad7b1a9abde45af
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2023-04-11 22:28:36 +0200

    Use a foreign key for todo's user_id

--- app/models/models_todos.py
+++ app/models/models_todos.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from sqlalchemy import Boolean, Column, Date, String
+from sqlalchemy import Boolean, Column, Date, ForeignKey, String
 
 from app.database import Base
 
@@ -9,7 +9,7 @@ class TodosItem(Base):
     __tablename__ = "todos_item"
 
     id: str = Column(String, primary_key=True, index=True)
-    user_id: str = Column(String, index=True, nullable=False)
+    user_id: str = Column(ForeignKey("core_user.id"), nullable=False, index=True)
     name: str = Column(String, nullable=False)
     creation: date = Column(Date, nullable=False)
     deadline: date | None = Column(Date)
