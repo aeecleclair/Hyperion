8786bc32f25e0e1b858191a6d303918946af3872
Shaîhh <guillaume.tavernier@ecl22.ec-lyon.fr>
2023-08-20 11:50:29 +0200

    Complete ModuleList

--- app/utils/types/module_list.py
+++ app/utils/types/module_list.py
@@ -5,35 +5,43 @@ from app.utils.types.module import Module
 
 
 class ModuleList(Module, Enum):
+    advert = Module(
+        root="advert",
+        default_allowed_groups_ids=[GroupType.student, GroupType.staff],
+    )
     admin = Module(
         root="admin",
         default_allowed_groups_ids=[GroupType.admin],
     )
     amap = Module(
         root="amap",
-        default_allowed_groups_ids=[GroupType.student, GroupType.admin],
+        default_allowed_groups_ids=[GroupType.student, GroupType.staff],
     )
     bdebooking = Module(
         root="booking",
-        default_allowed_groups_ids=[GroupType.admin, GroupType.student],
+        default_allowed_groups_ids=[GroupType.student, GroupType.staff],
     )
-    calendar = Module(
-        root="calendar",
-        default_allowed_groups_ids=[GroupType.student],
-    )
-    campaign = Module(
-        root="campaign",
-        default_allowed_groups_ids=[],
+    centralisation = Module(
+        root="centralisation",
+        default_allowed_groups_ids=[GroupType.student, GroupType.staff],
     )
     cinema = Module(
         root="cinema",
-        default_allowed_groups_ids=[GroupType.admin],
+        default_allowed_groups_ids=[GroupType.student, GroupType.staff],
+    )
+    calendar = Module(
+        root="event",
+        default_allowed_groups_ids=[GroupType.student, GroupType.staff],
     )
     loan = Module(
         root="loan",
-        default_allowed_groups_ids=[GroupType.admin],
+        default_allowed_groups_ids=[GroupType.student, GroupType.staff],
     )
     raffle = Module(
         root="tombola",
-        default_allowed_groups_ids=[],
+        default_allowed_groups_ids=[GroupType.student, GroupType.staff],
+    )
+    campaign = Module(
+        root="vote",
+        default_allowed_groups_ids=[GroupType.AE],
     )
