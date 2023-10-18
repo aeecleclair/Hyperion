d65c28f192d6729c2dac9aa29f2425fd46e1e2b6
Brzuszek Maël <mael.brzuszek@ecl21.ec-lyon.fr>
2023-10-18 11:09:53 +0200

    Remove unused file and __str__ definition

--- app/utils/types/raffle_types.py
+++ app/utils/types/raffle_types.py
@@ -5,6 +5,3 @@ class RaffleStatusType(str, Enum):
     creation = "creation"  # Can edit every parameter
     open = "open"  # Ordering is possible, no edition possible
     lock = "lock"  # Can't order
-
-    def __str__(self):
-        return f"{self.name}<{self.value}"
