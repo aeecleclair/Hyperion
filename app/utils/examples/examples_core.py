e8fc165f18485320ef857f39ec58e1b327b68f50
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2023-11-06 15:19:06 +0100

    Remove "example" from example dict

--- app/utils/examples/examples_core.py
+++ app/utils/examples/examples_core.py
@@ -1,36 +1,28 @@
-example_CoreUserUpdate = {
-    "example": {
-        "name": "Backend",
-        "firstname": "MyECL",
-        "nickname": "Hyperion",
-        "birthday": "2022-05-04",
-        "promo": 2021,
-        "floor": "Adoma",
-    }
+example_CoreUserUpdate: dict[str, object] = {
+    "name": "Backend",
+    "firstname": "MyECL",
+    "nickname": "Hyperion",
+    "birthday": "2022-05-04",
+    "promo": 2021,
+    "floor": "Adoma",
 }
 
-example_CoreUserCreateRequest = {
-    "example": {
-        "email": "user@example.fr",
-    }
+example_CoreUserCreateRequest: dict[str, object] = {
+    "email": "user@example.fr",
 }
 
 
-example_CoreBatchUserCreateRequest = {
-    "example": {
-        "email": "user@example.fr",
-        "account_type": "39691052-2ae5-4e12-99d0-7a9f5f2b0136",
-    }
+example_CoreBatchUserCreateRequest: dict[str, object] = {
+    "email": "user@example.fr",
+    "account_type": "39691052-2ae5-4e12-99d0-7a9f5f2b0136",
 }
 
 
-example_CoreUserActivateRequest = {
-    "example": {
-        "name": "Name",
-        "firstname": "Firstname",
-        "nickname": "Antoine",
-        "activation_token": "62D-QJI5IYrjuywH8IWnuBo0xHrbTCfw_18HP4mdRrA",
-        "password": "areallycomplexpassword",
-        "floor": "Autre",
-    }
+example_CoreUserActivateRequest: dict[str, object] = {
+    "name": "Name",
+    "firstname": "Firstname",
+    "nickname": "Antoine",
+    "activation_token": "62D-QJI5IYrjuywH8IWnuBo0xHrbTCfw_18HP4mdRrA",
+    "password": "areallycomplexpassword",
+    "floor": "Autre",
 }
