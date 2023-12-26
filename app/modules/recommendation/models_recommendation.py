7472b5693a8d54ce18b00d9c65de4c8c64551cf7
Julien <120588494+julien4215@users.noreply.github.com>
2023-12-26 19:16:25 +0100

    fix creation type

--- app/modules/recommendation/models_recommendation.py
+++ app/modules/recommendation/models_recommendation.py
@@ -1,4 +1,6 @@
-from sqlalchemy import String
+from datetime import datetime
+
+from sqlalchemy import DateTime, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.database import Base
@@ -8,7 +10,7 @@ class Recommendation(Base):
     __tablename__ = "recommendation"
 
     id: Mapped[str] = mapped_column(String, primary_key=True, nullable=False)
-    creation: Mapped[str] = mapped_column(String, nullable=False)
+    creation: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
     title: Mapped[str] = mapped_column(String, nullable=False)
     code: Mapped[str] = mapped_column(String, nullable=False)
     summary: Mapped[str] = mapped_column(String, nullable=False)
