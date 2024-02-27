7ae5fa5883456b908405b0b3d0878ab4c925a263
Shaîhh <guillaume.tavernier@etu.ec-lyon.fr>
2024-02-27 10:20:04 +0100

    fix: primary keys on Membership

--- app/models/models_phonebook.py
+++ app/models/models_phonebook.py
@@ -11,16 +11,14 @@ from app.database import Base
 class Membership(Base):
     __tablename__ = "phonebook_membership"
 
-    id: Mapped[str] = mapped_column(
-        String, primary_key=True, index=True, nullable=False
-    )
+    id: Mapped[str] = mapped_column(String, index=True, nullable=False)
     user_id: Mapped[str] = mapped_column(
         String, ForeignKey("core_user.id"), primary_key=True
     )
     association_id: Mapped[str] = mapped_column(
         String, ForeignKey("phonebook_association.id"), primary_key=True
     )
-    mandate_year: Mapped[int] = mapped_column(Integer, nullable=False)
+    mandate_year: Mapped[int] = mapped_column(Integer, primary_key=True)
     role_name: Mapped[str] = mapped_column(String, nullable=False)
     role_tags: Mapped[str] = mapped_column(String, nullable=False)
 
