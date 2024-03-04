b9f205cc53833eb9bb394fce79a72a21c5f678ff
Julien <120588494+julien4215@users.noreply.github.com>
2024-03-04 12:37:44 +0100

    apply new ruff rules

--- migrations/versions/5-loan_return_date.py
+++ migrations/versions/5-loan_return_date.py
@@ -9,8 +9,8 @@ Create Date: 2024-03-01 23:33:20.431056
 from datetime import datetime, timezone
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "2fcadbe2f0ad"
