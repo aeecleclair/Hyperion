59f98616fad714dda3107a47f553cc5ee7913786
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-03-16 02:54:11 +0100

    Raise a ValueError when trying to save a file whose name is not an uuid

--- app/utils/tools.py
+++ app/utils/tools.py
@@ -1,12 +1,9 @@
 import logging
 import os
-import random
 import re
 import secrets
-import shutil
 from collections.abc import Sequence
 from pathlib import Path
-from typing import Sequence
 
 import aiofiles
 from fastapi import HTTPException, UploadFile
@@ -21,6 +18,10 @@ from app.core.models_core import CoreUser
 from app.core.users import cruds_users
 
 hyperion_error_logger = logging.getLogger("hyperion.error")
+
+uuid_regex = re.compile(
+    r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"
+)
 
 
 def is_user_member_of_an_allowed_group(
@@ -111,6 +112,7 @@ async def save_file_as_data(
     - Maximum size is 2MB by default, it can be changed using `max_file_size` (in bytes) parameter.
     - `accepted_content_types` is a list of accepted content types. By default, only images are accepted.
         Use: `["image/jpeg", "image/png", "image/webp"]` to accept only images.
+    - Filename should be an uuid.
 
     The file extension will be inferred from the provided content file.
     There should only be one file with the same filename, thus, saving a new file will remove the existing even if its extension was different.
@@ -130,6 +132,12 @@ async def save_file_as_data(
             "image/png",
             "image/webp",
         ]
+
+    if not uuid_regex.match(filename):
+        hyperion_error_logger.error(
+            f"save_file_as_data: security issue, the filename is not a valid UUID: {filename}."
+        )
+        raise ValueError("The filename is not a valid UUID")
 
     if image.content_type not in accepted_content_types:
         raise HTTPException(
@@ -192,10 +200,7 @@ def get_file_from_data(
 
     WARNING: **NEVER** trust user input when calling this function. Always check that parameters are valid.
     """
-
-    if not re.match(
-        r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", filename
-    ):
+    if not uuid_regex.match(filename):
         hyperion_error_logger.error(
             f"get_file_from_data: security issue, the filename is not a valid UUID: {filename}. This mean that the user input was not properly checked."
         )
