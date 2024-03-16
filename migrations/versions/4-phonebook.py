7f20abcadd8deac2a987db4054e9b4e4282a95f4
Julien <120588494+julien4215@users.noreply.github.com>
2024-03-16 03:25:45 +0100

    change migration order

--- migrations/versions/4-phonebook.py
+++ migrations/versions/4-phonebook.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "6afc765adaa2"
-down_revision: Union[str, None] = "f17e6182b0a9"
+down_revision: Union[str, None] = "99a2c70e4a24"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
