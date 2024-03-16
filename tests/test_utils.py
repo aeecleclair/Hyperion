a02b300f7daa4b3ccd9db2cdb7fe3557b51e850d
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-03-16 03:28:20 +0100

    Add tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,32 @@
 import uuid
 
 import pytest
+from fastapi import UploadFile
 
-from app.utils.tools import get_file_from_data
+from app.utils.tools import get_file_from_data, save_file_as_data
+
+
+async def test_save_file():
+    valid_uuid = str(uuid.uuid4())
+    with open("assets/images/default_profile_picture.png", "rb") as file:
+        await save_file_as_data(
+            image=UploadFile(file, headers={"content-type": "image/png"}),
+            directory="test",
+            filename=valid_uuid,
+            request_id="request_id",
+        )
+
+
+async def test_save_file_raise_a_value_error_if_filename_isnt_an_uuid():
+    not_a_uuid = "not_a_uuid"
+    with pytest.raises(ValueError):
+        with open("assets/images/default_profile_picture.png", "rb") as file:
+            await save_file_as_data(
+                image=UploadFile(file),
+                directory="test",
+                filename=not_a_uuid,
+                request_id="request_id",
+            )
 
 
 def test_get_file_with_valid_uuid():
