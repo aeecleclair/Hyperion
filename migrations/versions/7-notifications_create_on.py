b0d6aaf68ca096657106e3a15560c17d99aa8578
Petitoto <Petitoto@users.noreply.github.com>
2024-03-17 19:47:24 +0100

    Fix alembic tree

--- migrations/versions/7-notifications_create_on.py
+++ migrations/versions/7-notifications_create_on.py
@@ -1,7 +1,7 @@
 """Notifications 'create_on' migration
 
 Revision ID: e3d06397960d
-Revises: 2fcadbe2f0ad
+Revises: 6afc765adaa2
 Create Date: 2024-03-17 19:19:14.297195
 
 """
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "e3d06397960d"
-down_revision: str | None = "2fcadbe2f0ad"
+down_revision: str | None = "6afc765adaa2"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
