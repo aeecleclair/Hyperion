9ee2ed53e9ac597e25d068a9f0625cc273628cf4
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2024-03-17 21:17:02 +0100

    Fix migrations

--- migrations/versions/5-elocaps.py
+++ migrations/versions/5-elocaps.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "21241f2aac83"
-down_revision: str | None = "99a2c70e4a24"
+down_revision: str | None = "3f9843f165e9"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
--- migrations/versions/6-loan_return_date.py
+++ migrations/versions/6-loan_return_date.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "2fcadbe2f0ad"
-down_revision: str | None = "3f9843f165e9"
+down_revision: str | None = "21241f2aac83"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
