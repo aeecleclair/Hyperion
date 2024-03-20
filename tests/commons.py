eb42302c6b9e0ee5b6140e3bd48f68355b8cebbb
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-03-20 23:36:51 +0100

    Add `warn_unused_ignores`

--- tests/commons.py
+++ tests/commons.py
@@ -28,7 +28,7 @@ from app.utils.types.floors_type import FloorsType
 @lru_cache
 def override_get_settings() -> Settings:
     """Override the get_settings function to use the testing session"""
-    return Settings(_env_file=".env.test", _env_file_encoding="utf-8")  # type: ignore[call-arg] # See https://github.com/pydantic/pydantic/issues/3072, TODO: remove when fixes
+    return Settings(_env_file=".env.test", _env_file_encoding="utf-8")
 
 
 settings = override_get_settings()
