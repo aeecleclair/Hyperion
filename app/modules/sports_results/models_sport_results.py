144f12d913037af36e06aafded22c7838456278e
BLEMENT33 <150018300+BLEMENT33@users.noreply.github.com>
2024-04-01 19:43:14 +0200

    add crud

--- app/modules/sports_results/models_sport_results.py
+++ app/modules/sports_results/models_sport_results.py
@@ -6,11 +6,21 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base
 
 
+class CaptainMembership(Base):
+    __tablename__ = "sport-team-captain-membership"
+
+    user_id: Mapped[str] = mapped_column(ForeignKey("core_user.id"), primary_key=True)
+    sport_id: Mapped[str] = mapped_column(String)
+
+
 class Captain(Base):
     __tablename__ = "sport-team-captains"
 
     user_id: Mapped[str] = mapped_column(ForeignKey("core_user.id"), primary_key=True)
-    sport: Mapped["Sport"] = relationship("Sport", back_populates="captain")
+    sport: Mapped["Sport"] = relationship(
+        "Sport",
+        back_populates="captain",
+    )
 
 
 class Sport(Base):
