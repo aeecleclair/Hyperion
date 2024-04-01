abdf869e93a11081a3dc2a02e38c791dcbeba638
BLEMENT33 <150018300+BLEMENT33@users.noreply.github.com>
2024-04-01 20:53:26 +0200

    endpoint

--- app/modules/sports_results/schemas_sport_results.py
+++ app/modules/sports_results/schemas_sport_results.py
@@ -1,23 +1,26 @@
 from datetime import date
 
-from app.database import Base
+from pydantic import BaseModel
 
 
-class Captain(Base):
+class Captain(BaseModel):
     user_id: str
     sport: "Sport"
 
 
-class Sport(Base):
+class Sport(BaseModel):
     id: str
     name: str
     capitains: list[Captain]
 
 
-class Result(Base):
-    id: str
+class ResultBase(BaseModel):
     sport_id: str
     score1: int
     score2: int
     rank: int
     match_date: date
+
+
+class ResultComplete(ResultBase):
+    id: str
