9c8eb78f403bc7d183f504a6589a664aa856cc10
Julien <120588494+julien4215@users.noreply.github.com>
2024-04-21 13:57:14 +0200

    configuration to use annotated declarative table

--- app/database.py
+++ app/database.py
@@ -1,7 +1,27 @@
 """File defining the Metadata."""
 
-from sqlalchemy.orm import DeclarativeBase
+import datetime
+import uuid
+from typing import Annotated
+
+from sqlalchemy import types
+from sqlalchemy.orm import DeclarativeBase, mapped_column
+
+from app.types.sqlalchemy import TZDateTime
+
+# Pre-configured field type for UUID primary key (see https://docs.sqlalchemy.org/en/20/orm/declarative_tables.html#mapping-whole-column-declarations-to-python-types)
+pk = Annotated[uuid.UUID, mapped_column(primary_key=True)]
 
 
 class Base(DeclarativeBase):
-    pass
+    """Base class for all models.
+
+    The type map is overriden to use our custom datetime type (see https://docs.sqlalchemy.org/en/20/orm/declarative_tables.html#customizing-the-type-map)"""
+
+    type_annotation_map = {
+        bool: types.Boolean(),
+        datetime.date: types.Date(),
+        datetime.datetime: TZDateTime(),
+        str: types.String(),
+        uuid.UUID: types.Uuid(),
+    }
