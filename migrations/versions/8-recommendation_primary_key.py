030a99bf6281c50d5d8a42f34662e0dc69ce2673
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-04-27 11:24:39 +0200

    Add tests for 8-recommendation_primary_key

--- migrations/versions/8-recommendation_primary_key.py
+++ migrations/versions/8-recommendation_primary_key.py
@@ -4,9 +4,14 @@ Create Date: 2024-04-21 02:08:19.548067
 """
 
 from collections.abc import Sequence
+from datetime import UTC, datetime
+from typing import TYPE_CHECKING
 
 import sqlalchemy as sa
 from alembic import op
+
+if TYPE_CHECKING:
+    from pytest_alembic import MigrationContext
 
 # revision identifiers, used by Alembic.
 revision: str = "d99516f0bbcb"
@@ -30,3 +35,30 @@ def downgrade() -> None:
             "id",
             type_=sa.types.String(),
         )
+
+
+def pre_test_upgrade(
+    alembic_runner: "MigrationContext",
+    alembic_connection: sa.Connection,
+) -> None:
+    alembic_runner.insert_into(
+        "recommendation",
+        {
+            "id": "66c363bc-c71f-4eae-8376-c37712a312f6",
+            "creation": datetime.now(UTC),
+            "title": "title",
+            "summary": "summary",
+            "description": "description",
+        },
+    )
+
+
+def test_upgrade(
+    alembic_runner: "MigrationContext",
+    alembic_connection: sa.Connection,
+) -> None:
+    rows = alembic_connection.execute(
+        sa.text("SELECT id from recommendation")
+    ).fetchall()
+
+    assert ("66c363bc-c71f-4eae-8376-c37712a312f6",) in rows
