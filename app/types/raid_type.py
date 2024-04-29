10fa37e615f3c21647be243278345c8b08a6d86d
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-04-29 16:02:14 +0200

    Fix bugs

--- app/types/raid_type.py
+++ app/types/raid_type.py
@@ -3,12 +3,14 @@ from enum import Enum
 
 class DocumentType(str, Enum):
     idCard = "idCard"  # the id card of the participant
-    medicalCertificate = "medicalCertificate"  # the medical certificate of the participant
+    medicalCertificate = (
+        "medicalCertificate"  # the medical certificate of the participant
+    )
     studentCard = "studentCard"  # the student card of the participant
     raidRules = "raidRules"  # the rules of the raid
 
 
-class Size(str, Enum): # for the T-shirt and the bike
+class Size(str, Enum):  # for the T-shirt and the bike
     XS = "XS"
     S = "S"
     M = "M"
@@ -16,19 +18,19 @@ class Size(str, Enum): # for the T-shirt and the bike
     XL = "XL"
 
 
-class MeetingPlace(str, Enum): # place of meeting for the raid
+class MeetingPlace(str, Enum):  # place of meeting for the raid
     centrale = "centrale"
     bellecour = "bellecour"
     anyway = "anyway"
 
 
-class Difficulty(str, Enum): # the difficulty of the raid
+class Difficulty(str, Enum):  # the difficulty of the raid
     discovery = "discovery"
     sports = "sports"
     expert = "expert"
 
 
-class Situation(str, Enum): # the situation of the participant
+class Situation(str, Enum):  # the situation of the participant
     centrale = "centrale"
     otherSchool = "otherSchool"
     corporatePartner = "corporatePartner"
