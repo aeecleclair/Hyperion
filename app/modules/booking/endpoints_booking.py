a9d076cc97ccb3ea7e71374f21794044fdabcae7
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2024-05-01 10:46:39 +0200

    Finish merging #353

--- app/modules/booking/endpoints_booking.py
+++ app/modules/booking/endpoints_booking.py
@@ -6,9 +6,10 @@ from fastapi import Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core import models_core
+from app.core.config import Settings
+from app.core.groups import cruds_groups
 from app.core.groups.groups_type import GroupType
 from app.core.module import Module
-from app.core.notification.notification_types import CustomTopic, Topic
 from app.core.notification.schemas_notification import Message
 from app.dependencies import (
     get_db,
@@ -265,21 +266,26 @@ async def create_booking(
     )
     await cruds_booking.create_booking(booking=db_booking, db=db)
     result = await cruds_booking.get_booking_by_id(db=db, booking_id=db_booking.id)
-
     try:
-        now = datetime.now(UTC)
-        message = Message(
-            context=f"new-booking-{id}",
-            is_visible=True,
-            title="📅 Réservations - Nouvelle réservation ",
-            content=f"{result.applicant.nickname} - {result.room.name} {result.start.strftime('%m/%d/%Y, %H:%M')} - {result.reason}",
-            # The notification will expire in 3 days
-            expire_on=now.replace(day=now.day + 3),
+        manager_group_id = result.room.manager.group_id
+        manager_group = await cruds_groups.get_group_by_id(
+            db=db,
+            group_id=manager_group_id,
         )
-        await notification_tool.send_notification_to_topic(
-            custom_topic=CustomTopic(Topic.bookingadmin),
-            message=message,
-        )
+        if result and manager_group:
+            now = datetime.now(UTC)
+            message = Message(
+                context=f"new-booking-{id}",
+                is_visible=True,
+                title="📅 Réservations - Nouvelle réservation ",
+                content=f"{result.applicant.nickname} - {result.room.name} {result.start.strftime('%m/%d/%Y, %H:%M')} - {result.reason}",
+                # The notification will expire in 3 days
+                expire_on=now.replace(day=now.day + 3),
+            )
+            await notification_tool.send_notification_to_users(
+                user_ids=[user.id for user in manager_group.members],
+                message=message,
+            )
 
     except Exception as error:
         hyperion_error_logger.error(
@@ -325,7 +331,7 @@ async def edit_booking(
             db=db,
         )
     except ValueError as error:
-        raise HTTPException(status_code=422, detail=str(error))
+        raise HTTPException(status_code=400, detail=str(error))
 
 
 @module.router.patch(
