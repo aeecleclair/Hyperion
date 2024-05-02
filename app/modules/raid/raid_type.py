a72190f87fa430e925b4ab86f8db59cd581fe467
Maxime Roucher <maximeroucher@gmail.com>
2024-05-02 13:44:27 +0200

    feat: adding parent authorization

--- app/modules/raid/raid_type.py
+++ app/modules/raid/raid_type.py
@@ -2,12 +2,13 @@ from enum import Enum
 
 
 class DocumentType(str, Enum):
-    idCard = "idCard"  # the id card of the participant
-    medicalCertificate = (
+    id_card = "idCard"  # the id card of the participant
+    medical_certificate = (
         "medicalCertificate"  # the medical certificate of the participant
     )
-    studentCard = "studentCard"  # the student card of the participant
-    raidRules = "raidRules"  # the rules of the raid
+    student_card = "studentCard"  # the student card of the participant
+    raid_rules = "raidRules"  # the rules of the raid
+    parent_authorization = "parentAuthorization"  # the parent authorization
 
 
 class Size(str, Enum):  # for the T-shirt and the bike
@@ -32,8 +33,8 @@ class Difficulty(str, Enum):  # the difficulty of the raid
 
 class Situation(str, Enum):  # the situation of the participant
     centrale = "centrale"
-    otherSchool = "otherSchool"
-    corporatePartner = "corporatePartner"
+    other_school = "otherSchool"
+    corporate_partner = "corporatePartner"
     other = "other"
 
 
