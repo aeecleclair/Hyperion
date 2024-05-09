353a73a22b4aff797cfab698bd99c39bb511cfac
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2024-05-09 19:23:34 +0200

    fix : test and config deprecated

--- app/modules/flap/schemas_flappybird.py
+++ app/modules/flap/schemas_flappybird.py
@@ -8,10 +8,6 @@ from app.core.schemas_core import CoreUserSimple
 class FlappyBirdScoreBase(BaseModel):
     value: int
 
-    # Required latter to initiate schema using models
-    class Config:
-        orm_mode = True
-
 
 class FlappyBirdScoreInDB(FlappyBirdScoreBase):
     id: str
@@ -24,10 +20,6 @@ class FlappyBirdScoreFeedback(BaseModel):
     value: int
     user: CoreUserSimple
     creation_time: datetime
-
-    # Required latter to initiate schema using models
-    class Config:
-        orm_mode = True
 
 
 class FlappyBirdScoreCompleteFeedBack(FlappyBirdScoreFeedback):
--- tests/test_flappybird.py
+++ tests/test_flappybird.py
@@ -1,78 +1,44 @@
 from datetime import UTC, datetime
 
+import pytest_asyncio
+
 from app.core import models_core
 from app.core.groups.groups_type import GroupType
-from app.main import app
 from app.modules.flap import models_flappybird
 from tests.commons import (
-    TestingSessionLocal,
+    add_object_to_db,
     client,
     create_api_access_token,
     create_user_with_groups,
+    event_loop,  # noqa
 )
 
-user_id = "2e10a287-5e5d-4151-bfa5-bcfffa325433"
+flappybird_score: models_flappybird.FlappyBirdScore | None = None
 user: models_core.CoreUser | None = None
+token: str = ""
 
 
-@app.on_event("startup")  # create the datas needed in the tests
-async def startuptest():
+@pytest_asyncio.fixture(scope="module", autouse=True)
+async def init_objects():
     global user
-    async with TestingSessionLocal() as db:
-        # We add a todo item to be able to try the endpoint
-        user = await create_user_with_groups([GroupType.student], db=db)
+    user = await create_user_with_groups([GroupType.student])
 
-        flappybird_score = models_flappybird.FlappyBirdScore(
-            id="0b7dc7bf-0ab4-421a-bbe7-7ec064fcec8d",
-            user_id=user.id,
-            user=user,
-            value=25,
-            creation_time=datetime.now(UTC),
-        )
-        db.add(flappybird_score)
+    global user_id
+    user_id = "2e10a287-5e5d-4151-bfa5-bcfffa325433"
 
-        await db.commit()
-
-
-def test_create_rows():  # A first test is needed to run startuptest once and create the datas needed for the actual tests
-    with (
-        client
-    ):  # That syntax trigger the startup events in commons.py and all test files
-        pass
-
-
-def test_create_flappybird_score():
+    global token
     token = create_api_access_token(user=user)
-    response = client.post(
-        "/flappybird/scores/me?value=25",
-        headers={"Authorization": f"Bearer {token}"},
+
+    global flappybird_score
+    flappybird_score = models_flappybird.FlappyBirdScore(
+        id="0b7dc7bf-0ab4-421a-bbe7-7ec064fcec8d",
+        user_id=user.id,
+        user=user,
+        value=25,
+        creation_time=datetime.now(UTC),
     )
-    assert response.status_code == 201
-    json = response.json()
-    assert "value" in json
-    assert json["value"] == 25
 
-
-def test_get_flappybird_user_score():
-    token = create_api_access_token(user=user)
-    response = client.get(
-        "/flappybird/scores/me",
-        headers={"Authorization": f"Bearer {token}"},
-    )
-    assert response.status_code == 200
-    data = response.json()
-    assert len(data) > 0
-
-
-def test_get_flappybird_position_score():
-    token = create_api_access_token(user=user)
-    response = client.get(
-        "/flappybird/scores/me",
-        headers={"Authorization": f"Bearer {token}"},
-    )
-    assert response.status_code == 200
-    data = response.json()
-    assert len(data) > 0
+    await add_object_to_db(flappybird_score)
 
 
 def test_get_flappybird_score():
