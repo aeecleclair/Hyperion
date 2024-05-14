bb98def01065d3825e6ec19753b586c9ac3e0d50
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-05-14 14:27:02 +0200

    Merge and update ph migration files

--- migrations/versions/12-ph.py
+++ migrations/versions/12-ph.py
@@ -1,21 +1,34 @@
-"""empty message
+"""10-ph
 
-Create Date: 2024-04-25 19:44:53.520776
+Create Date: 2024-05-08 13:02:15.712830
 """
 
 from collections.abc import Sequence
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pytest_alembic import MigrationContext
 
 import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "7b2d2f884627"
-down_revision: str | None = "d99516f0bbcb"
+revision: str = "fce1716123e2"
+down_revision: str | None = "e22bfe152f72"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
+    op.create_table(
+        "ph_papers",
+        sa.Column("id", sa.String(), nullable=False),
+        sa.Column("name", sa.String(), nullable=False),
+        sa.Column("release_date", sa.Date(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_index(op.f("ix_ph_papers_id"), "ph_papers", ["id"], unique=False)
+
     with op.batch_alter_table("notification_topic_membership") as batch_op:
         batch_op.alter_column(
             "topic",
@@ -23,7 +36,6 @@ def upgrade() -> None:
             type_=sa.Enum(
                 "cinema",
                 "advert",
-                "bookingadmin",
                 "amap",
                 "booking",
                 "event",
@@ -38,6 +50,8 @@ def upgrade() -> None:
 
 
 def downgrade() -> None:
+    op.drop_index(op.f("ix_ph_papers_id"), table_name="ph_papers")
+    op.drop_table("ph_papers")
     with op.batch_alter_table("notification_topic_membership") as batch_op:
         batch_op.alter_column(
             "topic",
@@ -45,7 +59,6 @@ def downgrade() -> None:
             type_=sa.Enum(
                 "cinema",
                 "advert",
-                "bookingadmin",
                 "amap",
                 "booking",
                 "event",
@@ -56,3 +69,17 @@ def downgrade() -> None:
             ),
             existing_nullable=False,
         )
+
+
+def pre_test_upgrade(
+    alembic_runner: "MigrationContext",
+    alembic_connection: sa.Connection,
+) -> None:
+    pass
+
+
+def test_upgrade(
+    alembic_runner: "MigrationContext",
+    alembic_connection: sa.Connection,
+) -> None:
+    pass
