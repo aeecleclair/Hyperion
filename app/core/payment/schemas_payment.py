e3fe00b11ae5185e996be32dac5b73567ddd936c
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-05-16 15:04:51 +0200

    Ignore @computed_field on @property decorator

--- app/core/payment/schemas_payment.py
+++ app/core/payment/schemas_payment.py
@@ -22,7 +22,7 @@ class CheckoutComplete(BaseModel):
 
     payments: list[CheckoutPayment]
 
-    @computed_field
+    @computed_field  # type: ignore[misc]
     @property
     def payment_completed(self) -> bool:
         total_paid = sum([payment.paid_amount for payment in self.payments])
