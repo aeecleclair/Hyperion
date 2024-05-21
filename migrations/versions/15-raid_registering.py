fe8447d05119d7f8d73d99d278b7eb5619403eb9
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-05-21 16:20:18 +0200

    Fix migration

--- migrations/versions/15-raid_registering.py
+++ migrations/versions/15-raid_registering.py
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "9d7d834743d1"
-down_revision: str | None = "1dadc35d79a5"
+down_revision: str | None = "146039b64c92"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
