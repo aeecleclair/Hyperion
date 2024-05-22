603b62097b8754a0c8c1a3c337f73d5905fe6031
Malem38 <m38.lemonnier@gmail.com>
2024-05-22 16:12:00 +0200

    add product related endpoints body

--- app/modules/cdr/endpoints_cdr.py
+++ app/modules/cdr/endpoints_cdr.py
@@ -41,6 +41,7 @@ async def is_user_a_seller(
     sellers = await cruds_cdr.get_sellers(db)
 
     sellers_groups = [str(seller.group_id) for seller in sellers]
+    sellers_groups.append(GroupType.admin_cdr)
 
     if is_user_member_of_an_allowed_group(
         user=user,
@@ -55,6 +56,38 @@ async def is_user_a_seller(
     raise HTTPException(
         status_code=403,
         detail="Unauthorized, user is not a seller.",
+    )
+
+
+async def is_user_in_a_seller_group(
+    seller_id: uuid.UUID,
+    user: models_core.CoreUser = Depends(
+        is_user_a_member,
+    ),
+    request_id: str = Depends(get_request_id),
+    db: AsyncSession = Depends(get_db),
+):
+    seller = await cruds_cdr.get_seller_by_id(db, seller_id=seller_id)
+
+    if not seller:
+        raise HTTPException(
+            status_code=404,
+            detail="Seller not found.",
+        )
+
+    if is_user_member_of_an_allowed_group(
+        user=user,
+        allowed_groups=[str(seller.group_id), GroupType.admin_cdr],
+    ):
+        return user
+
+    hyperion_access_logger.warning(
+        f"Is_user_a_member_of: Unauthorized, user is not a seller ({request_id})",
+    )
+
+    raise HTTPException(
+        status_code=403,
+        detail="Unauthorized, user is not in this seller group.",
     )
 
 
@@ -210,9 +243,9 @@ async def delete_seller(
 )
 async def get_products(
     db: AsyncSession = Depends(get_db),
-    user: models_core.CoreUser = Depends(is_user_a_member),
+    user: models_core.CoreUser = Depends(is_user_a_member_of(GroupType.admin_cdr)),
 ):
-    pass
+    return await cruds_cdr.get_products(db)
 
 
 @module.router.get(
@@ -223,21 +256,22 @@ async def get_products(
 async def get_products_by_seller_id(
     seller_id: uuid.UUID,
     db: AsyncSession = Depends(get_db),
-    user: models_core.CoreUser = Depends(is_user_a_member),
+    user: models_core.CoreUser = Depends(is_user_a_seller),
 ):
-    pass
+    await is_user_in_a_seller_group(seller_id, user)
+    return await cruds_cdr.get_products_by_seller_id(db, seller_id)
 
 
 @module.router.get(
-    "/cdr/products/public_displayed/",
+    "/cdr/products/available_online/",
     response_model=list[schemas_cdr.ProductComplete],
     status_code=200,
 )
-async def get_public_displayed_products(
+async def get_available_online_products(
     db: AsyncSession = Depends(get_db),
     user: models_core.CoreUser = Depends(is_user_a_member),
 ):
-    pass
+    return await cruds_cdr.get_available_online_products(db)
 
 
 @module.router.get(
@@ -248,22 +282,137 @@ async def get_public_displayed_products(
 async def get_product_by_id(
     product_id: uuid.UUID,
     db: AsyncSession = Depends(get_db),
-    user: models_core.CoreUser = Depends(is_user_a_member),
+    user: models_core.CoreUser = Depends(is_user_a_member_of(GroupType.admin_cdr)),
 ):
-    pass
+    return await cruds_cdr.get_product_by_id(db, product_id)
 
 
 @module.router.post(
     "/cdr/products/",
-    response_model=schemas_cdr.ProductBase,
+    response_model=schemas_cdr.ProductComplete,
     status_code=201,
 )
 async def create_product(
     product: schemas_cdr.ProductBase,
     db: AsyncSession = Depends(get_db),
-    user: models_core.CoreUser = Depends(is_user_a_member),
+    user: models_core.CoreUser = Depends(is_user_a_seller),
 ):
-    pass
+    await is_user_in_a_seller_group(
+        product.seller_id,
+        user,
+    )
+    status = await get_core_data(schemas_cdr.Status, db)
+    if status.status == CdrStatus.closed:
+        raise HTTPException(
+            status_code=403,
+            detail="CDR is closed. You cant add a new product.",
+        )
+    db_product = models_cdr.CdrProduct(
+        id=str(uuid.uuid4()),
+        **product.model_dump(),
+    )
+    try:
+        await cruds_cdr.create_product(db, db_product)
+        await db.commit()
+        return db_product
+    except Exception as error:
+        await db.rollback()
+        raise HTTPException(status_code=422, detail=str(error))
+
+
+@module.router.post(
+    "/cdr/products/{product_id}/document_constraint/{document_id}",
+    response_model=schemas_cdr.ProductComplete,
+    status_code=201,
+)
+async def create_document_constraint(
+    product_id: uuid.UUID,
+    document_id: uuid.UUID,
+    db: AsyncSession = Depends(get_db),
+    user: models_core.CoreUser = Depends(is_user_a_seller),
+):
+    db_product = await cruds_cdr.get_product_by_id(product_id=product_id, db=db)
+    if not db_product:
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid product_id",
+        )
+    db_document = await cruds_cdr.get_document_by_id(document_id=document_id, db=db)
+    if not db_document:
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid document_id",
+        )
+    await is_user_in_a_seller_group(
+        db_product.seller_id,
+        user,
+    )
+    status = await get_core_data(schemas_cdr.Status, db)
+    if status.status == CdrStatus.closed:
+        raise HTTPException(
+            status_code=403,
+            detail="CDR is closed. You cant add a new constraint.",
+        )
+    db_document_constraint = models_cdr.DocumentConstraint(
+        document_id=document_id,
+        product_id=product_id,
+    )
+    try:
+        await cruds_cdr.create_document_constraint(db, db_document_constraint)
+        await db.commit()
+        return await cruds_cdr.get_product_by_id(product_id=product_id, db=db)
+    except Exception as error:
+        await db.rollback()
+        raise HTTPException(status_code=422, detail=str(error))
+
+
+@module.router.post(
+    "/cdr/products/{product_id}/product_constraint/{constraint_id}",
+    response_model=schemas_cdr.ProductComplete,
+    status_code=201,
+)
+async def create_product_constraint(
+    product_id: uuid.UUID,
+    constraint_id: uuid.UUID,
+    db: AsyncSession = Depends(get_db),
+    user: models_core.CoreUser = Depends(is_user_a_seller),
+):
+    db_product = await cruds_cdr.get_product_by_id(product_id=product_id, db=db)
+    if not db_product:
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid product_id",
+        )
+    db_constraint = await cruds_cdr.get_product_by_id(product_id=constraint_id, db=db)
+    if not db_constraint:
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid document_id",
+        )
+    await is_user_in_a_seller_group(
+        db_product.seller_id,
+        user,
+    )
+    status = await get_core_data(schemas_cdr.Status, db)
+    if status.status in [
+        CdrStatus.onsite,
+        CdrStatus.closed,
+    ] or (status.status == CdrStatus.online and db_product.available_online):
+        raise HTTPException(
+            status_code=403,
+            detail="This product can't be edited now. Please try creating a new product.",
+        )
+    db_product_constraint = models_cdr.ProductConstraint(
+        product_id=product_id,
+        product_constraint_id=constraint_id,
+    )
+    try:
+        await cruds_cdr.create_product_constraint(db, db_product_constraint)
+        await db.commit()
+        return await cruds_cdr.get_product_by_id(product_id=product_id, db=db)
+    except Exception as error:
+        await db.rollback()
+        raise HTTPException(status_code=422, detail=str(error))
 
 
 @module.router.patch(
@@ -276,7 +425,35 @@ async def update_product(
     db: AsyncSession = Depends(get_db),
     user: models_core.CoreUser = Depends(is_user_a_member),
 ):
-    pass
+    status = await get_core_data(schemas_cdr.Status, db)
+    db_product = await cruds_cdr.get_product_by_id(product_id=product_id, db=db)
+    if not db_product:
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid product_id",
+        )
+    await is_user_in_a_seller_group(
+        db_product.seller_id,
+        user,
+    )
+    if status.status in [
+        CdrStatus.onsite,
+        CdrStatus.closed,
+    ] or (status.status == CdrStatus.online and db_product.available_online):
+        raise HTTPException(
+            status_code=403,
+            detail="This product can't be edited now. Please try creating a new product.",
+        )
+    try:
+        await cruds_cdr.update_product(
+            product_id=product_id,
+            product=product,
+            db=db,
+        )
+        await db.commit()
+    except Exception as error:
+        await db.rollback()
+        raise HTTPException(status_code=400, detail=str(error))
 
 
 @module.router.delete(
@@ -284,11 +461,111 @@ async def update_product(
     status_code=204,
 )
 async def delete_product(
-    seller_id: uuid.UUID,
+    product_id: uuid.UUID,
     db: AsyncSession = Depends(get_db),
     user: models_core.CoreUser = Depends(is_user_a_member),
 ):
-    pass
+    status = await get_core_data(schemas_cdr.Status, db)
+    db_product = await cruds_cdr.get_product_by_id(product_id=product_id, db=db)
+    if not db_product:
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid product_id",
+        )
+    await is_user_in_a_seller_group(
+        db_product.seller_id,
+        user,
+    )
+    if status.status != CdrStatus.pending:
+        raise HTTPException(
+            status_code=403,
+            detail="You can't delete a product once CDR has started.",
+        )
+    try:
+        await cruds_cdr.delete_product(
+            product_id=product_id,
+            db=db,
+        )
+        await db.commit()
+    except Exception as error:
+        await db.rollback()
+        raise HTTPException(status_code=400, detail=str(error))
+
+
+@module.router.delete(
+    "/cdr/products/{product_id}/document_constraint/{document_id}",
+    status_code=204,
+)
+async def delete_document_constraint(
+    product_id: uuid.UUID,
+    document_id: uuid.UUID,
+    db: AsyncSession = Depends(get_db),
+    user: models_core.CoreUser = Depends(is_user_a_seller),
+):
+    db_product = await cruds_cdr.get_product_by_id(product_id=product_id, db=db)
+    if not db_product:
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid product_id",
+        )
+    await is_user_in_a_seller_group(
+        db_product.seller_id,
+        user,
+    )
+    status = await get_core_data(schemas_cdr.Status, db)
+    if status.status != CdrStatus.pending:
+        raise HTTPException(
+            status_code=403,
+            detail="You can't delete a constraint once CDR has started.",
+        )
+    try:
+        await cruds_cdr.delete_document_constraint(
+            product_id=product_id,
+            document_id=document_id,
+            db=db,
+        )
+        await db.commit()
+    except Exception as error:
+        await db.rollback()
+        raise HTTPException(status_code=400, detail=str(error))
+
+
+@module.router.delete(
+    "/cdr/products/{product_id}/product_constraint/{constraint_id}",
+    status_code=204,
+)
+async def delete_product_constraint(
+    product_id: uuid.UUID,
+    constraint_id: uuid.UUID,
+    db: AsyncSession = Depends(get_db),
+    user: models_core.CoreUser = Depends(is_user_a_seller),
+):
+    db_product = await cruds_cdr.get_product_by_id(product_id=product_id, db=db)
+    if not db_product:
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid product_id",
+        )
+    await is_user_in_a_seller_group(
+        db_product.seller_id,
+        user,
+    )
+    status = await get_core_data(schemas_cdr.Status, db)
+    if status.status != CdrStatus.pending:
+        raise HTTPException(
+            status_code=403,
+            detail="You can't delete a constraint once CDR has started.",
+        )
+    try:
+        await cruds_cdr.delete_product_constraint(
+            product_id=product_id,
+            product_constraint_id=constraint_id,
+            db=db,
+        )
+        await db.commit()
+    except Exception as error:
+        await db.rollback()
+        raise HTTPException(status_code=400, detail=str(error))
 
 
 @module.router.get(
