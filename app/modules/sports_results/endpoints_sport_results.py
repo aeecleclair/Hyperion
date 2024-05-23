6bfebad36b05f6a02f7f714d907e5fb7c776e75c
BLEMENT33 <150018300+BLEMENT33@users.noreply.github.com>
2024-05-23 13:29:12 +0200

    feat : add sport endpoints

--- app/modules/sports_results/endpoints_sport_results.py
+++ app/modules/sports_results/endpoints_sport_results.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core import models_core
 from app.core.groups.groups_type import GroupType
 from app.core.module import Module
-from app.dependencies import get_db, is_user_a_member
+from app.dependencies import get_db, is_user_a_member, is_user_a_member_of
 from app.modules.sports_results import (
     cruds_sport_results,
     models_sport_results,
@@ -201,101 +201,71 @@ async def delete_result(
 
 
 @module.router.post(
-    "/sport-results/sport",
+    "/sport-results/sport/",
     response_model=schemas_sport_results.Sport,
     status_code=201,
 )
 async def add_sport(
-    result: schemas_sport_results.ResultBase,
+    sport: schemas_sport_results.SportBase,
     db: AsyncSession = Depends(get_db),
-    user: models_core.CoreUser = Depends(is_user_a_member),
+    user: models_core.CoreUser = Depends(is_user_a_member_of(GroupType.BDS)),
 ):
-    if not cruds_sport_results.is_user_a_captain_of_a_sport(
-        user.id,
-        result.sport_id,
-        db,
-    ):
-        raise HTTPException(status_code=401, detail="Not a captain")
-
-    result_complete = schemas_sport_results.ResultComplete(
+    sport_complete = schemas_sport_results.SportComplete(
         id=str(uuid.uuid4()),
-        **result.model_dump(),
+        **sport.model_dump(),
     )
-
     try:
-        result_db = models_sport_results.Result(
-            id=result_complete.id,
-            sport_id=result_complete.sport_id,
-            score1=result_complete.score1,
-            score2=result_complete.score2,
-            rank=result_complete.rank,
-            location=result_complete.location,
-            match_date=result_complete.match_date,
+        sport_db = models_sport_results.Sport(
+            id=sport_complete.id,
+            name=sport_complete.name,
+            captains=sport_complete.captains,
         )
-        return await cruds_sport_results.create_result(result=result_db, db=db)
+        return await cruds_sport_results.create_sport(sport=sport_db, db=db)
     except ValueError as error:
         raise HTTPException(status_code=400, detail=str(error))
 
 
 @module.router.patch(
-    "/sport-results/{result_id}",
+    "/sport-results/sport/{sport_id}",
     status_code=204,
 )
 async def update_sport(
-    result_id: str,
-    result_update: schemas_sport_results.ResultUpdate,
+    sport_id: str,
+    sport_update: schemas_sport_results.SportUpdate,
     db: AsyncSession = Depends(get_db),
     user: models_core.CoreUser = Depends(is_user_a_member),
 ):
-    result = await cruds_sport_results.get_result_by_id(result_id=result_id, db=db)
-    if not result:
+    sport = await cruds_sport_results.get_sport_by_id(sport_id=sport_id, db=db)
+    if not sport:
         raise HTTPException(
             status_code=404,
             detail="Invalid id",
         )
 
-    if not cruds_sport_results.is_user_a_captain_of_a_sport(
-        user.id,
-        result.sport_id,
-        db,
-    ) or not cruds_sport_results.is_user_a_captain_of_a_sport(
-        user.id,
-        result_update.sport_id,
-        db,
-    ):
-        raise HTTPException(status_code=401, detail="Not a captain")
-
-    await cruds_sport_results.update_result(
-        result_id=result_id,
-        result_update=result_update,
+    await cruds_sport_results.update_sport(
+        sport_id=sport_id,
+        sport_update=sport_update,
         db=db,
     )
 
 
 @module.router.delete(
-    "/sport-results/{result_id}",
+    "/sport-results/sport/{sport_id}",
     status_code=204,
 )
 async def delete_sport(
-    result_id: str,
+    sport_id: str,
     db: AsyncSession = Depends(get_db),
     user: models_core.CoreUser = Depends(is_user_a_member),
 ):
-    result = await cruds_sport_results.get_result_by_id(result_id=result_id, db=db)
-    if not result:
+    sport = await cruds_sport_results.get_sport_by_id(sport_id=sport_id, db=db)
+    if not sport:
         raise HTTPException(
             status_code=404,
             detail="Invalid id",
         )
 
-    if not cruds_sport_results.is_user_a_captain_of_a_sport(
-        user.id,
-        result.sport_id,
-        db,
-    ):
-        raise HTTPException(status_code=401, detail="Not a captain")
-
-    await cruds_sport_results.delete_result(
-        result_id=result_id,
+    await cruds_sport_results.delete_sport(
+        sport_id=sport_id,
         db=db,
     )
