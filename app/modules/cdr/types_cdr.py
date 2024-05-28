00fe8ff6f9111a7bdc2aa199d8e187e1d0e9f72b
Malem38 <m38.lemonnier@gmail.com>
2024-05-28 09:57:15 +0200

    add even more tests

--- app/modules/cdr/types_cdr.py
+++ app/modules/cdr/types_cdr.py
@@ -5,9 +5,6 @@ class DocumentSignatureType(str, Enum):
     material = "material"
     numeric = "numeric"
 
-    def __str__(self):
-        return f"{self.name}<{self.value}>"
-
 
 class PaymentType(str, Enum):
     cash = "cash"
@@ -16,16 +13,10 @@ class PaymentType(str, Enum):
     card = "card"
     archived = "archived"
 
-    def __str__(self):
-        return f"{self.name}<{self.value}>"
-
 
 class AvailableMembership(str, Enum):
     aeecl = "AEECL"
     useecl = "USEECL"
-
-    def __str__(self):
-        return f"{self.name}<{self.value}>"
 
 
 class CdrStatus(str, Enum):
@@ -34,15 +25,9 @@ class CdrStatus(str, Enum):
     onsite = "onsite"
     closed = "closed"
 
-    def __str__(self):
-        return f"{self.name}<{self.value}>"
-
 
 class CdrLogActionType(str, Enum):
     purchase_add = "purchase_add"
     purchase_delete = "purchase_delete"
     payment_add = "payment_add"
     payment_delete = "payment_delete"
-
-    def __str__(self):
-        return f"{self.name}<{self.value}>"
