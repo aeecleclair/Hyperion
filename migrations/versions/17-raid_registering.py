db974ed1125ae8c09e71f524efdd289c7ccb4134
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-06-11 15:46:01 +0200

    Rebase migrations

--- migrations/versions/17-raid_registering.py
+++ migrations/versions/17-raid_registering.py
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "9d7d834743d1"
-down_revision: str | None = "146039b64c92"
+down_revision: str | None = "bb8fbaf26f5a"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
