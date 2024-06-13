c57ea577469db2b7ca035074a9e61678ef35e8f4
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-06-13 21:35:44 +0200

    feat: use a seperate coredata_raid file

--- app/modules/raid/schemas_raid.py
+++ app/modules/raid/schemas_raid.py
@@ -149,34 +149,8 @@ class EmergencyContact(BaseModel):
     phone: str | None = None
 
 
-class RaidInformation(core_data.BaseCoreData):
-    raid_start_date: date | None = None
-    raid_end_date: date | None = None
-    raid_registering_end_date: date | None = None
-    payment_link: str | None = None
-    contact: str | None = None
-    president: EmergencyContact | None = None
-    volunteer_responsible: EmergencyContact | None = None
-    security_responsible: EmergencyContact | None = None
-    rescue: EmergencyContact | None = None
-    raid_rules_id: str | None = None
-    raid_information_id: str | None = None
-
-
 class RaidDriveFoldersCreation(BaseModel):
     parent_folder_id: str
-
-
-class RaidDriveFolders(core_data.BaseCoreData):
-    parent_folder_id: str | None = None
-    registering_folder_id: str | None = None
-    security_folder_id: str | None = None
-
-
-class RaidPrice(core_data.BaseCoreData):
-    student_price: int | None = None
-    partner_price: int | None = None
-    t_shirt_price: int | None = None
 
 
 class PaymentUrl(BaseModel):
