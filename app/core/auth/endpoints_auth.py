4c1e54d2fdca16bc2bf4e565e9a2198152da6327
Armand Didierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-06-16 14:18:00 +0200

    Improve auth test coverage and fix errors (#469)
    ### Description
    
    Fix:
     - external users can be allowed for specific AuthClient
     - AuthClients can be restricted to specific groups
    
    ### Checklist
    
    - [x] Created tests which fail without the change (if possible)
    - [x] All tests passing
    - [x] Extended the documentation, if necessary

--- app/core/auth/endpoints_auth.py
+++ app/core/auth/endpoints_auth.py
@@ -65,7 +65,7 @@ async def login_for_access_token(
     settings: Settings = Depends(get_settings),
 ):
     """
-    Ask for a JWT acc   ess token using oauth password flow.
+    Ask for a JWT access token using oauth password flow.
 
     *username* and *password* must be provided
 
@@ -126,9 +126,9 @@ async def get_authorize_page(
     **This endpoint is a UI endpoint which send and html page response. It will redirect to `/auth/authorization-flow/authorize-validation`**
     """
     return templates.TemplateResponse(
+        request,
         "connexion.html",
         {
-            "request": request,
             "response_type": authorizereq.response_type,
             "redirect_uri": authorizereq.redirect_uri,
             "client_id": authorizereq.client_id,
@@ -173,9 +173,9 @@ async def post_authorize_page(
     """
 
     return templates.TemplateResponse(
+        request,
         "connexion.html",
         {
-            "request": request,
             "response_type": response_type,
             "redirect_uri": redirect_uri,
             "client_id": client_id,
@@ -306,9 +306,9 @@ async def authorize_validation(
             f"Authorize-validation: Invalid user email or password for email {authorizereq.email} ({request_id})",
         )
         return templates.TemplateResponse(
+            request,
             "connexion.html",
             {
-                "request": request,
                 "response_type": authorizereq.response_type,
                 "redirect_uri": redirect_uri,
                 "client_id": authorizereq.client_id,
@@ -336,7 +336,7 @@ async def authorize_validation(
             if authorizereq.state:
                 url += "&state=" + authorizereq.state
             return RedirectResponse(url, status_code=status.HTTP_302_FOUND)
-    if auth_client.allow_external_users:
+    if not auth_client.allow_external_users:
         if is_user_external(user):
             # TODO We should show an HTML page explaining the issue
             hyperion_access_logger.warning(
--- app/core/users/cruds_users.py
+++ app/core/users/cruds_users.py
@@ -80,7 +80,13 @@ async def get_user_by_email(
     """Return user with id from database as a dictionary"""
 
     result = await db.execute(
-        select(models_core.CoreUser).where(models_core.CoreUser.email == email),
+        select(models_core.CoreUser)
+        .where(models_core.CoreUser.email == email)
+        .options(
+            # The group relationship need to be loaded to be able
+            # to check if the user is a member of a specific group
+            selectinload(models_core.CoreUser.groups),
+        ),
     )
     return result.scalars().first()
 
