e4e17d88c80750583269a1888c4d56081dfd09f1
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-07-25 14:55:48 +0200

    Base our gunicorn config file on uvicorn-gunicorn-docker's and use it in our docker image

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ FROM tiangolo/uvicorn-gunicorn-fastapi:python3.11
 # Image running several instances of uvicorn in parallel with gunicorn, listens on port 80
 # See https://github.com/tiangolo/uvicorn-gunicorn-fastapi-docker
 
-COPY ./gunicorn.conf.py /app/gunicorn.conf.py
+# Gunicorn config file must be named `gunicorn_conf.py` to be used by `uvicorn-gunicorn-fastapi`
+# See https://github.com/tiangolo/uvicorn-gunicorn-docker?tab=readme-ov-file#gunicorn_conf
+COPY ./gunicorn.conf.py /app/gunicorn_conf.py
 
 COPY ./alembic.ini /app/alembic.ini
 COPY ./migrations /app/migrations
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,62 @@
 import logging
+import multiprocessing
 import os
 
 from app.app import init_db
 from app.core.config import construct_prod_settings
 from app.core.log import LogConfig
+
+# This gunicorn configuration file is based on [`uvicorn-gunicorn-docker` image config file](https://github.com/tiangolo/uvicorn-gunicorn-docker/blob/master/docker-images/gunicorn_conf.py)
+# It define an `on_starting` hook that instantiate the database and run migrations
+
+# For usage with uvicorn-gunicorn-docker image see:
+# https://github.com/tiangolo/uvicorn-gunicorn-docker?tab=readme-ov-file#gunicorn_conf
+
+
+workers_per_core_str = os.getenv("WORKERS_PER_CORE", "1")
+max_workers_str = os.getenv("MAX_WORKERS")
+use_max_workers = None
+if max_workers_str:
+    use_max_workers = int(max_workers_str)
+web_concurrency_str = os.getenv("WEB_CONCURRENCY", None)
+
+host = os.getenv("HOST", "0.0.0.0")
+port = os.getenv("PORT", "80")
+bind_env = os.getenv("BIND", None)
+use_loglevel = os.getenv("LOG_LEVEL", "info")
+if bind_env:  # noqa: SIM108
+    use_bind = bind_env
+else:
+    use_bind = f"{host}:{port}"
+
+cores = multiprocessing.cpu_count()
+workers_per_core = float(workers_per_core_str)
+default_web_concurrency = workers_per_core * cores
+if web_concurrency_str:
+    web_concurrency = int(web_concurrency_str)
+    assert web_concurrency > 0  # noqa: S101
+else:
+    web_concurrency = max(int(default_web_concurrency), 2)
+    if use_max_workers:
+        web_concurrency = min(web_concurrency, use_max_workers)
+accesslog_var = os.getenv("ACCESS_LOG", "-")
+use_accesslog = accesslog_var or None
+errorlog_var = os.getenv("ERROR_LOG", "-")
+use_errorlog = errorlog_var or None
+graceful_timeout_str = os.getenv("GRACEFUL_TIMEOUT", "120")
+timeout_str = os.getenv("TIMEOUT", "120")
+keepalive_str = os.getenv("KEEP_ALIVE", "5")
+
+# Gunicorn config variables
+loglevel = use_loglevel
+workers = web_concurrency
+bind = use_bind
+errorlog = use_errorlog
+worker_tmp_dir = "/dev/shm"  # noqa: S108
+accesslog = use_accesslog
+graceful_timeout = int(graceful_timeout_str)
+timeout = int(timeout_str)
+keepalive = int(keepalive_str)
 
 
 def on_starting(server) -> None:
