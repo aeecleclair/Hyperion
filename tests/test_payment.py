5f02072cbb16039c05f561bac272475a5c1e7735
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-08-02 15:43:13 +0200

    Test payment tool

--- tests/test_payment.py
+++ tests/test_payment.py
@@ -1,20 +1,39 @@
 import uuid
+from typing import TYPE_CHECKING
 
+import pytest
 import pytest_asyncio
 from fastapi.testclient import TestClient
+from helloasso_api_wrapper.exceptions import ApiV5BadRequest
+from helloasso_api_wrapper.models.carts import (
+    InitCheckoutBody,
+    InitCheckoutResponse,
+)
 from pytest_mock import MockerFixture
+from requests import Response
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core import schemas_core
+from app.core.groups.groups_type import GroupType
 from app.core.payment import cruds_payment, models_payment, schemas_payment
+from app.core.payment.payment_tool import PaymentTool
+from app.types.exceptions import PaymentToolCredentialsNotSetException
 from app.types.module import Module
 from tests.commons import (
     TestingSessionLocal,
     add_object_to_db,
+    create_user_with_groups,
+    override_get_settings,
 )
+
+if TYPE_CHECKING:
+    from app.core.config import Settings
 
 checkout_with_existing_checkout_payment: models_payment.Checkout
 existing_checkout_payment: models_payment.CheckoutPayment
 checkout: models_payment.Checkout
+
+user_schema: schemas_core.CoreUser
 
 TEST_MODULE_ROOT = "tests"
 
@@ -54,6 +73,13 @@ async def init_objects() -> None:
         secret="secret",
     )
     await add_object_to_db(checkout)
+
+    global user_schema
+    user = await create_user_with_groups(groups=[GroupType.student])
+    user_schema = schemas_core.CoreUser(**user.__dict__)
+
+
+# Test endpoints #
 
 
 def test_webhook_with_invalid_body(client: TestClient) -> None:
@@ -340,3 +366,168 @@ async def test_webhook_payment_callback_fail(
     mocked_hyperion_security_logger.assert_called_with(
         f"Payment: call to module {TEST_MODULE_ROOT} payment callback for checkout (hyperion_checkout_id: {checkout.id}, HelloAsso checkout_id: {checkout.id}) failed with an error Test error",
     )
+
+
+# Test Payment tool #
+
+
+def test_payment_tool_unavailable():
+    settings = override_get_settings()
+
+    payment_tool = PaymentTool(settings)
+
+    assert not payment_tool.is_payment_available()
+
+
+async def test_payment_tool_get_checkout():
+    settings = override_get_settings()
+
+    payment_tool = PaymentTool(settings)
+
+    async with TestingSessionLocal() as db:
+        # Get existing checkout
+        existing_checkout = await payment_tool.get_checkout(
+            checkout_id=checkout_with_existing_checkout_payment.id,
+            db=db,
+        )
+        assert existing_checkout is not None
+        assert existing_checkout.id == checkout_with_existing_checkout_payment.id
+
+        # Get non existing checkout
+        unexisting_checkout = await payment_tool.get_checkout(
+            checkout_id=uuid.uuid4(),
+            db=db,
+        )
+        assert unexisting_checkout is None
+
+
+async def test_payment_tool_init_checkout_with_unavailable_payment(
+    mocker: MockerFixture,
+):
+    settings = override_get_settings()
+
+    payment_tool = PaymentTool(settings)
+
+    db: AsyncSession = mocker.MagicMock()
+
+    with pytest.raises(
+        PaymentToolCredentialsNotSetException,
+        match="HelloAsso API credentials are not set",
+    ):
+        await payment_tool.init_checkout(
+            module="test",
+            helloasso_slug="test",
+            checkout_amount=100,
+            checkout_name="test",
+            redirection_uri="test",
+            db=db,
+        )
+
+
+async def test_payment_tool_init_checkout(
+    mocker: MockerFixture,
+):
+    # We create a mocked settings object with the required HelloAsso API credentials
+    settings: Settings = mocker.MagicMock()
+    settings.HELLOASSO_API_BASE = "https://example.com"
+    settings.HELLOASSO_CLIENT_ID = "clientid"
+    settings.HELLOASSO_CLIENT_SECRET = "secret"
+
+    # We mock the whole HelloAssoAPIWrapper to avoid making real API calls
+    # and prevent the class initialization from failing to authenticate
+    mocker.patch(
+        "app.core.payment.payment_tool.HelloAssoAPIWrapper",
+    )
+
+    redirect_url = "https://example.com"
+
+    payment_tool = PaymentTool(settings=settings)
+
+    # We mock the HelloAssoAPIWrapper `init_a_checkout` method to return a mocked response
+    mocker.patch.object(
+        payment_tool.hello_asso.checkout_intents_management,  # type: ignore # we know that payment_tool.hello_asso is not null as we patched settings to enable payment
+        "init_a_checkout",
+        return_value=InitCheckoutResponse(id=7, redirectUrl=redirect_url),
+    )
+
+    async with TestingSessionLocal() as db:
+        returned_checkout = await payment_tool.init_checkout(
+            module="testtool",
+            helloasso_slug="test",
+            checkout_amount=100,
+            checkout_name="test",
+            redirection_uri="redirect",
+            db=db,
+            payer_user=user_schema,
+        )
+
+        assert returned_checkout.payment_url == redirect_url
+
+        # We want to check that the checkout was created in the database
+        created_checkout = await payment_tool.get_checkout(
+            checkout_id=returned_checkout.id,
+            db=db,
+        )
+        assert created_checkout is not None
+
+
+async def test_payment_tool_init_checkout_with_one_failure(
+    mocker: MockerFixture,
+):
+    """
+    When HelloAsso init_checkout fail a first time, we want to retry a second time without payers infos.
+    """
+    # We create a mocked settings object with the required HelloAsso API credentials
+    settings: Settings = mocker.MagicMock()
+    settings.HELLOASSO_API_BASE = "https://example.com"
+    settings.HELLOASSO_CLIENT_ID = "clientid"
+    settings.HELLOASSO_CLIENT_SECRET = "secret"
+
+    # We mock the whole HelloAssoAPIWrapper to avoid making real API calls
+    # and prevent the class initialization from failing to authenticate
+    mocker.patch(
+        "app.core.payment.payment_tool.HelloAssoAPIWrapper",
+    )
+
+    redirect_url = "https://example.com"
+
+    payment_tool = PaymentTool(settings=settings)
+
+    # We create a side effect for the `init_a_checkout` method that will raise an error the first time
+    # init_checkout is called with a payer, and return a mocked response the second time
+    def init_a_checkout_side_effect(
+        helloasso_slug: str,
+        init_checkout_body: InitCheckoutBody,
+    ):
+        if init_checkout_body.payer is not None:
+            r = Response()
+            r.status_code = 400
+            raise ApiV5BadRequest(r)
+        return InitCheckoutResponse(id=7, redirectUrl=redirect_url)
+
+    # We mock the HelloAssoAPIWrapper `init_a_checkout` method to return a mocked response
+    mocker.patch.object(
+        payment_tool.hello_asso.checkout_intents_management,  # type: ignore # we know that payment_tool.hello_asso is not null as we patched settings to enable payment
+        "init_a_checkout",
+        side_effect=init_a_checkout_side_effect,
+    )
+
+    async with TestingSessionLocal() as db:
+        returned_checkout = await payment_tool.init_checkout(
+            module="testtool",
+            helloasso_slug="test",
+            checkout_amount=100,
+            checkout_name="test",
+            redirection_uri="redirect",
+            db=db,
+            payer_user=user_schema,
+        )
+
+        assert returned_checkout.payment_url == redirect_url
+
+        # We want to check that the checkout was created in the database
+        created_checkout = await payment_tool.get_checkout(
+            checkout_id=returned_checkout.id,
+            db=db,
+        )
+        assert created_checkout is not None
