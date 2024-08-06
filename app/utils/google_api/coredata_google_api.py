ab72394738b20fc169f215630c2241bb1fa5f37e
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-08-06 17:13:39 +0200

    Implement GoogleAPI authentication flow

---
+++ app/utils/google_api/coredata_google_api.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from app.types.core_data import BaseCoreData
+
+
+class GoogleAPICredentials(BaseCoreData):
+    token: str
+    refresh_token: str
+    token_uri: str
+    client_id: str
+    client_secret: str
+    scopes: list[str]
+    expiry: datetime
+
+
+class GoogleAPIOAuthFlow(BaseCoreData):
+    state: str | None = None
