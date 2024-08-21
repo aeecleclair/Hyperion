aeecleclair/Hyperion

commit 0af7649f8d2ddabc36803eb1c91977e068a15c9d

Use postgresql for tests (#450)
### Description

Fix #403 

Add Postgresql databse support for tests. Use a TestAppClient per test
file, to prevent tests from a module to impact other modules

Configure a Postgresql database for the test workflow.
Run tests with Postgresql. Run migration tests with SQLite and
Postgresql.

### Checklist

- [x] Created tests which fail without the change (if possible)
- [x] All tests passing
- [x] Extended the documentation, if necessary

---------

Co-authored-by: julien4215 <120588494+julien4215@users.noreply.github.com>

tests/test_migrations.py
@@ -16,8 +16,7 @@ from pytest_alembic.tests.experimental import downgrade_leaves_no_trace  # noqa:
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Connection
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test_migration.db"
-
+from tests.commons import SQLALCHEMY_DATABASE_URL_SYNC
 
 pre_test_upgrade_dict: dict[
     str,
@@ -96,12 +95,9 @@ def alembic_connection() -> Generator[Connection, None, None]:
 
     Due to an issue with event loop, we can't run Alembic from asynchronous functions. The tests can't be async, so we need to use a synchronous connection.
     """
-    # We need to delete the test database before each test
-    Path("test_migration.db").unlink(missing_ok=True)
-
     # We use `echo=False` to disable SQLAlchemy logging for migrations
     # as errors are easier to see without all SQLAlchemy info
-    connectable = create_engine(SQLALCHEMY_DATABASE_URL, echo=False)
+    connectable = create_engine(SQLALCHEMY_DATABASE_URL_SYNC, echo=False)
 
     with connectable.begin() as connection:
         yield connection

.github/workflows/test.yml
@@ -30,6 +30,23 @@ jobs:
         ports:
           # Maps port 6379 on service container to the host
           - 6379:6379
+      postgres:
+        # Docker Hub image
+        image: postgres
+        # Provide the password for postgres
+        env:
+          POSTGRES_PASSWORD: "somerealpassword"
+          POSTGRES_USER: "hyperion"
+          POSTGRES_DB: "hyperion"
+        # Set health checks to wait until postgres has started
+        options: >-
+          --health-cmd pg_isready
+          --health-interval 10s
+          --health-timeout 5s
+          --health-retries 5
+        ports:
+          # Maps tcp port 5432 on service container to the host
+          - 5432:5432
 
     steps:
       - name: Check out the code
@@ -61,9 +78,14 @@ jobs:
         with:
           path: .pytest_cache
           key: pytest_cache-${{ github.head_ref }}
+      
+      - name: Run migration unit tests with SQLite
+        run: python -m pytest tests/test_migrations.py
 
-      - name: Run unit tests
+      - name: Run unit tests with Postgresql
         run: python -m pytest --cov
+        env:
+          SQLITE_DB: ""
 
       - name: Upload coverage reports to Codecov
         uses: codecov/codecov-action@v4

.gitignore
@@ -27,7 +27,3 @@ logs/
 
 # Pytest-cov
 .coverage
-
-# Migration tests
-test_migration.db
-test_migration.db-journal

migrations/versions/0-6-init.py
@@ -2,7 +2,7 @@
 
 Revision ID: 17b92dc4b50d
 Revises:
-Create Date: 2024-04-12 18:19:12.439926
+Create Date: 2024-05-26 16:25:22.390935
 
 """
 
@@ -929,6 +929,74 @@ def downgrade() -> None:
     op.drop_table("amap_delivery")
     op.drop_index(op.f("ix_advert_advertisers_id"), table_name="advert_advertisers")
     op.drop_table("advert_advertisers")
+    sa.Enum(
+        "Autre",
+        "Adoma",
+        "Exte",
+        "T1",
+        "T2",
+        "T3",
+        "T4",
+        "T56",
+        "U1",
+        "U2",
+        "U3",
+        "U4",
+        "U56",
+        "V1",
+        "V2",
+        "V3",
+        "V45",
+        "V6",
+        "X1",
+        "X2",
+        "X3",
+        "X4",
+        "X5",
+        "X6",
+        name="floorstype",
+    ).drop(op.get_bind())
+    sa.Enum(
+        "cinema",
+        "advert",
+        "bookingadmin",
+        "amap",
+        "booking",
+        "event",
+        "loan",
+        "raffle",
+        "vote",
+        name="topic",
+    ).drop(op.get_bind())
+    sa.Enum(
+        "eventAE",
+        "eventUSE",
+        "independentAssociation",
+        "happyHour",
+        "direction",
+        "nightParty",
+        "other",
+        name="calendareventtype",
+    ).drop(op.get_bind())
+    sa.Enum(
+        "comity",
+        "section_ae",
+        "club_ae",
+        "section_use",
+        "club_use",
+        "association_independant",
+        name="kinds",
+    ).drop(op.get_bind())
+    sa.Enum(
+        "waiting",
+        "open",
+        "closed",
+        "counting",
+        "published",
+        name="statustype",
+    ).drop(op.get_bind())
+    sa.Enum("midi", "soir", name="amapslottype").drop(op.get_bind())
+    sa.Enum("creation", "open", "lock", name="rafflestatustype").drop(op.get_bind())
     # ### end Alembic commands ###
 
 

migrations/versions/8-recommendation_primary_key.py
@@ -6,6 +6,7 @@ Create Date: 2024-04-21 02:08:19.548067
 from collections.abc import Sequence
 from datetime import UTC, datetime
 from typing import TYPE_CHECKING
+from uuid import UUID
 
 import sqlalchemy as sa
 from alembic import op
@@ -61,4 +62,10 @@ def test_upgrade(
         sa.text("SELECT id from recommendation"),
     ).fetchall()
 
-    assert ("66c363bc-c71f-4eae-8376-c37712a312f6",) in rows
+    assert len(rows) > 0
+
+    if isinstance(rows[0][0], UUID):
+        assert (UUID("66c363bc-c71f-4eae-8376-c37712a312f6"),) in rows
+    else:
+        # SQLite does not support UUID and will use strings
+        assert ("66c363bc-c71f-4eae-8376-c37712a312f6",) in rows

migrations/versions/9-external_users.py
@@ -78,6 +78,11 @@ def downgrade() -> None:
     op.drop_column("core_user_unconfirmed", "external")
     op.drop_column("core_user", "external")
 
+    core_user = sa.sql.table("core_user", sa.Column("floor"))
+    op.execute(
+        core_user.update().where(core_user.c.floor.is_(None)).values(floor="Autre"),
+    )
+
     with op.batch_alter_table("core_user") as batch_op:
         batch_op.alter_column(
             "floor",
@@ -108,6 +113,7 @@ def downgrade() -> None:
                 "X6",
                 name="floorstype",
             ),
+            # We make this column non nullable, we must provide a default value
             nullable=False,
         )
 

pyproject.toml
@@ -114,7 +114,13 @@ ignore_missing_imports = true
 
 [tool.pytest.ini_options]
 asyncio_mode = "auto"
-filterwarnings = "error"
+filterwarnings = [
+    "error",
+    # We don't know how to fix the warning:
+    # `ResourceWarning: connection <psycopg.Connection [IDLE] (host=... user=... database=...) at 0x1166bf310> was deleted while still open. Please use 'with' or '.close()' to close the connection`
+    # It only happen when running tests with Postgresql
+    "ignore:connection <.*> was deleted while still open.:ResourceWarning",
+]
 
 [tool.coverage.run]
 source_pkgs = ["app"]

commit c839090800ba4329aa19ec9ba6e6bd2de349c11c

Drop test db before migration tests (#529)
### Description

If we don't drop the test database before migration tests, missing
columns won't be detected

This was broken since https://github.com/aeecleclair/Hyperion/pull/450

app/app.py
@@ -17,7 +17,6 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from fastapi.routing import APIRoute
-from sqlalchemy import MetaData
 from sqlalchemy.engine import Connection, Engine
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
@@ -126,18 +125,7 @@ def update_db_tables(
         # We have an Engine, we want to acquire a Connection
         with sync_engine.begin() as conn:
             if drop_db:
-                # All tables should be dropped, including the alembic_version table
-                # or Hyperion will think that the database is up to date and will not initialize it
-                # when running tests a second time.
-                # To let SQLAlchemy drop the alembic_version table, we created a AlembicVersion model.
-
-                # `Base.metadata.drop_all(conn)` is only able to drop tables that are defined in models
-                # This means that if a model is deleted, its table will never be dropped by `Base.metadata.drop_all(conn)`
-
-                # Thus we construct a metadata object that reflects the database instead of only using models
-                my_metadata: MetaData = MetaData(schema=Base.metadata.schema)
-                my_metadata.reflect(bind=conn, resolve_fks=False)
-                my_metadata.drop_all(bind=conn)
+                initialization.drop_db_sync(conn)
 
             alembic_current_revision = get_alembic_current_revision(conn)
 

app/utils/initialization.py
@@ -1,10 +1,11 @@
-from sqlalchemy import select
+from sqlalchemy import Connection, MetaData, select
 from sqlalchemy.engine import Engine, create_engine
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session, selectinload
 
 from app.core import models_core
 from app.core.config import Settings
+from app.types.sqlalchemy import Base
 
 # These utils are used at startup to run database initializations & migrations
 
@@ -78,3 +79,21 @@ def create_group_sync(
         raise
     else:
         return group
+
+
+def drop_db_sync(conn: Connection):
+    """
+    Drop all tables in the database
+    """
+    # All tables should be dropped, including the alembic_version table
+    # or Hyperion will think that the database is up to date and will not initialize it
+    # when running tests a second time.
+    # To let SQLAlchemy drop the alembic_version table, we created a AlembicVersion model.
+
+    # `Base.metadata.drop_all(conn)` is only able to drop tables that are defined in models
+    # This means that if a model is deleted, its table will never be dropped by `Base.metadata.drop_all(conn)`
+
+    # Thus we construct a metadata object that reflects the database instead of only using models
+    my_metadata: MetaData = MetaData(schema=Base.metadata.schema)
+    my_metadata.reflect(bind=conn, resolve_fks=False)
+    my_metadata.drop_all(bind=conn)

tests/test_migrations.py
@@ -16,6 +16,7 @@ from pytest_alembic.tests.experimental import downgrade_leaves_no_trace  # noqa:
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Connection
 
+from app.utils.initialization import drop_db_sync
 from tests.commons import SQLALCHEMY_DATABASE_URL_SYNC
 
 pre_test_upgrade_dict: dict[
@@ -105,6 +106,11 @@ def alembic_connection() -> Generator[Connection, None, None]:
     connectable = create_engine(SQLALCHEMY_DATABASE_URL_SYNC, echo=False)
 
     with connectable.begin() as connection:
+        # For other tests we use a test app client that will drop the database during its startup
+        # but for migrations tests we use the database Connection directly
+        # We drop the database to ensure that the database is empty
+        drop_db_sync(connection)
+
         yield connection
 
     connectable.dispose()