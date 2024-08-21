71408c9401dd85855ce83d623daaff23fb222186
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-08-21 20:54:16 +0200

    Reorder migrations

--- migrations/versions/19-fix_phonebook.py
+++ migrations/versions/19-fix_phonebook.py
@@ -18,7 +18,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "6df7fb89081d"
-down_revision: str | None = "bb8fbaf26f5a"
+down_revision: str | None = "5d05a19f14bc"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
