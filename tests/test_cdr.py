fc9d9cff7a7132097b48f3e1a34806adb4b50174
Thonyk <timr.web@free.fr>
2024-08-24 17:37:10 +0200

    Fix: useless cdr test

--- tests/test_cdr.py
+++ tests/test_cdr.py
@@ -447,18 +447,10 @@ def test_update_cdr_user_user(client: TestClient):
 def test_get_cdr_user_seller(client: TestClient):
     response = client.get(
         f"/cdr/users/{cdr_user.id}",
-        headers={"Authorization": f"Bearer {token_bde}"},
+        headers={"Authorization": f"Bearer {token_user}"},
     )
     assert response.status_code == 200
     assert str(cdr_user.id) == response.json()["id"]
-
-
-def test_get_cdr_user_user(client: TestClient):
-    response = client.get(
-        f"/cdr/users/{cdr_user.id}",
-        headers={"Authorization": f"Bearer {token_user}"},
-    )
-    assert response.status_code == 403
 
 
 def test_get_all_sellers_admin(client: TestClient):
