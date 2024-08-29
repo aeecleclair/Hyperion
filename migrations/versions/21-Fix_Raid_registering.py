3f93456e8752aa7d17e6c5cb8858b966a4e4aec0
Maxime Roucher <maximeroucher@gmail.com>
2024-08-29 17:04:26 +0200

    fix: migration

--- migrations/versions/21-Fix_Raid_registering.py
+++ migrations/versions/21-Fix_Raid_registering.py
@@ -24,8 +24,8 @@ def upgrade() -> None:
     with op.batch_alter_table("raid_participant") as batch_op:
         batch_op.alter_column(
             "bike_size",
-            existing_type=sa.Enum("XS", "S", "M", "L", "XL", "None_", name="size"),
-            type_=sa.VARCHAR(length=2),
+            existing_type=sa.VARCHAR(length=2),
+            type_=sa.Enum("XS", "S", "M", "L", "XL", "None_", name="size"),
             existing_nullable=True,
         )
         batch_op.alter_column(
