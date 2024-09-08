cdd01fe6cda1edb3fd0d1f07f4a837f5f8aa1797
Rotheem <114694873+Rotheem@users.noreply.github.com>
2024-09-08 18:31:10 +0200

    User fusion (#563)
    ### Description
    
    Please explain the changes you made here.
    
    ### Checklist
    
    - [ ] Created tests which fail without the change (if possible)
    - [ ] All tests passing
    - [ ] Extended the documentation, if necessary
    
    ---------
    
    Co-authored-by: Maxime Lemonnier <98470688+Malem38@users.noreply.github.com>

---
+++ tests/test_user_fusion.py
@@ -0,0 +1,84 @@
+import pytest_asyncio
+from fastapi.testclient import TestClient
+
+from app.core import models_core
+from app.core.groups.groups_type import GroupType
+from tests.commons import (
+    create_api_access_token,
+    create_user_with_groups,
+)
+
+admin_user: models_core.CoreUser
+student_user_to_delete: models_core.CoreUser
+student_user_to_keep: models_core.CoreUser
+
+token_admin_user: str
+token_student_user: str
+
+
+FABRISTPP_EMAIL_1 = "fabristpp.eclair1@etu.ec-lyon.fr"
+FABRISTPP_EMAIL_2 = "fabristpp.eclair3@ecl21.ec-lyon.fr"
+
+
+@pytest_asyncio.fixture(scope="module", autouse=True)
+async def init_objects() -> None:
+    global admin_user, student_user_to_delete, student_user_to_keep
+
+    admin_user = await create_user_with_groups(
+        [GroupType.admin],
+    )
+    student_user_to_keep = await create_user_with_groups(
+        [GroupType.student, GroupType.BDE],
+        email=FABRISTPP_EMAIL_1,
+    )
+    student_user_to_delete = await create_user_with_groups(
+        [GroupType.student, GroupType.BDE, GroupType.CAA],
+        email=FABRISTPP_EMAIL_2,
+    )
+
+    global token_admin_user, token_student_user
+    token_admin_user = create_api_access_token(admin_user)
+    token_student_user = create_api_access_token(student_user_to_keep)
+
+
+def test_fusion_users(client: TestClient) -> None:
+    response = client.patch(
+        "/users/fusion",
+        headers={"Authorization": f"Bearer {token_student_user}"},
+        json={
+            "user_kept_email": student_user_to_keep.email,
+            "user_deleted_email": student_user_to_delete.email,
+        },
+    )
+    assert response.status_code == 403
+
+    response = client.patch(
+        "/users/fusion",
+        headers={"Authorization": f"Bearer {token_admin_user}"},
+        json={
+            "user_kept_email": student_user_to_keep.email,
+            "user_deleted_email": student_user_to_delete.email,
+        },
+    )
+    assert response.status_code == 204
+
+    response = client.get(
+        f"/users/{student_user_to_delete.id}",
+        headers={"Authorization": f"Bearer {token_admin_user}"},
+    )
+    assert response.status_code == 404
+
+    response = client.get(
+        f"/users/{student_user_to_keep.id}",
+        headers={"Authorization": f"Bearer {token_admin_user}"},
+    )
+    assert response.status_code == 200
+
+    response = client.get(
+        f"/groups/{GroupType.CAA.value}",
+        headers={"Authorization": f"Bearer {token_admin_user}"},
+    )
+    assert response.status_code == 200
+    users = response.json()["members"]
+    users_ids = [user["id"] for user in users]
+    assert student_user_to_keep.id in users_ids
