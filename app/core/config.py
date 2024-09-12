7900e400a871eeb7ae63610198f87136e1b78f8d
Thonyk <timr.web@free.fr>
2024-09-13 00:10:03 +0200

    Bump version

--- app/core/config.py
+++ app/core/config.py
@@ -192,7 +192,7 @@ class Settings(BaseSettings):
 
     # Hyperion follows Semantic Versioning
     # https://semver.org/
-    HYPERION_VERSION: str = "3.0.3"
+    HYPERION_VERSION: str = "3.0.4"
     MINIMAL_TITAN_VERSION_CODE: int = 130
 
     ######################################
