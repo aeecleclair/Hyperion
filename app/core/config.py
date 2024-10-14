4c4e0dae5ab5ab5c288cd901a16af4f42b1f9a37
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2024-10-14 09:43:09 +0200

    Bump version

--- app/core/config.py
+++ app/core/config.py
@@ -192,7 +192,7 @@ class Settings(BaseSettings):
 
     # Hyperion follows Semantic Versioning
     # https://semver.org/
-    HYPERION_VERSION: str = "3.1.1"
+    HYPERION_VERSION: str = "3.1.2"
     MINIMAL_TITAN_VERSION_CODE: int = 130
 
     ######################################
