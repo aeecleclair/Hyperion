2f2b2464228d0bc3974ec8bffe657936a6111d7c
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-23 17:41:05 +0200

    Advert

--- app/modules/advert/models_advert.py
+++ app/modules/advert/models_advert.py
@@ -9,28 +9,30 @@ from app.types.sqlalchemy import Base, TZDateTime
 class Advertiser(Base):
     __tablename__ = "advert_advertisers"
 
-    id: Mapped[str] = mapped_column(String, primary_key=True, index=True)
-    name: Mapped[str] = mapped_column(String, nullable=False, unique=True)
-    group_manager_id: Mapped[str] = mapped_column(String, nullable=False)
+    id: Mapped[str] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    group_manager_id: Mapped[str]
 
     adverts: Mapped[list["Advert"]] = relationship(
         "Advert",
         lazy="subquery",
         back_populates="advertiser",
+        init=False,
     )
 
 
 class Advert(Base):
     __tablename__ = "advert_adverts"
 
-    id: Mapped[str] = mapped_column(String, primary_key=True, nullable=False)
+    id: Mapped[str] = mapped_column(primary_key=True)
     advertiser_id: Mapped[str] = mapped_column(ForeignKey("advert_advertisers.id"))
+    title: Mapped[str]
+    content: Mapped[str]
+    date: Mapped[datetime]
+    tags: Mapped[str | None]
     advertiser: Mapped[Advertiser] = relationship(
         "Advertiser",
         lazy="joined",
         back_populates="adverts",
+        init=False,
     )
-    title: Mapped[str] = mapped_column(String, nullable=False)
-    content: Mapped[str] = mapped_column(String, nullable=False)
-    date: Mapped[datetime] = mapped_column(TZDateTime, nullable=False)
-    tags: Mapped[str] = mapped_column(String, nullable=True)
