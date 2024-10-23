cf89f780c77e5b5b73c1bdd0d7685f082ea901b4
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-23 17:24:19 +0200

    CDR

--- app/modules/cdr/models_cdr.py
+++ app/modules/cdr/models_cdr.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from typing import TYPE_CHECKING
 
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, MappedAsDataclass, mapped_column, relationship
 
 if TYPE_CHECKING:
     from app.core.models_core import CoreUser
@@ -16,19 +16,18 @@ from app.types.membership import AvailableAssociationMembership
 from app.types.sqlalchemy import Base, PrimaryKey
 
 
-class Seller(Base):
+class Seller(MappedAsDataclass, Base):
     __tablename__ = "cdr_seller"
 
     id: Mapped[PrimaryKey]
     name: Mapped[str]
     group_id: Mapped[str] = mapped_column(
         ForeignKey("core_group.id"),
-        nullable=False,
     )
     order: Mapped[int]
 
 
-class DocumentConstraint(Base):
+class DocumentConstraint(MappedAsDataclass, Base):
     __tablename__ = "cdr_document_constraint"
 
     product_id: Mapped[uuid.UUID] = mapped_column(
@@ -41,7 +40,7 @@ class DocumentConstraint(Base):
     )
 
 
-class ProductConstraint(Base):
+class ProductConstraint(MappedAsDataclass, Base):
     __tablename__ = "cdr_product_constraint"
 
     product_id: Mapped[uuid.UUID] = mapped_column(
@@ -54,7 +53,7 @@ class ProductConstraint(Base):
     )
 
 
-class TicketGenerator(Base):
+class TicketGenerator(MappedAsDataclass, Base):
     __tablename__ = "cdr_ticket_generator"
 
     id: Mapped[PrimaryKey]
@@ -66,7 +65,7 @@ class TicketGenerator(Base):
     expiration: Mapped[datetime]
 
 
-class CdrProduct(Base):
+class CdrProduct(MappedAsDataclass, Base):
     __tablename__ = "cdr_product"
 
     id: Mapped[PrimaryKey]
@@ -75,9 +74,11 @@ class CdrProduct(Base):
     )
     name_fr: Mapped[str]
     name_en: Mapped[str | None]
-    description_fr: Mapped[str | None]
-    description_en: Mapped[str | None]
     available_online: Mapped[bool]
+
+    description_fr: Mapped[str | None] = mapped_column(default=None)
+    description_en: Mapped[str | None] = mapped_column(default=None)
+
     product_constraints: Mapped[list["CdrProduct"]] = relationship(
         "CdrProduct",
         secondary="cdr_product_constraint",
@@ -85,31 +86,38 @@ class CdrProduct(Base):
         secondaryjoin="ProductConstraint.product_constraint_id==CdrProduct.id",
         lazy="joined",
         join_depth=1,
+        init=False,
     )
     document_constraints: Mapped[list["Document"]] = relationship(
         "app.modules.cdr.models_cdr.Document",
         secondary="cdr_document_constraint",
         lazy="selectin",  # Constraints are always loaded in cruds so we set this to not have to put selectinload
+        init=False,
     )
     variants: Mapped[list["ProductVariant"]] = relationship(
         "ProductVariant",
         lazy="selectin",
+        init=False,
     )
-    related_membership: Mapped[AvailableAssociationMembership | None]
     tickets: Mapped[list["TicketGenerator"]] = relationship(
         "TicketGenerator",
         lazy="selectin",
+        init=False,
+    )
+
+    related_membership: Mapped[AvailableAssociationMembership | None] = mapped_column(
+        default=None,
     )
 
 
-class Curriculum(Base):
+class Curriculum(MappedAsDataclass, Base):
     __tablename__ = "cdr_curriculum"
 
     id: Mapped[PrimaryKey]
     name: Mapped[str]
 
 
-class CurriculumMembership(Base):
+class CurriculumMembership(MappedAsDataclass, Base):
     __tablename__ = "cdr_curriculum_membership"
 
     user_id: Mapped[str] = mapped_column(
@@ -122,7 +130,7 @@ class CurriculumMembership(Base):
     )
 
 
-class AllowedCurriculum(Base):
+class AllowedCurriculum(MappedAsDataclass, Base):
     __tablename__ = "cdr_allowed_curriculum"
 
     product_variant_id: Mapped[uuid.UUID] = mapped_column(
@@ -135,7 +143,7 @@ class AllowedCurriculum(Base):
     )
 
 
-class ProductVariant(Base):
+class ProductVariant(MappedAsDataclass, Base):
     __tablename__ = "cdr_product_variant"
 
     id: Mapped[PrimaryKey]
@@ -144,20 +152,25 @@ class ProductVariant(Base):
     )
     name_fr: Mapped[str]
     name_en: Mapped[str | None]
-    description_fr: Mapped[str | None]
-    description_en: Mapped[str | None]
     price: Mapped[int]
     enabled: Mapped[bool]
     unique: Mapped[bool]
+    related_membership_added_duration: Mapped[timedelta | None] = mapped_column(
+        default=None,
+    )
+
+    description_fr: Mapped[str | None] = mapped_column(default=None)
+    description_en: Mapped[str | None] = mapped_column(default=None)
+
     allowed_curriculum: Mapped[list[Curriculum]] = relationship(
         "Curriculum",
         secondary="cdr_allowed_curriculum",
         lazy="selectin",
+        init=False,
     )
-    related_membership_added_duration: Mapped[timedelta | None]
 
 
-class Document(Base):
+class Document(MappedAsDataclass, Base):
     __tablename__ = "cdr_document"
 
     id: Mapped[PrimaryKey]
@@ -167,7 +180,7 @@ class Document(Base):
     name: Mapped[str]
 
 
-class Purchase(Base):
+class Purchase(MappedAsDataclass, Base):
     __tablename__ = "cdr_purchase"
 
     user_id: Mapped[str] = mapped_column(
@@ -178,13 +191,18 @@ class Purchase(Base):
         ForeignKey("cdr_product_variant.id"),
         primary_key=True,
     )
-    product_variant: Mapped["ProductVariant"] = relationship("ProductVariant")
+
     quantity: Mapped[int]
     validated: Mapped[bool]
     purchased_on: Mapped[datetime]
 
+    product_variant: Mapped["ProductVariant"] = relationship(
+        "ProductVariant",
+        init=False,
+    )
 
-class Signature(Base):
+
+class Signature(MappedAsDataclass, Base):
     __tablename__ = "cdr_signature"
 
     user_id: Mapped[str] = mapped_column(
@@ -201,13 +219,12 @@ class Signature(Base):
     numeric_signature_id: Mapped[str | None]
 
 
-class Payment(Base):
+class Payment(MappedAsDataclass, Base):
     __tablename__ = "cdr_payment"
 
     id: Mapped[PrimaryKey]
     user_id: Mapped[str] = mapped_column(
         ForeignKey("core_user.id"),
-        nullable=False,
     )
     total: Mapped[int]
     payment_type: Mapped[PaymentType] = mapped_column(
@@ -215,34 +232,32 @@ class Payment(Base):
     )
 
 
-class CdrAction(Base):
+class CdrAction(MappedAsDataclass, Base):
     __tablename__ = "cdr_action"
 
     id: Mapped[PrimaryKey]
-    user_id: Mapped[str | None] = mapped_column(
-        ForeignKey("core_user.id"),
-        nullable=True,
-    )  # Who made the request
     subject_id: Mapped[str] = mapped_column(
         ForeignKey("core_user.id"),
-        nullable=False,
     )  # For who the request was made
     action_type: Mapped[CdrLogActionType]
     action: Mapped[str]
     timestamp: Mapped[datetime]
+    user_id: Mapped[str | None] = mapped_column(
+        ForeignKey("core_user.id"),
+        default=None,
+    )  # Who made the request
 
 
-class Checkout(Base):
+class Checkout(MappedAsDataclass, Base):
     __tablename__ = "cdr_checkout"
     id: Mapped[PrimaryKey]
-    user_id = mapped_column(
+    user_id: Mapped[str] = mapped_column(
         ForeignKey("core_user.id"),
-        nullable=False,
     )
     checkout_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("payment_checkout.id"))
 
 
-class Ticket(Base):
+class Ticket(MappedAsDataclass, Base):
     __tablename__ = "cdr_ticket"
     id: Mapped[PrimaryKey]
     secret: Mapped[uuid.UUID] = mapped_column(unique=True)
@@ -252,18 +267,26 @@ class Ticket(Base):
     product_variant_id: Mapped[uuid.UUID] = mapped_column(
         ForeignKey("cdr_product_variant.id"),
     )
-    product_variant: Mapped["ProductVariant"] = relationship("ProductVariant")
+
     user_id: Mapped[str] = mapped_column(
         ForeignKey("core_user.id"),
     )
     name: Mapped[str]
-    user: Mapped["CoreUser"] = relationship("CoreUser")
+
     scan_left: Mapped[int]
     tags: Mapped[str]  # Comma separated values
     expiration: Mapped[datetime]
+    user: Mapped["CoreUser"] = relationship(
+        "CoreUser",
+        init=False,
+    )
+    product_variant: Mapped["ProductVariant"] = relationship(
+        "ProductVariant",
+        init=False,
+    )
 
 
-class CustomDataField(Base):
+class CustomDataField(MappedAsDataclass, Base):
     __tablename__ = "cdr_customdata_field"
     id: Mapped[PrimaryKey]
     product_id: Mapped[uuid.UUID] = mapped_column(
@@ -272,12 +295,12 @@ class CustomDataField(Base):
     name: Mapped[str]
 
 
-class CustomData(Base):
+class CustomData(MappedAsDataclass, Base):
     __tablename__ = "cdr_customdata"
     field_id: Mapped[uuid.UUID] = mapped_column(
         ForeignKey("cdr_customdata_field.id"),
         primary_key=True,
     )
-    field: Mapped["CustomDataField"] = relationship("CustomDataField")
     user_id: Mapped[str] = mapped_column(ForeignKey("core_user.id"), primary_key=True)
     value: Mapped[str]
+    field: Mapped["CustomDataField"] = relationship("CustomDataField", init=False)
