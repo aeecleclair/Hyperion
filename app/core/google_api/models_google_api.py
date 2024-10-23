53194a3724aade5a9a6138da7e353f6f8f61e989
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-23 17:52:24 +0200

    GoogleAPI

--- app/core/google_api/models_google_api.py
+++ app/core/google_api/models_google_api.py
@@ -1,9 +1,9 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, MappedAsDataclass, mapped_column
 
 from app.types.sqlalchemy import Base
 
 
-class OAuthFlowState(Base):
+class OAuthFlowState(MappedAsDataclass, Base):
     __tablename__ = "google_api_oauth_flow_state"
 
     state: Mapped[str] = mapped_column(primary_key=True)
