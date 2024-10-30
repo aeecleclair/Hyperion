49077d26f4939be0581150d41aae1d3cccf53f40
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-30 11:23:07 +0100

    Add scan endpoint

--- app/core/myeclpay/schemas_myeclpay.py
+++ app/core/myeclpay/schemas_myeclpay.py
@@ -1,13 +1,12 @@
 from datetime import datetime
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import Base64Bytes, BaseModel
 
 from app.core.myeclpay.types_myeclpay import (
     CGUVersion,
     HistoryType,
     TransactionStatus,
-    TransactionType,
 )
 
 
@@ -22,3 +21,25 @@ class History(BaseModel):
     total: int
     creation: datetime
     status: TransactionStatus
+
+
+class QRCodeContentBase(BaseModel):
+    qr_code_id: UUID
+    total: int
+    creation: datetime
+    walled_device_id: UUID
+
+
+class QRCodeContent(QRCodeContentBase):
+    signature: Base64Bytes
+
+
+class QRCodeContentData(BaseModel):
+    """
+    Format of the data stored in the QR code,
+    """
+
+    id: UUID
+    tot: int
+    iat: datetime
+    key: UUID
