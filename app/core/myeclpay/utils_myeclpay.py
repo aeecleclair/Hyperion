2d3c55b6d67f73c756a8da57778a7869006b3433
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-30 11:23:07 +0100

    Add scan endpoint

---
+++ app/core/myeclpay/utils_myeclpay.py
@@ -0,0 +1,49 @@
+import logging
+from uuid import UUID
+
+from cryptography.hazmat.primitives.asymmetric import ed25519
+
+from app.core.myeclpay.schemas_myeclpay import (
+    QRCodeContentBase,
+    QRCodeContentData,
+)
+
+hyperion_security_logger = logging.getLogger("hyperion.security")
+
+
+def compute_signable_data(content: QRCodeContentBase) -> bytes:
+    return (
+        QRCodeContentData(
+            id=content.qr_code_id,
+            tot=content.total,
+            iat=content.creation,
+            key=content.walled_device_id,
+        )
+        .model_dump_json()
+        .encode("utf-8")
+    )
+
+
+def verify_signature(
+    public_key_bytes: bytes,
+    signature: bytes,
+    data: bytes,
+    wallet_device_id: UUID,
+    request_id: str,
+) -> bool:
+    """
+    Verify the signature of `data` with `signature` using ed25519.
+    """
+    try:
+        loaded_public_key = ed25519.Ed25519PublicKey.from_public_bytes(public_key_bytes)
+        loaded_public_key.verify(
+            signature,
+            data,
+        )
+    except Exception as error:
+        hyperion_security_logger.info(
+            f"MyECLPay: Failed to verify signature for QR Code with WalletDevice {wallet_device_id}: {error} ({request_id})",
+        )
+        return False
+    else:
+        return True
