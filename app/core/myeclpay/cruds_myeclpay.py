c38ed78a97d9d57188dc5786e9c87b9ed3c59582
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-30 11:23:07 +0100

    Add scan endpoint

--- app/core/myeclpay/cruds_myeclpay.py
+++ app/core/myeclpay/cruds_myeclpay.py
@@ -1,5 +1,5 @@
+from collections.abc import Sequence
 from datetime import datetime
-from typing import Sequence
 from uuid import UUID
 
 from sqlalchemy import or_, select, update
@@ -7,21 +7,64 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
 from app.core.myeclpay import models_myeclpay
-from app.core.myeclpay.types_myeclpay import WalletType
+from app.core.myeclpay.types_myeclpay import (
+    TransactionStatus,
+    TransactionType,
+    WalletType,
+)
 
 
 async def create_wallet(
     wallet_id: UUID,
-    type: WalletType,
+    wallet_type: WalletType,
     balance: int,
     db: AsyncSession,
 ) -> None:
     wallet = models_myeclpay.Wallet(
         id=wallet_id,
-        type=type,
+        type=wallet_type,
         balance=balance,
     )
     db.add(wallet)
+
+
+async def get_wallet(
+    wallet_id: UUID,
+    db: AsyncSession,
+) -> models_myeclpay.Wallet | None:
+    result = await db.execute(
+        select(models_myeclpay.Wallet).where(
+            models_myeclpay.Wallet.id == wallet_id,
+        ),
+    )
+    return result.scalars().first()
+
+
+async def get_wallet_device(
+    wallet_device_id: UUID,
+    db: AsyncSession,
+) -> models_myeclpay.WalletDevice | None:
+    result = await db.execute(
+        select(models_myeclpay.WalletDevice).where(
+            models_myeclpay.WalletDevice.id == wallet_device_id,
+        ),
+    )
+    return result.scalars().first()
+
+
+async def increment_wallet_balance(
+    wallet_id: UUID,
+    amount: int,
+    db: AsyncSession,
+) -> None:
+    """
+    Append `amount` to the wallet balance.
+    """
+    await db.execute(
+        update(models_myeclpay.Wallet)
+        .where(models_myeclpay.Wallet.id == wallet_id)
+        .values(balance=models_myeclpay.Wallet.balance + amount),
+    )
 
 
 async def create_user_payment(
@@ -68,6 +111,34 @@ async def get_user_payment(
     return result.scalars().first()
 
 
+async def create_transaction(
+    transaction_id: UUID,
+    giver_wallet_id: UUID,
+    giver_wallet_device_id: UUID,
+    receiver_wallet_id: UUID,
+    transaction_type: TransactionType,
+    seller_user_id: str | None,
+    total: int,
+    creation: datetime,
+    status: TransactionStatus,
+    store_note: str | None,
+    db: AsyncSession,
+) -> None:
+    transaction = models_myeclpay.Transaction(
+        id=transaction_id,
+        giver_wallet_id=giver_wallet_id,
+        giver_wallet_device_id=giver_wallet_device_id,
+        receiver_wallet_id=receiver_wallet_id,
+        transaction_type=transaction_type,
+        seller_user_id=seller_user_id,
+        total=total,
+        creation=creation,
+        status=status,
+        store_note=store_note,
+    )
+    db.add(transaction)
+
+
 async def get_transactions_by_wallet_id(
     wallet_id: UUID,
     db: AsyncSession,
@@ -98,3 +169,51 @@ async def get_transfers_by_wallet_id(
         ),
     )
     return result.scalars().all()
+
+
+async def get_seller_by_user_id_and_store_id(
+    store_id: UUID,
+    user_id: str,
+    db: AsyncSession,
+) -> models_myeclpay.Seller | None:
+    result = await db.execute(
+        select(models_myeclpay.Seller).where(
+            models_myeclpay.Seller.user_id == user_id,
+            models_myeclpay.Seller.store_id == store_id,
+        ),
+    )
+    return result.scalars().first()
+
+
+async def get_store(
+    store_id: UUID,
+    db: AsyncSession,
+) -> models_myeclpay.Store | None:
+    result = await db.execute(
+        select(models_myeclpay.Store).where(
+            models_myeclpay.Store.id == store_id,
+        ),
+    )
+    return result.scalars().first()
+
+
+async def create_used_qrcode(
+    qr_code_id: UUID,
+    db: AsyncSession,
+) -> None:
+    wallet = models_myeclpay.UsedQRCode(
+        qr_code_id=qr_code_id,
+    )
+    db.add(wallet)
+
+
+async def get_used_qrcode(
+    qr_code_id: UUID,
+    db: AsyncSession,
+) -> models_myeclpay.UsedQRCode | None:
+    result = await db.execute(
+        select(models_myeclpay.UsedQRCode).where(
+            models_myeclpay.UsedQRCode.qr_code_id == qr_code_id,
+        ),
+    )
+    return result.scalars().first()
