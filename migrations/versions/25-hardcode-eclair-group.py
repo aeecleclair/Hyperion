c75bd2aacac301771da2caafc5c5b90b55fd80b4
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-11-14 10:38:20 +0100

    Update eclair group id in all module defining a group foreignkey

--- migrations/versions/25-hardcode-eclair-group.py
+++ migrations/versions/25-hardcode-eclair-group.py
@@ -39,6 +39,34 @@ def upgrade() -> None:
         sa.Column("description", sa.String(), nullable=True),
     )
 
+    t_booking_manager = sa.Table(
+        "booking_manager",
+        sa.MetaData(),
+        sa.Column("id", sa.String(), nullable=False),
+        sa.Column("name", sa.String(), nullable=False),
+        sa.Column("group_id", sa.String(), nullable=False),
+    )
+    t_cdr_seller = sa.Table(
+        "cdr_seller",
+        sa.MetaData(),
+        sa.Column("id", sa.String(), nullable=False),
+        sa.Column("name", sa.String(), nullable=False),
+        sa.Column("group_id", sa.String(), nullable=False),
+        sa.Column("order", sa.Integer(), nullable=False),
+    )
+    t_phonebook_association_associated_groups = sa.Table(
+        "phonebook_association_associated_groups",
+        sa.MetaData(),
+        sa.Column("association_id", sa.String(), nullable=False),
+        sa.Column("group_id", sa.String(), nullable=False),
+    )
+    t_raffle = sa.Table(
+        "raffle",
+        sa.MetaData(),
+        sa.Column("id", sa.String(), nullable=False),
+        sa.Column("group_id", sa.String(), nullable=False),
+    )
+
     conn = op.get_bind()
     res = conn.execute(
         sa.select(
@@ -72,6 +100,26 @@ def upgrade() -> None:
         .where(t_membership.c.group_id == old_eclair_id)
         .values(group_id=new_eclair_id),
     )
+    conn.execute(
+        t_booking_manager.update()
+        .where(t_booking_manager.c.group_id == old_eclair_id)
+        .values(group_id=new_eclair_id),
+    )
+    conn.execute(
+        t_cdr_seller.update()
+        .where(t_cdr_seller.c.group_id == old_eclair_id)
+        .values(group_id=new_eclair_id),
+    )
+    conn.execute(
+        t_phonebook_association_associated_groups.update()
+        .where(t_phonebook_association_associated_groups.c.group_id == old_eclair_id)
+        .values(group_id=new_eclair_id),
+    )
+    conn.execute(
+        t_raffle.update()
+        .where(t_raffle.c.group_id == old_eclair_id)
+        .values(group_id=new_eclair_id),
+    )
 
     # We delete the old group
     conn.execute(t_group.delete().where(t_group.c.id == old_eclair_id))
@@ -86,7 +134,13 @@ def downgrade() -> None:
     pass
 
 
-user_id = "7a3ed1bb-c46c-486e-a439-f77ed5c8a7f9"
+test_user_id = "7a3ed1bb-c46c-486e-a439-f77ed5c8a7f9"
+test_booking_manager_id = "76a2d7fd-a782-4c12-8e4d-33205c55a574"
+test_cdr_seller_id = "2965a535-deb3-44ba-9f6c-f9d8f8cb5aea"
+test_phonebook_association_id = "9304ac6b-a455-4811-bc7a-13b2d7002073"
+test_raffle_id = "4cd0d369-9b11-456d-a70e-0e9be4591b75"
+
+test_old_eclair_id = "33cd5e2b-49e6-4d65-a652-474466a5dcde"
 
 
 def pre_test_upgrade(
@@ -96,7 +150,7 @@ def pre_test_upgrade(
     alembic_runner.insert_into(
         "core_user",
         {
-            "id": user_id,
+            "id": test_user_id,
             "email": "email@email.fr",
             "password_hash": "password_hash",
             "name": "name",
@@ -113,7 +167,7 @@ def pre_test_upgrade(
     alembic_runner.insert_into(
         "core_group",
         {
-            "id": "33cd5e2b-49e6-4d65-a652-474466a5dcde",
+            "id": test_old_eclair_id,
             "name": "eclair",
             "description": "",
         },
@@ -122,8 +176,56 @@ def pre_test_upgrade(
         "core_membership",
         {
             "user_id": "7a3ed1bb-c46c-486e-a439-f77ed5c8a7f9",
-            "group_id": "33cd5e2b-49e6-4d65-a652-474466a5dcde",
+            "group_id": test_old_eclair_id,
             "description": "",
+        },
+    )
+
+    alembic_runner.insert_into(
+        "booking_manager",
+        {
+            "id": test_booking_manager_id,
+            "name": "TestBookingManager",
+            "group_id": test_old_eclair_id,
+        },
+    )
+
+    alembic_runner.insert_into(
+        "cdr_seller",
+        {
+            "id": test_cdr_seller_id,
+            "name": "TestCDRSeller",
+            "group_id": test_old_eclair_id,
+            "order": 1,
+        },
+    )
+
+    alembic_runner.insert_into(
+        "phonebook_association",
+        {
+            "id": test_phonebook_association_id,
+            "name": "TestAsso",
+            "description": "TestAsso",
+            "kind": "Club AE",
+            "mandate_year": 2024,
+            "deactivated": False,
+        },
+    )
+    alembic_runner.insert_into(
+        "phonebook_association_associated_groups",
+        {
+            "association_id": test_phonebook_association_id,
+            "group_id": test_old_eclair_id,
+        },
+    )
+    alembic_runner.insert_into(
+        "raffle",
+        {
+            "id": test_raffle_id,
+            "name": "TestRaffle",
+            "status": "creation",
+            "group_id": test_old_eclair_id,
+            "description": "TestRaffle",
         },
     )
 
@@ -143,7 +245,49 @@ def test_upgrade(
     assert rows[0][0] == new_eclair_id
 
     rows = alembic_connection.execute(
-        sa.text(f"SELECT group_id from core_membership WHERE user_id = '{user_id}'"),
+        sa.text(
+            f"SELECT group_id from core_membership WHERE user_id = '{test_user_id}'",
+        ),
+    ).fetchall()
+
+    assert len(rows) > 0
+
+    assert rows[0][0] == new_eclair_id
+
+    rows = alembic_connection.execute(
+        sa.text(
+            f"SELECT group_id from booking_manager WHERE id = '{test_booking_manager_id}'",
+        ),
+    ).fetchall()
+
+    assert len(rows) > 0
+
+    assert rows[0][0] == new_eclair_id
+
+    rows = alembic_connection.execute(
+        sa.text(
+            f"SELECT group_id from cdr_seller WHERE id = '{test_cdr_seller_id}'",
+        ),
+    ).fetchall()
+
+    assert len(rows) > 0
+
+    assert rows[0][0] == new_eclair_id
+
+    rows = alembic_connection.execute(
+        sa.text(
+            f"SELECT group_id from phonebook_association_associated_groups WHERE association_id = '{test_phonebook_association_id}'",
+        ),
+    ).fetchall()
+
+    assert len(rows) > 0
+
+    assert rows[0][0] == new_eclair_id
+
+    rows = alembic_connection.execute(
+        sa.text(
+            f"SELECT group_id from raffle WHERE id = '{test_raffle_id}'",
+        ),
     ).fetchall()
 
     assert len(rows) > 0
