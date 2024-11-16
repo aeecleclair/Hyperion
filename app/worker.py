f544e769d32d097a3b09431d2703545ab5aecb9b
Daihecyy <paul.saubusse@gmail.com>
2024-11-17 00:04:30 +0100

    Worker redis settings configuration

--- app/worker.py
+++ app/worker.py
@@ -1,5 +1,9 @@
 import logging
 
+from arq.connections import RedisSettings
+
+from app.dependencies import get_settings
+
 scheduler_logger = logging.getLogger("scheduler")
 
 
@@ -8,7 +12,15 @@ async def run_task(ctx, job_function):
     return await job_function()
 
 
+settings = get_settings()  # Ce fichier ne doit être lancé que par la prod
+
+
 class WorkerSettings:
     functions = [run_task]
     allow_abort_jobs = True
     keep_result = 0
+    settings = RedisSettings(
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        password=settings.REDIS_PASSWORD,
+    )
