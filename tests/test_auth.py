cdf92deca15355928b2d878bdd8cf43a4e045434
Thonyk <timr.web@free.fr>
2024-11-17 01:21:00 +0100

    Fix: test auth

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -8,7 +8,7 @@ from fastapi.testclient import TestClient
 
 from app.core import models_core
 from app.core.auth import models_auth
-from app.core.groups.groups_type import AccountType
+from app.core.groups.groups_type import AccountType, GroupType
 from tests.commons import (
     add_object_to_db,
     create_api_access_token,
@@ -37,7 +37,7 @@ async def init_objects() -> None:
 
     global ecl_user
     ecl_user = await create_user_with_groups(
-        groups=[],
+        groups=[GroupType.eclair],
         account_type=AccountType.student,
         email="email@etu.ec-lyon.fr",
         password="azerty",
@@ -475,7 +475,7 @@ def test_authorization_code_flow_with_auth_client_restricting_allowed_groups_and
 ) -> None:
     # For an user that is a member of a required group #
     data_with_invalid_client_id = {
-        "client_id": "AcceptingOnlyECLUsersAuthClient",
+        "client_id": "RestrictingUsersGroupsAuthClient",
         "client_secret": "secret",
         "redirect_uri": "http://127.0.0.1:8000/docs",
         "response_type": "code",
@@ -502,13 +502,13 @@ def test_authorization_code_flow_with_auth_client_restricting_allowed_groups_and
 ) -> None:
     # For an user that is not a member of a required group #
     data_with_invalid_client_id = {
-        "client_id": "AcceptingOnlyECLUsersAuthClient",
+        "client_id": "RestrictingUsersGroupsAuthClient",
         "client_secret": "secret",
         "redirect_uri": "http://127.0.0.1:8000/docs",
         "response_type": "code",
         "scope": "API openid",
         "state": "azerty",
-        "email": "email@myecl.fr",
+        "email": "external@myecl.fr",
         "password": "azerty",
     }
     response = client.post(
