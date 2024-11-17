cef6b95d54061287ac2b34c8a23b3da59e3e6242
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2024-11-17 04:12:42 +0100

    SUper commit

--- app/utils/communication/future_notifications.py
+++ app/utils/communication/future_notifications.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-from fastapi import BackgroundTasks
-from sqlalchemy.ext.asyncio import AsyncSession
-
+from fastapi import BackgroundTasks, Depends
 from app.core.notification.notification_types import CustomTopic
 from app.core.notification.schemas_notification import Message
+from app.dependencies import get_scheduler
 from app.types.scheduler import Scheduler
 from app.utils.communication.notifications import NotificationManager, NotificationTool
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class FutureNotificationTool(NotificationTool):
@@ -14,55 +14,31 @@ class FutureNotificationTool(NotificationTool):
         self,
         background_tasks: BackgroundTasks,
         notification_manager: NotificationManager,
-        scheduler: Scheduler,
         db: AsyncSession,
         ):
         super().__init__(background_tasks, notification_manager, db)
-        self.scheduler = scheduler
-    async def send_future_notification_to_user_defer_to(
+    async def send_future_notification_to_user(
         self,
         user_id: str,
         message: Message,
         defer_date: datetime,
         job_id: str,
+        scheduler: Scheduler = Depends(get_scheduler),
     ) -> None:
-
-        await self.scheduler.queue_job_defer_to(self.send_notification_to_users,
+        
+        await scheduler.queue_job_defer_to(self.send_notification_to_users,
             user_ids=[user_id],
             message=message, job_id=job_id, defer_date=defer_date)
-
-    async def send_future_notification_to_topic_defer_to(
+    
+    async def send_future_notification_to_topic(
         self,
         message: Message,
         custom_topic: CustomTopic,
         defer_date: datetime,
         job_id: str,
+        scheduler: Scheduler = Depends(get_scheduler),
     ) -> None:
-
-        await self.scheduler.queue_job_defer_to(self.send_notification_to_topic,
+        
+        await scheduler.queue_job_defer_to(self.send_notification_to_topic,
             custom_topic=custom_topic,
             message=message, job_id=job_id, defer_date=defer_date)
-
-    async def send_future_notification_to_user_time_defer(
-        self,
-        user_id: str,
-        message: Message,
-        defer_seconds: float,
-        job_id: str,
-    ) -> None:
-
-        await self.scheduler.queue_job_time_defer(self.send_notification_to_users,
-            user_ids=[user_id],
-            message=message, job_id=job_id, defer_seconds=defer_seconds)
-
-    async def send_future_notification_to_topic_time_defer(
-        self,
-        message: Message,
-        custom_topic: CustomTopic,
-        defer_seconds: float,
-        job_id: str,
-    ) -> None:
-
-        await self.scheduler.queue_job_time_defer(self.send_notification_to_topic,
-            custom_topic=custom_topic,
-            message=message, job_id=job_id, defer_seconds=defer_seconds)
