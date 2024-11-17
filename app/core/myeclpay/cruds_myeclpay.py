9b9881afa65866fbc6f6be7e5ed12b6bc9a44aa6
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-11-17 09:43:56 +0100

    Precise store id when manipulating sellers

--- app/core/myeclpay/cruds_myeclpay.py
+++ app/core/myeclpay/cruds_myeclpay.py
@@ -58,24 +58,28 @@ async def get_admin_sellers(
 
 
 async def get_seller(
-    seller_user_id: UUID,
+    seller_user_id: str,
+    store_id: UUID,
     db: AsyncSession,
 ) -> models_myeclpay.Seller | None:
     result = await db.execute(
         select(models_myeclpay.Seller).where(
             models_myeclpay.Seller.user_id == seller_user_id,
+            models_myeclpay.Seller.store_id == store_id,
         ),
     )
     return result.scalars().first()
 
 
 async def delete_seller(
-    seller_user_id: UUID,
+    seller_user_id: str,
+    store_id: UUID,
     db: AsyncSession,
 ) -> None:
     await db.execute(
         delete(models_myeclpay.Seller).where(
             models_myeclpay.Seller.user_id == seller_user_id,
+            models_myeclpay.Seller.store_id == store_id,
         ),
     )
 
