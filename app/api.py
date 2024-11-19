8cebc462732c02372ce12063f7e3115c68492a86
Thonyk <timr.web@free.fr>
2024-11-19 04:40:26 +0100

    Fix: add router

--- app/api.py
+++ app/api.py
@@ -10,6 +10,7 @@ from app.core.google_api import endpoints_google_api
 from app.core.groups import endpoints_groups
 from app.core.notification import endpoints_notification
 from app.core.payment import endpoints_payment
+from app.core.schools import endpoints_schools
 from app.core.users import endpoints_users
 from app.modules.module_list import module_list
 
@@ -24,6 +25,7 @@ api_router.include_router(endpoints_groups.router)
 api_router.include_router(endpoints_notification.router)
 api_router.include_router(endpoints_payment.router)
 api_router.include_router(endpoints_users.router)
+api_router.include_router(endpoints_schools.router)
 
 for module in module_list:
     api_router.include_router(module.router)
