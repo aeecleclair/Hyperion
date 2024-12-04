8fdbab47ce9da9f4042c092c34bfa1429953b22f
Thonyk <timr.web@free.fr>
2024-12-04 14:07:16 +0100

    Fix: add log

--- app/app.py
+++ app/app.py
@@ -199,14 +199,18 @@ def initialize_module_visibility(
             coredata_core.ModuleVisibilityAwareness,
             db,
         )
+        hyperion_error_logger.info(module_awareness)
 
         new_modules = [
             module
             for module in module_list
             if module.root not in module_awareness.roots
         ]
+        hyperion_error_logger.info(
+            f"Startup: Checking modules visibility settings for {[module.root for module in new_modules]}",
+        )
         # Is run to create default module visibilities or when the table is empty
-        if new_modules:
+        if len(new_modules) != 0:
             hyperion_error_logger.info(
                 "Startup: Some modules visibility settings are empty, initializing them",
             )
