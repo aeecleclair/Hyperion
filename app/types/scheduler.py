4ceea58835f5c6a9acac942781eee23c4f6607da
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-12-05 15:54:27 +0100

    Inject db in run_task

--- app/types/scheduler.py
+++ app/types/scheduler.py
@@ -1,17 +1,18 @@
 import asyncio
 import logging
 from collections.abc import AsyncGenerator, Callable, Coroutine
-from datetime import datetime, timedelta
-from inspect import iscoroutinefunction, signature
+from datetime import datetime
+from inspect import signature
 from typing import TYPE_CHECKING, Any
 
 from arq.connections import RedisSettings
 from arq.jobs import Job
 from arq.typing import WorkerSettingsBase
 from arq.worker import create_worker
-from fastapi import params
 
 from app import dependencies
+from app.types.exceptions import SchedulerNotStartedError
+from app.utils.tools import execute_async_or_sync_method
 
 if TYPE_CHECKING:
     from arq import Worker
@@ -21,7 +22,7 @@ scheduler_logger = logging.getLogger("scheduler")
 
 
 async def run_task(
-    ctx,
+    ctx: dict[Any, Any] | None,
     job_function: Callable[..., Any],
     _dependency_overrides: dict[Callable[..., Any], Callable[..., Any]],
     **kwargs,
@@ -31,40 +32,43 @@ async def run_task(
 
     `job_function` may be a coroutine function or a regular function
 
-    The method will inject the following known dependencies into the job_function if needed:
-     - `get_db`
+    The method will inject an `AsyncSession` object, using `get_db`, in the kwargs if the job_function requires it
+
+    NOTE: As a consequence, it is not possible to plan a job using a custom AsyncSession.
+    Passing a custom AsyncSession would not be advisable as it would require the
+    db connection to remain open for the duration of the job.
     """
     scheduler_logger.debug(f"Job function consumed {job_function}")
 
+    require_db_for_kwargs: list[str] = []
     sign = signature(job_function)
     for param in sign.parameters.values():
-        if isinstance(param.default, params.Depends):
+        if isinstance(param.default, AsyncSession):
             # We iterate over the parameters of the job_function
-            # If we find a Depends object, we may want to inject the dependency
+            # If we find a AsyncSession object, we want to inject the dependency
+            require_db_for_kwargs.append(param.name)
+        else:
+            # We could support other types of dependencies
+            pass
 
-            # It is not easy to support any kind of Depends object
-            # as the corresponding method may be async or not, or be a special FastAPI object
-            # like BackgroundTasks or Request
+    # We distinguish between methods requiring a db and those that don't
+    # to only open the db connection when needed
+    if require_db_for_kwargs:
+        # `get_db` is the real dependency, defined in dependency.py
+        # `_get_db` may be the real dependency or an override
+        _get_db: Callable[[], AsyncGenerator[AsyncSession, None]] = (
+            _dependency_overrides.get(
+                dependencies.get_db,
+                dependencies.get_db,
+            )
+        )
 
-            # For now we filter accepted Depends objects manually
-            # and only accept `get_db`
-            if param.default.dependency == dependencies.get_db:
-                # `get_db` is the real dependency, defined in dependency.py
-                # `_get_db` may be the real dependency or an override
-                _get_db: Callable[[], AsyncGenerator[AsyncSession, None]] = (
-                    _dependency_overrides.get(
-                        dependencies.get_db,
-                        dependencies.get_db,
-                    )
-                )
-
-                async for db in _get_db():
-                    kwargs["db"] = db
-
-    if iscoroutinefunction(job_function):
-        return await job_function(**kwargs)
+        async for db in _get_db():
+            for name in require_db_for_kwargs:
+                kwargs[name] = db
+            await execute_async_or_sync_method(job_function, **kwargs)
     else:
-        return job_function(**kwargs)
+        await execute_async_or_sync_method(job_function, **kwargs)
 
 
 class Scheduler:
@@ -77,13 +81,14 @@ class Scheduler:
 
     # See https://github.com/fastapi/fastapi/discussions/9143#discussioncomment-5157572
 
+    # Pointer to the app dependency overrides dict
+    _dependency_overrides: dict[Callable[..., Any], Callable[..., Any]]
+
     def __init__(self):
         # ArqWorker, in charge of scheduling and executing tasks
         self.worker: Worker | None = None
         # Task will contain the asyncio task that runs the worker
         self.task: asyncio.Task | None = None
-        # Pointer to the get_db dependency
-        # self._dependency_overrides: Callable[[], AsyncGenerator[AsyncSession, None]]
 
     async def start(
         self,
@@ -130,30 +135,6 @@ class Scheduler:
         if self.worker is not None:
             await self.worker.close()
 
-    async def queue_job_time_defer(
-        self,
-        job_function: Callable[..., Coroutine[Any, Any, Any]],
-        job_id: str,
-        defer_seconds: int,
-        **kwargs,
-    ):
-        """
-        Queue a job to execute job_function in defer_seconds amount of seconds
-        job_id will allow to abort if needed
-        """
-        if self.worker is None:
-            scheduler_logger.error("Scheduler not started")
-            return None
-        job = await self.worker.pool.enqueue_job(
-            "run_task",
-            job_function=job_function,
-            _job_id=job_id,
-            _defer_by=timedelta(seconds=defer_seconds),
-            _dependency_overrides=self._dependency_overrides,
-            **kwargs,
-        )
-        scheduler_logger.debug(f"Job {job_id} queued {job}")
-
     async def queue_job_defer_to(
         self,
         job_function: Callable[..., Coroutine[Any, Any, Any]],
@@ -166,8 +147,8 @@ class Scheduler:
         job_id will allow to abort if needed
         """
         if self.worker is None:
-            scheduler_logger.error("Scheduler not started")
-            return None
+            raise SchedulerNotStartedError
+
         job = await self.worker.pool.enqueue_job(
             "run_task",
             job_function=job_function,
@@ -183,8 +164,7 @@ class Scheduler:
         cancel a queued job based on its job_id
         """
         if self.worker is None:
-            scheduler_logger.error("Scheduler not started")
-            return None
+            raise SchedulerNotStartedError
         job = Job(job_id, redis=self.worker.pool)
         scheduler_logger.debug(f"Job aborted {job}")
         await job.abort()
