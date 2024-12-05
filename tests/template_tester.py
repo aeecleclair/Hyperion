1a2a2fcfdc3e161dfd05376f3ccd2cdddd59678d
Daihecyy <paul.saubusse@gmail.com>
2024-12-05 12:42:21 +0100

    Fixing template tester

--- tests/template_tester.py
+++ tests/template_tester.py
@@ -20,7 +20,9 @@ with file_account_exists_mail.open(mode="w") as f:
     f.write(html_account_exists_mail)
 
 template_activation_mail = env.get_template("activation_mail.html")
-html_activation_mail = template_activation_mail.render(calypsso_activate_url="https://myecl.fr")
+html_activation_mail = template_activation_mail.render(
+    calypsso_activate_url="https://myecl.fr",
+)
 file_activation_mail = Path("./test_outputs/test_activation_mail.html")
 file_activation_mail.touch(exist_ok=True)
 with file_activation_mail.open(mode="w") as f:
@@ -35,9 +37,13 @@ with file_migration_mail.open(mode="w") as f:
     f.write(html_migration_mail)
 
 
-template_migration_mail_already_used = env.get_template("migration_mail_already_used.html")
+template_migration_mail_already_used = env.get_template(
+    "migration_mail_already_used.html",
+)
 html_migration_mail_already_used = template_migration_mail_already_used.render()
-file_migration_mail_already_used = Path("./test_outputs/test_migration_mail_already_used.html")
+file_migration_mail_already_used = Path(
+    "./test_outputs/test_migration_mail_already_used.html",
+)
 file_migration_mail_already_used.touch(exist_ok=True)
 with file_migration_mail_already_used.open(mode="w") as f:
     f.write(html_migration_mail_already_used)
@@ -51,10 +57,13 @@ with file_reset_mail.open(mode="w") as f:
     f.write(html_reset_mail)
 
 
-
 template_reset_mail_does_not_exist = env.get_template("reset_mail_does_not_exist.html")
-html_reset_mail_does_not_exist = template_reset_mail_does_not_exist.render(calypsso_register_url="https://myecl.fr")
-file_reset_mail_does_not_exist = Path("./test_outputs/test_reset_mail_does_not_exist.html")
+html_reset_mail_does_not_exist = template_reset_mail_does_not_exist.render(
+    calypsso_register_url="https://myecl.fr",
+)
+file_reset_mail_does_not_exist = Path(
+    "./test_outputs/test_reset_mail_does_not_exist.html",
+)
 file_reset_mail_does_not_exist.touch(exist_ok=True)
 with file_reset_mail_does_not_exist.open(mode="w") as f:
     f.write(html_reset_mail_does_not_exist)
