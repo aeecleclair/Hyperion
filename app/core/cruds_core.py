3b9686587015ee2f39e11e3d26108d27aef9e2e6
Thonyk <timr.web@free.fr>
2024-12-14 03:20:37 +0100

    Feat: group sync with association membership and move cruds

--- app/core/cruds_core.py
+++ app/core/cruds_core.py
@@ -1,11 +1,15 @@
 from collections.abc import Sequence
+from datetime import UTC, date, datetime
+from uuid import UUID
 
-from sqlalchemy import delete, select
+from sqlalchemy import delete, select, update
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core import models_core
 from app.core.groups.groups_type import AccountType
+from app.modules.cdr import schemas_cdr
+from app.types.membership import AvailableAssociationMembership
 
 
 async def get_modules_by_user(
@@ -182,3 +186,87 @@ async def delete_core_data_crud(
         ),
     )
     await db.commit()
+
+
+async def get_curents_memberships_by_name(
+    db: AsyncSession,
+    membership: AvailableAssociationMembership,
+) -> Sequence[models_core.CoreAssociationMembership]:
+    result = await db.execute(
+        select(models_core.CoreAssociationMembership).where(
+            models_core.CoreAssociationMembership.membership == membership,
+            models_core.CoreAssociationMembership.end_date > datetime.now(UTC).date(),
+            models_core.CoreAssociationMembership.start_date < datetime.now(UTC).date(),
+        ),
+    )
+    return result.scalars().all()
+
+
+async def get_actual_memberships_by_user_id(
+    db: AsyncSession,
+    user_id: str,
+) -> Sequence[models_core.CoreAssociationMembership]:
+    result = await db.execute(
+        select(models_core.CoreAssociationMembership).where(
+            models_core.CoreAssociationMembership.user_id == user_id,
+            models_core.CoreAssociationMembership.end_date
+            > date(datetime.now(UTC).year, 9, 5),
+        ),
+    )
+    return result.scalars().all()
+
+
+async def get_membership_by_user_id_and_membership_name(
+    db: AsyncSession,
+    user_id: str,
+    membership: AvailableAssociationMembership,
+) -> models_core.CoreAssociationMembership | None:
+    result = await db.execute(
+        select(models_core.CoreAssociationMembership).where(
+            models_core.CoreAssociationMembership.user_id == user_id
+            and models_core.CoreAssociationMembership.membership == membership,
+        ),
+    )
+    return result.scalars().first()
+
+
+async def get_membership_by_id(
+    db: AsyncSession,
+    membership_id: UUID,
+) -> models_core.CoreAssociationMembership | None:
+    result = await db.execute(
+        select(models_core.CoreAssociationMembership).where(
+            models_core.CoreAssociationMembership.id == membership_id,
+        ),
+    )
+    return result.scalars().first()
+
+
+def create_membership(
+    db: AsyncSession,
+    membership: models_core.CoreAssociationMembership,
+):
+    db.add(membership)
+
+
+async def delete_membership(
+    db: AsyncSession,
+    membership_id: UUID,
+):
+    await db.execute(
+        delete(models_core.CoreAssociationMembership).where(
+            models_core.CoreAssociationMembership.id == membership_id,
+        ),
+    )
+
+
+async def update_membership(
+    db: AsyncSession,
+    membership_id: UUID,
+    membership: schemas_cdr.MembershipEdit,
+):
+    await db.execute(
+        update(models_core.CoreAssociationMembership)
+        .where(models_core.CoreAssociationMembership.id == membership_id)
+        .values(**membership.model_dump(exclude_none=True)),
+    )
