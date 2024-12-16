4b016ae92ddcc1a0719bd6d8052718428bd967ac
Daihecyy <paul.saubusse@gmail.com>
2024-12-16 13:52:52 +0100

    POST endpoint

--- app/modules/cmm/models_cmm.py
+++ app/modules/cmm/models_cmm.py
@@ -14,18 +14,6 @@ from app.modules.cmm.types_cmm import MemeStatus
 from app.types.sqlalchemy import Base, PrimaryKey
 
 
-class Report(Base):
-    __tablename__ = "cmm_report"
-
-    id: Mapped[PrimaryKey]
-    user_id: Mapped[str] = mapped_column(ForeignKey("core_user.id"))
-    user: Mapped[CoreUser] = relationship("CoreUser", init=False)
-    creation_time: Mapped[datetime]
-    meme_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("cmm_meme.id"))
-    meme: Mapped[Meme] = relationship("Meme", init=False)
-    description: Mapped[str | None] = mapped_column(default=None)
-
-
 class Vote(Base):
     ___tablename___ = "cmm_vote"
 
@@ -53,9 +41,3 @@ class Meme(Base):
         default_factory=list,
     )
     status: Mapped[MemeStatus]
-    reports: Mapped[list[Report]] = relationship(
-        "Report",
-        secondary="cmm_report",
-        lazy="selectin",
-        default_factory=list,
-    )
