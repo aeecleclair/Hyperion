735b074fe1b42209d563eba3af8de9975a7cd602
Daihecyy <paul.saubusse@gmail.com>
2024-12-23 16:57:59 +0100

    Inbetween commit

---
+++ tests/test_cmm.py
@@ -0,0 +1,16 @@
+"""
+* [ ] All crud operations on meme
+    * [ ] Ajout de n memes avec les valeurs bien choisies pour avoir les sort par plusieurs users
+    * [ ] Delete de 1 meme
+    * [ ] read d'une page de meme dans tous les sens
+* [ ] All crud operations on vote
+    * [ ] Vote sur 1 meme par plusieurs user
+    * [ ] Changement du vote
+    * [ ] Delete du vote
+    * [ ] Read d'une page meme avec mes votes
+* [ ] All crud operations on ban
+    * [ ] Ban d'un user et impact sur ses memes
+    * [ ] Unban d'un user et impact sur ses memes
+* [ ] Testing edge cases with ban
+    * [ ] Call d'une méthode par un user ban
+"""
