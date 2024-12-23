c8bb7d2e28925c30ab8b104f04d1df7cfa1c9193
Thonyk <timr.web@free.fr>
2024-12-24 00:58:56 +0100

    Fix: add explicit verification for users during tests

--- tests/test_schools.py
+++ tests/test_schools.py
@@ -3,7 +3,7 @@ from fastapi.testclient import TestClient
 from pytest_mock import MockerFixture
 
 from app.core import models_core
-from app.core.groups.groups_type import GroupType
+from app.core.groups.groups_type import AccountType, GroupType
 from app.core.schools.schools_type import SchoolType
 from tests.commons import (
     add_object_to_db,
@@ -12,6 +12,8 @@ from tests.commons import (
 )
 
 admin_user: models_core.CoreUser
+ens_user: models_core.CoreUser
+new_school_user: models_core.CoreUser
 
 UNIQUE_TOKEN = "my_unique_token"
 
@@ -20,7 +22,7 @@ id_test_ens = "4d133de7-24c4-4dbc-be73-4705a2ddd315"
 
 @pytest_asyncio.fixture(scope="module", autouse=True)
 async def init_objects() -> None:
-    global admin_user
+    global admin_user, ens_user, new_school_user
 
     ens = models_core.CoreSchool(
         id=id_test_ens,
@@ -31,16 +33,18 @@ async def init_objects() -> None:
 
     admin_user = await create_user_with_groups([GroupType.admin])
 
-    await create_user_with_groups(
+    ens_user = await create_user_with_groups(
         [],
         school_id=id_test_ens,
         email="test@ens.fr",
+        account_type=AccountType.other_school_student,
     )
 
-    await create_user_with_groups(
+    new_school_user = await create_user_with_groups(
         [],
         school_id=SchoolType.no_school,
         email="test@school.fr",
+        account_type=AccountType.external,
     )
 
 
@@ -69,15 +73,22 @@ def test_read_school(client: TestClient) -> None:
 def test_create_school(client: TestClient) -> None:
     token = create_api_access_token(admin_user)
 
-    response = client.post(
+    school = client.post(
         "/schools/",
         json={
             "name": "school",
-            "email_regex": "^.*@school.fr$",
+            "email_regex": r"^.*@school\.fr$",
         },
         headers={"Authorization": f"Bearer {token}"},
     )
-    assert response.status_code == 201
+    assert school.status_code == 201
+
+    response = client.get(
+        f"/users/{new_school_user.id}",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    data = response.json()
+    assert data["school_id"] == school.json()["id"]
 
 
 def test_update_school(client: TestClient) -> None:
@@ -175,3 +186,10 @@ def test_delete_school(client: TestClient) -> None:
     )
     assert response.status_code == 404
     assert response.json() == {"detail": "School not found"}
+
+    response = client.get(
+        f"/users/{ens_user.id}",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 200
+    assert response.json()["school_id"] == SchoolType.no_school
