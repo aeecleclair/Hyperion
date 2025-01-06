cd621b7635ec2f9fc8d7cd2ca4b985067ff60e24
Marc-Andrieu <146140470+Marc-Andrieu@users.noreply.github.com>
2025-01-06 12:45:47 +0100

    Bump version 4.0.0 (#642)
    > [!IMPORTANT]
    > We really need to release new versions **soon**, as we have not for
    several months and there's some time needed to test in alpha before
    prod.
    
    This goes hand in hand with the **major version bump** [for
    Titan](../../Titan/pull/462).
    
    Both PRs are ready, and _seemingly_ waiting for some more **breaking
    changes** to be merged, including (by likelihood):
    
    - [ ] Allow students from other Schools to have a MyECL account: #641 
    - [ ] MyECLPay: #611 
    - [ ] "_Centrale Mega Meme_" Meme module: [no PR yet] [see
    branch](../tree/cmm)
    - [x] External Notifications: #613

--- app/core/config.py
+++ app/core/config.py
@@ -192,8 +192,8 @@ class Settings(BaseSettings):
 
     # Hyperion follows Semantic Versioning
     # https://semver.org/
-    HYPERION_VERSION: str = "3.1.2"
-    MINIMAL_TITAN_VERSION_CODE: int = 136
+    HYPERION_VERSION: str = "4.0.0"
+    MINIMAL_TITAN_VERSION_CODE: int = 137
 
     ######################################
     # Automatically generated parameters #
