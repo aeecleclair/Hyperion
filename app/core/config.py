1d1de79767539e4c6496b33a440187a5c85669e4
Timothée Robert <114694873+Rotheem@users.noreply.github.com>
2025-01-14 17:02:10 +0100

    Bump Titan minimal version (#653)
    ### Description
    
    Please explain the changes you made here.
    
    ### Checklist
    
    - [ ] Created tests which fail without the change (if possible)
    - [ ] All tests passing
    - [ ] Extended the documentation, if necessary

--- app/core/config.py
+++ app/core/config.py
@@ -193,7 +193,7 @@ class Settings(BaseSettings):
     # Hyperion follows Semantic Versioning
     # https://semver.org/
     HYPERION_VERSION: str = "4.0.0"
-    MINIMAL_TITAN_VERSION_CODE: int = 137
+    MINIMAL_TITAN_VERSION_CODE: int = 139
 
     ######################################
     # Automatically generated parameters #
