e819f450fa97b89c681035c7ec6d563327d0116f
Thonyk <timr.web@free.fr>
2025-02-03 02:21:38 +0100

    Feat: adapt reception route to new system

--- app/modules/cdr/models_cdr.py
+++ app/modules/cdr/models_cdr.py
@@ -12,7 +12,6 @@ from app.modules.cdr.types_cdr import (
     DocumentSignatureType,
     PaymentType,
 )
-from app.types.membership import AvailableAssociationMembership
 from app.types.sqlalchemy import Base, PrimaryKey
 
 
@@ -78,6 +77,11 @@ class CdrProduct(Base):
 
     description_fr: Mapped[str | None] = mapped_column(default=None)
     description_en: Mapped[str | None] = mapped_column(default=None)
+    related_membership_id: Mapped[uuid.UUID | None] = mapped_column(
+        ForeignKey("core_association_membership.id"),
+        nullable=True,
+        default=None,
+    )
 
     product_constraints: Mapped[list["CdrProduct"]] = relationship(
         "CdrProduct",
@@ -104,9 +108,10 @@ class CdrProduct(Base):
         lazy="selectin",
         default_factory=list,
     )
-
-    related_membership: Mapped[AvailableAssociationMembership | None] = mapped_column(
-        default=None,
+    related_membership: Mapped[CoreAssociationMembership | None] = relationship(
+        "CoreAssociationMembership",
+        lazy="joined",
+        init=False,
     )
 
 
