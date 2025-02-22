13f331a8accf4301c6915d18c74c8fac49fe3b69
Warix <39554785+warix8@users.noreply.github.com>
2025-02-22 15:22:42 +0100

    Factory Structure
    Initialise db for factories

--- app/utils/factory.py
+++ app/utils/factory.py
@@ -7,7 +7,7 @@ class Factory(ABC):
     def __init__(
         self,
         name: str,
-        depends_on: list["Factory"],
+        depends_on: list[type["Factory"]],
     ) -> None:
         self.name = name
         self.depends_on = depends_on
