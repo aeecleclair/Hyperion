ce2838a218782e8051530f9f132f02139a244136
cotanoine <tan.maillard@gmail.com>
2025-02-22 23:12:33 +0100

    Créé le fichier : factory_loan.py

---
+++ app/modules/loan/factory_loan.py
@@ -0,0 +1,123 @@
+import uuid
+from datetime import date
+
+from app.core.factory_core import CoreFactory
+from app.core.users import cruds_users
+from app.modules.loan import cruds_loan, models_loan
+from app.utils.factory import Factory
+
+
+class LoanFactory(Factory):
+    def __init__(self):
+        super().__init__(
+            name="Loan",
+            depends_on=[CoreFactory],
+        )
+
+    async def create_loan(self, db):
+        loaner_id_1 = str(uuid.uuid4())
+        group_manager_id_1 = str(uuid.uuid4())
+        await cruds_loan.create_loaner(
+            db=db,
+            loaner=models_loan.Loaner(
+                id=loaner_id_1,
+                name="Eclair",
+                group_manager_id=group_manager_id_1,
+            ),
+        )
+
+        loaner_id_2 = str(uuid.uuid4())
+        group_manager_id_2 = str(uuid.uuid4())
+        await cruds_loan.create_loaner(
+            db=db,
+            loaner=models_loan.Loaner(
+                id=loaner_id_2,
+                name="Bazar",
+                group_manager_id=group_manager_id_2,
+            ),
+        )
+        item_id_1 = str(uuid.uuid4())
+        item_1 = await cruds_loan.create_item(
+            db=db,
+            item=models_loan.Item(
+                id=item_id_1,
+                name="Cable HDMI",
+                loaner_id=loaner_id_1,
+                suggested_caution=0,
+                total_quantity=50,
+                suggested_lending_duration=360000,
+            ),
+        )
+
+        item_id_2 = str(uuid.uuid4())
+        item_2 = await cruds_loan.create_item(
+            db=db,
+            item=models_loan.Item(
+                id=item_id_2,
+                name="PC n°1",
+                loaner_id=loaner_id_1,
+                suggested_caution=1000,
+                total_quantity=100,
+                suggested_lending_duration=180000,
+            ),
+        )
+
+        item_id_3 = str(uuid.uuid4())
+        item_3 = await cruds_loan.create_item(
+            db=db,
+            item=models_loan.Item(
+                id=item_id_3,
+                name="Loup-Garou",
+                loaner_id=loaner_id_2,
+                suggested_caution=10,
+                total_quantity=2,
+                suggested_lending_duration=72000,
+            ),
+        )
+
+        user = await cruds_users.get_user_by_email(db, "demo.test@myecl.fr")
+        if user is None:
+            return
+        loan_id_1 = str(uuid.uuid4())
+        await cruds_loan.create_loan(
+            db=db,
+            loan=models_loan.Loan(
+                id=loan_id_1,
+                borrower_id=user.id,
+                loaner_id=loaner_id_1,
+                start=date(2025, 2, 22),
+                end=date(2025, 2, 25),
+                notes=None,
+                caution=None,
+                returned=False,
+                returned_date=None,
+                items=[item_1, item_2],
+            ),
+        )
+
+        loan_id_2 = str(uuid.uuid4())
+        await cruds_loan.create_loan(
+            db=db,
+            loan=models_loan.Loan(
+                id=loan_id_2,
+                borrower_id=user.id,
+                loaner_id=loaner_id_2,
+                start=date(2025, 2, 22),
+                end=date(2025, 2, 25),
+                notes=None,
+                caution="10",
+                returned=True,
+                returned_date=date(2025, 2, 24),
+                items=[item_3],
+            ),
+        )
+
+    async def run(self, db):
+        await self.create_loan(db)
+
+    async def should_run(self, db):
+        campaigns = await cruds_loan.get_loaners(db=db)
+        return len(campaigns) == 0
+
+
+factory = LoanFactory()
