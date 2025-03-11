ea36116e565489945ea75a285a8da5c04a49f7b8
Thonyk <timr.web@free.fr>
2025-03-11 12:10:46 +0100

    Feat: get all permissions

--- app/module.py
+++ app/module.py
@@ -8,10 +8,19 @@ hyperion_error_logger = logging.getLogger("hyperion.error")
 
 all_module_list: list[CoreModule] = []
 permissions_list: list[str] = []
+full_name_permissions_list: list[str] = []
 
 all_module_list.extend(module_list)
 all_module_list.extend(core_module_list)
 
 for module in all_module_list:
     if module.permissions:
-        permissions_list.extend(module.permissions.__members__.keys())
+        permissions_list.extend(
+            module.permissions.__members__.keys(),
+        )
+        full_name_permissions_list.extend(
+            [
+                module.permissions.__name__ + "." + name
+                for name in module.permissions.__members__
+            ],
+        )
