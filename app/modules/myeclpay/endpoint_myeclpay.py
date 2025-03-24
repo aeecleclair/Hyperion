e094d5bbbd716c8309dfd3d009c65ea7449c9c9a
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2025-03-24 17:23:35 +0100

    defining module

--- app/modules/myeclpay/endpoint_myeclpay.py
+++ app/modules/myeclpay/endpoint_myeclpay.py
@@ -1,12 +1,8 @@
-"""
-This file is needed in order to have MyECLPay in visibility.
-"""
-
 from app.core.groups.groups_type import AccountType
 from app.types.module import Module
 
 module = Module(
-    root="myeclpay",
+    root="payment",
     tag="MyECLPay",
     default_allowed_account_types=[AccountType.student, AccountType.staff],
 )
