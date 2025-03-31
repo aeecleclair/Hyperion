ad7c373cf52cce1e24a270c7faccfece83283861
Marc-Andrieu <146140470+Marc-Andrieu@users.noreply.github.com>
2025-03-31 19:45:08 +0200

    Bump version

--- app/core/utils/config.py
+++ app/core/utils/config.py
@@ -192,7 +192,7 @@ class Settings(BaseSettings):
 
     # Hyperion follows Semantic Versioning
     # https://semver.org/
-    HYPERION_VERSION: str = "4.3.0"
+    HYPERION_VERSION: str = "4.3.2"
     MINIMAL_TITAN_VERSION_CODE: int = 139
 
     ######################################
