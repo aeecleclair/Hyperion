31954b77c2832de20ec3c799224ed0f6c532c6c1
Thonyk <timr.web@free.fr>
2025-05-03 23:22:59 +0200

    feat: upgrade S3Access

--- app/types/s3_access.py
+++ app/types/s3_access.py
@@ -1,5 +1,6 @@
-import datetime
+from datetime import UTC, datetime, timedelta
 from io import BytesIO
+from logging import Logger
 
 import boto3  # type: ignore[import-untyped]
 import botocore
@@ -9,15 +10,31 @@ from app.core.utils.config import Settings
 from app.types.exceptions import (
     InvalidS3AccessError,
     InvalidS3BucketNameError,
+    InvalidS3FileNameError,
 )
 
 
 class S3Access:
-    """Classe pour gérer l'accès S3 avec verrouillage d'objet."""
+    """Class to manage S3 access with object locking."""
 
-    def __init__(self, settings: Settings):
+    def __init__(
+        self,
+        settings: Settings,
+        folder: str = "logs/",
+        retention: int = -1,
+        failure_logger: Logger | None = None,
+    ) -> None:
         self.bucket_name = settings.S3_BUCKET_NAME or "default-bucket-name"
-        self.folder = "logs/"
+        self.folder = folder
+        self.retention = retention
+        self.failure_logger = failure_logger
+        if settings.S3_ACCESS_KEY_ID is None or settings.S3_SECRET_ACCESS_KEY is None:
+            if self.failure_logger:
+                self.failure_logger.critical(
+                    "S3_ACCESS_KEY_ID or S3_SECRET_ACCESS_KEY is not set. Working with logger only.",
+                )
+            self.s3 = None
+            return
         self.s3 = boto3.client(
             "s3",
             aws_access_key_id=settings.S3_ACCESS_KEY_ID or "default",
@@ -32,38 +49,69 @@ class S3Access:
         ):
             raise InvalidS3BucketNameError(self.bucket_name)
 
-    def write_secure_log(self, message: str, date: datetime.datetime):
+    def write_secure_log(self, message: str, filename: str) -> str:
         """Écrit un log dans un objet S3 immuable"""
-        filename = f"{self.folder}log_{date.strftime('%Y%m%dT%H%M%S')}.txt"
+        if filename.find("/") != -1:
+            raise InvalidS3FileNameError(filename)
+        filename = f"{self.folder}{filename}"
 
         file_object = BytesIO(message.encode("utf-8"))
 
+        if self.s3 is None:
+            if self.failure_logger:
+                self.failure_logger.warning(
+                    f"POST Filename: {filename}, Message: {message}",
+                )
+            return filename
         result = self.s3.upload_fileobj(
             file_object,
             self.bucket_name,
             filename,
-            ExtraArgs={
-                "ContentType": "text/plain",
-            },
+            extra_args={
+                "ObjectLockMode": "COMPLIANCE",
+                "ObjectLockRetainUntilDate": datetime.now(UTC)
+                + timedelta(days=self.retention),
+            }
+            if self.retention > 0
+            else {},
         )
-        return filename if result is None else result
+        if result is None:
+            return filename
+        if self.failure_logger:
+            self.failure_logger.warning(f"Filename: {filename}, Message: {message}")
+            self.failure_logger.info(f"Filename: {filename}, Result: {result}")
+        return filename
 
     def get_log_with_name(self, name: str) -> str:
-        """Récupère les logs avec un préfixe donné"""
+        """Récupère les logs avec un nom donné"""
+        if name.find("/") != -1:
+            raise InvalidS3FileNameError(name)
+        name = f"{self.folder}{name}"
         file_object = BytesIO()
+        if self.s3 is None:
+            if self.failure_logger:
+                self.failure_logger.warning(f"GET Filename: {name}")
+            return name
         self.s3.download_fileobj(self.bucket_name, name, file_object)
         return file_object.getvalue().decode("utf-8")
 
     def list_object(self, prefix: str):
         """Liste les objets S3 avec un préfixe donné"""
+        if prefix.find("/") != -1:
+            raise InvalidS3FileNameError(prefix)
+        prefix = f"{self.folder}{prefix}"
+        if self.s3 is None:
+            if self.failure_logger:
+                self.failure_logger.warning(f"LIST Prefix: {prefix}")
+            return {"Contents": []}
         objects = self.s3.list_objects_v2(Prefix=prefix, Bucket=self.bucket_name)
 
         return objects
 
-    def list_log_content_for_prefix(self, prefix: str) -> list[str]:
+    def get_log_content_for_prefix(self, prefix: str) -> list[str]:
         """Liste les fichiers de logs dans le bucket S3"""
         objects = self.list_object(prefix)
         if "Contents" not in objects or not objects["Contents"]:
             return []
-        file_names = [obj["Key"] for obj in objects["Contents"]]
+        file_names = [obj["Key"].split("/")[-1] for obj in objects["Contents"]]
         return [self.get_log_with_name(x) for x in file_names]
