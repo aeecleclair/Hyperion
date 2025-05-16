a54f3a15a5e3f6aa8339d82fe8523fe29b61b78e
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-05-16 13:21:54 +0200

    Release 4.4.2

--- app/core/utils/config.py
+++ app/core/utils/config.py
@@ -214,7 +214,7 @@ class Settings(BaseSettings):
 
     # Hyperion follows Semantic Versioning
     # https://semver.org/
-    HYPERION_VERSION: str = "4.4.1"
+    HYPERION_VERSION: str = "4.4.2"
     MINIMAL_TITAN_VERSION_CODE: int = 139
 
     # Maximum wallet balance for MyECLPay in cents, we will prevent user from adding more money to their wallet if it will make their balance exceed this value
