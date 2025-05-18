d9b70449b9eadbe5a0866549373981784e0753da
Thonyk <timr.web@free.fr>
2025-05-18 02:56:11 +0200

    feat: fix method name and add deleters

--- app/modules/raid/user_deleter_raid.py
+++ app/modules/raid/user_deleter_raid.py
@@ -1,14 +1,17 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.modules.raid.cruds_raid import is_user_a_participant
 from app.types.module_user_deleter import ModuleUserDeleter
 
 
 class RaidUserDeleter(ModuleUserDeleter):
-    async def can_delete_user(
+    async def has_reason_not_to_delete_user(
         self,
         user_id: str,
         db: AsyncSession,
     ) -> str:
+        if await is_user_a_participant(user_id, db) is not None:
+            return "\n   - User is a participant in the current edition"
         return ""
 
     async def delete_user(self, user_id: str, db: AsyncSession) -> None:
