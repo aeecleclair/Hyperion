c1b3a7d63eb37004e095558fc86a9fdc2029dc3a
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-06-01 13:07:39 +0200

    Release ToS version 2

--- app/core/myeclpay/utils_myeclpay.py
+++ app/core/myeclpay/utils_myeclpay.py
@@ -23,7 +23,7 @@ hyperion_security_logger = logging.getLogger("hyperion.security")
 hyperion_myeclpay_logger = logging.getLogger("hyperion.myeclpay")
 hyperion_error_logger = logging.getLogger("hyperion.error")
 
-LATEST_TOS = 1
+LATEST_TOS = 2
 MAX_TRANSACTION_TOTAL = 2000
 QRCODE_EXPIRATION = 5  # minutes
 
