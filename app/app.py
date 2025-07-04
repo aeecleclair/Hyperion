d07ac8d0af750571149b64e7716db004e21f36f9
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-07-04 18:21:25 +0200

    Refactor module_list and core_module_list
    Co-authored-by: Thonyk <timr.web@free.fr>

--- app/app.py
+++ app/app.py
@@ -46,6 +46,8 @@ from app.types.exceptions import (
     GoogleAPIInvalidCredentialsError,
     MultipleWorkersWithoutRedisInitializationError,
 )
+from app.module import module_list
+from app.types.exceptions import ContentHTTPException, GoogleAPIInvalidCredentialsError
 from app.types.sqlalchemy import Base
 from app.utils import initialization
 from app.utils.redis import limiter
