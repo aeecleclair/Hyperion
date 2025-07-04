04ac4526b7fa913bd1304db24ad0c05f9f9670c6
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2025-07-04 12:53:19 +0200

    Remove Message class

--- app/core/notification/models_notification.py
+++ app/core/notification/models_notification.py
@@ -6,36 +6,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 from app.types.sqlalchemy import Base
 
 
-class Message(Base):
-    __tablename__ = "notification_message"
-
-    # A context represents a topic (ex: a loan),
-    # there can only by one notification per context (ex: the loan should be returned, the loan is overdue or has been returned)
-    context: Mapped[str] = mapped_column(index=True, primary_key=True)
-    # If there can be one notification per context, there can be multiple messages per notification: one by firebase_device_token
-    # A firebase_device_token is related to a device from an user (its phone, its computer, etc.)
-    firebase_device_token: Mapped[str] = mapped_column(
-        index=True,
-        primary_key=True,
-    )
-
-    title: Mapped[str | None]
-    content: Mapped[str | None]
-
-    # TODO
-    # An action id is used by Titan to know what to do when receiving the notification
-    action_module: Mapped[str | None]
-    action_table: Mapped[str | None]
-
-    # We can plan a notification to be sent later, the frontend should not display it before the planned date
-    delivery_datetime: Mapped[datetime | None]
-    # Messages should be deleted after a certain time
-    expire_on: Mapped[datetime]
-
-    # A message can be visible or not, if it is not visible, it should only trigger an action
-    is_visible: Mapped[bool] = mapped_column(default=True)
-
-
 class FirebaseDevice(Base):
     __tablename__ = "notification_firebase_devices"
 
