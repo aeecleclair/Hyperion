42dbf36fc118738bf4c92f74950994221f8824e8
Rayan <rbelahcene10@gmail.com>
2025-08-11 21:54:32 +0200

    Remove comments

--- app/modules/cdr/coredata_cdr.py
+++ app/modules/cdr/coredata_cdr.py
@@ -1,10 +1,6 @@
-# from app.modules.cdr.types_cdr import CdrStatus
 from datetime import UTC, datetime
 
 from app.types.core_data import BaseCoreData
-
-# class Status(BaseCoreData):
-#     status: CdrStatus = CdrStatus.pending
 
 
 class CdrYear(BaseCoreData):
