86885f66a736f050afd3d53ee32b567f2487e830
Marc-Andrieu <146140470+Marc-Andrieu@users.noreply.github.com>
2025-09-16 01:14:20 +0200

    Rebase migration

--- migrations/versions/41_promo_above_2000.py
+++ migrations/versions/41_promo_above_2000.py
@@ -1,4 +1,4 @@
-"""40-promo-above-2000
+"""41-promo-above-2000
 
 Create Date: 2025-09-07 09:54:34.421809
 """
@@ -17,7 +17,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "91fadc90f892"
-down_revision: str | None = "2880c583d7f1"
+down_revision: str | None = "0f66a87dc3ce"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
