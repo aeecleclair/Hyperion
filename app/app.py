af97e20f10ccbff5ebe422cc7427ba73a94c562a
Thonyk <timr.web@free.fr>
2025-10-24 10:57:45 +0200

    Fix: correct redis limiter input key value

--- app/app.py
+++ app/app.py
@@ -684,7 +684,7 @@ def get_application(settings: Settings, drop_db: bool = False) -> FastAPI:
         if redis_client and settings.ENABLE_RATE_LIMITER:  # If redis is configured
             process, log = limiter(
                 redis_client,
-                ip_address,
+                str(ip_address),
                 settings.REDIS_LIMIT,
                 settings.REDIS_WINDOW,
             )
