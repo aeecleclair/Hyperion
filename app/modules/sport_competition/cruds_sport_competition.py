ec2c9175ff462b87be8c38b8c2b1bda028cef1e2
Thonyk <timr.web@free.fr>
2026-01-09 10:32:39 +0100

    fix: correct delete

--- app/modules/sport_competition/cruds_sport_competition.py
+++ app/modules/sport_competition/cruds_sport_competition.py
@@ -297,11 +297,12 @@ async def remove_user_from_group(
     edition_id: UUID,
     db: AsyncSession,
 ) -> None:
-    await db.delete(
-        models_sport_competition.CompetitionGroupMembership(
-            user_id=user_id,
-            group=group,
-            edition_id=edition_id,
+    await db.execute(delete(
+        models_sport_competition.CompetitionGroupMembership,
+        ).where(
+            models_sport_competition.CompetitionGroupMembership.user_id == user_id,
+            models_sport_competition.CompetitionGroupMembership.group == group,
+            models_sport_competition.CompetitionGroupMembership.edition_id == edition_id,
         ),
     )
     await db.flush()
