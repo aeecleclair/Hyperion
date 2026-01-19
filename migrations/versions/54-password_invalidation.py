7abf71b59cf0c483b03f3343e0ff52d8704b3ce1
Marc-Andrieu <marc.andrieu@outlook.com>
2026-01-19 14:16:18 +0100

    Fix migration rebase

--- migrations/versions/54-password_invalidation.py
+++ migrations/versions/54-password_invalidation.py
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "cfba514689ed"
-down_revision: str | None = "c4812e1ab108"
+down_revision: str | None = "9fc3dc926600"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
