523775aca1dfc8086c98410c2618f4e97a8c5a86
Timothée Robert <114694873+Rotheem@users.noreply.github.com>
2026-02-05 14:46:48 +0100

    Fix competition validation (#951)
    # Description
    
    ## Summary
    
    <!--BRIEF description: DONT'T EXPLAIN the code: JUSTIFY what this PR is
    for!-->
    
    ...
    
    <!--#### Sources at the end-->
    
    ## Issues/PR dependencies
    
    <!--Use a keyword, then #123 for the same repo or
    aeecleclair/RepoName#123 for another-->
    
    ### Issues to be resolved
    
    <!--Keywords: "closes", "fixes", "resolves" -->
    <!--Fixes #-->
    
    ### Required PRs
    
    <!--Keywords: "depends on", "blocked by" -->
    <!--Depends on #-->
    
    ## Changes Made
    
    <!--DESCRIBE the changes: tell the BIG STEPS, use a CHECKLIST to show
    progress. You can explain below how the code works.-->
    
    - [x] ...
    - [ ] ...
    
    ## Additional Notes
    
    <!--Anything relevant that does not quite fit in the summary-->
    
    <!--Don't touch thses two tags-->
    <details>
    <summary>
    
    # Classification
    
    </summary>
    
    ## Type of Change
    
    - [ ] 🐛 Bug fix (non-breaking change which fixes an issue)
    - [ ] ✨ New feature (non-breaking change which adds functionality)
    - [ ] 🔨 Refactor (non-breaking change that neither fixes a bug nor adds
    a feature)
    - [ ] 🔧 Infra CI/CD (changes to configs of workflows)
    - [ ] 💥 BREAKING CHANGE (fix or feature that require a new minimal
    version of the front-end)
    - [ ] 😶‍🌫️ No impact for the end-users
    
    ## Impact & Scope
    
    - [ ] Core functionality changes
    - [ ] Single module changes
    - [ ] Multiple modules changes
    - [ ] Database migrations required
    - [ ] Other: ... <!--Not module-oriented: write something!-->
    
    ## Testing
    
    - [ ] 1. Tested this locally
    - [ ] 2. Added/modified tests that pass the CI (or tested in a
    downstream fork)
    - [ ] 3. Tested in a deployed pre-prod
    - [ ] 0. Untestable (exceptionally), will be tested in prod directly
    
    ## Documentation
    
    - [ ] Updated [the docs](docs.myecl.fr) accordingly : <!--[Docs#0 -
    Title](https://github.com/aeecleclair/myecl-documentation/pull/0)-->
    - [ ] `"` Docstrings
    - [ ] `#` Inline comments
    - [ ] No documentation needed
    
    </details>

--- app/modules/sport_competition/cruds_sport_competition.py
+++ app/modules/sport_competition/cruds_sport_competition.py
@@ -297,11 +297,14 @@ async def remove_user_from_group(
     edition_id: UUID,
     db: AsyncSession,
 ) -> None:
-    await db.delete(
-        models_sport_competition.CompetitionGroupMembership(
-            user_id=user_id,
-            group=group,
-            edition_id=edition_id,
+    await db.execute(
+        delete(
+            models_sport_competition.CompetitionGroupMembership,
+        ).where(
+            models_sport_competition.CompetitionGroupMembership.user_id == user_id,
+            models_sport_competition.CompetitionGroupMembership.group == group,
+            models_sport_competition.CompetitionGroupMembership.edition_id
+            == edition_id,
         ),
     )
     await db.flush()
@@ -917,7 +920,9 @@ async def count_validated_participants_by_school_and_sport_ids(
         .join(
             models_sport_competition.CompetitionUser,
             models_sport_competition.CompetitionParticipant.user_id
-            == models_sport_competition.CompetitionUser.user_id,
+            == models_sport_competition.CompetitionUser.user_id
+            and models_sport_competition.CompetitionParticipant.edition_id
+            == models_sport_competition.CompetitionUser.edition_id,
         )
         .where(
             models_sport_competition.CompetitionParticipant.sport_id == sport_id,
@@ -2460,7 +2465,9 @@ async def count_validated_purchases_by_product_id_and_school_id(
         .join(
             models_sport_competition.CompetitionUser,
             models_sport_competition.CompetitionPurchase.user_id
-            == models_sport_competition.CompetitionUser.user_id,
+            == models_sport_competition.CompetitionUser.user_id
+            and models_sport_competition.CompetitionPurchase.edition_id
+            == models_sport_competition.CompetitionUser.edition_id,
         )
         .join(
             models_users.CoreUser,
