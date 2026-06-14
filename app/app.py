0ff9e692041fdab3a9846f0221699a9bc7e4f893
Armand Didierjean <95971503+armanddidierjean@users.noreply.github.com>
2026-06-14 18:54:14 +0200

    Use HTTP_422_UNPROCESSABLE_CONTENT instead of HTTP_422_UNPROCESSABLE_ENTITY (#75)

--- app/app.py
+++ app/app.py
@@ -733,7 +733,7 @@ def get_application(settings: Settings, drop_db: bool = False) -> FastAPI:
         )
 
         return JSONResponse(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
             content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
         )
 
