bb9d991d08bf171b0f9b612969e7370b912dcd08
Ilyass <bombefire7@gmail.com>
2026-08-11 22:22:27 +0200

    resolving mypy

--- app/modules/cdr/cruds_cdr.py
+++ app/modules/cdr/cruds_cdr.py
@@ -862,7 +862,7 @@ async def get_total_payment_types(
 async def get_total_payment(
     db: AsyncSession,
     cdr_year: int,
-) -> int:
+):
 
     result = await db.execute(
         select(
@@ -870,7 +870,7 @@ async def get_total_payment(
         ).where(models_cdr.Payment.year == cdr_year),
     )
     try:
-        return result.all()[0].total / 100
+        return result.all()[0].total // 100
     except IndexError:
         return 0
 
