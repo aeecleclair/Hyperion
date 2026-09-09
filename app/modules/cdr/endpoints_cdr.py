c88b8001935300696a02c1f79835b88e380613e4
Ilyass <bombefire7@gmail.com>
2026-09-09 03:57:01 +0200

    Adding the year of cdr as a param in the endpoint

--- app/modules/cdr/endpoints_cdr.py
+++ app/modules/cdr/endpoints_cdr.py
@@ -2763,6 +2763,7 @@ async def get_total_payments(
     user: models_users.CoreUser = Depends(
         is_user_allowed_to([CdrPermissions.manage_cdr]),
     ),
+    cdr_year: coredata_cdr.CdrYear = Depends(get_current_cdr_year),
 ):
     """
     Get a user's payments.
@@ -2778,6 +2779,7 @@ async def get_total_payments(
         )
     return await cruds_cdr.get_payment_products_by_sell(
         db=db,
+        cdr_year=cdr_year.year
     )
 
 @module.router.get(
@@ -2790,6 +2792,7 @@ async def get_total_payments(
     user: models_users.CoreUser = Depends(
         is_user_allowed_to([CdrPermissions.manage_cdr]),
     ),
+    cdr_year: coredata_cdr.CdrYear = Depends(get_current_cdr_year),
 ):
     """
     Get a user's payments.
@@ -2805,6 +2808,7 @@ async def get_total_payments(
         )
     return await cruds_cdr.get_total_payment_types(
         db=db,
+        cdr_year=cdr_year.year
     )
 
 @module.router.get(
