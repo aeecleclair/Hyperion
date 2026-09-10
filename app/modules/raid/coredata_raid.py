b1107539463f2206eb03c6747c1b026854eaacd0
maximeroucher <maximeroucher@gmail.com>
2026-09-10 23:15:04 +0200

    feat: adding scholarship price

--- app/modules/raid/coredata_raid.py
+++ app/modules/raid/coredata_raid.py
@@ -23,5 +23,6 @@ class RaidPrice(core_data.BaseCoreData):
     student_price: int | None = None
     partner_price: int | None = None
     external_price: int | None = None
+    scholarship_price: int | None = None
     t_shirt_price: int | None = None
     volunteer_price: int | None = None
